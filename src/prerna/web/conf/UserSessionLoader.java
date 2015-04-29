51aeed6a23f07da2e8c3c649a599888bba6674c1
karverma <karverma@deloitte.com>
2015-04-29 06:36:02 +0000

    Adding user-specific DB filtering, adding anonymous user to session when created
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@7155 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/web/conf/UserSessionLoader.java
@@ -0,0 +1,23 @@
+package prerna.web.conf;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import prerna.auth.User;
+import prerna.auth.User.LOGIN_TYPES;
+import prerna.util.Constants;
+
+@WebListener
+public class UserSessionLoader implements HttpSessionListener {
+	
+	public void sessionCreated(HttpSessionEvent sessionEvent) {
+		System.out.println("Session Created: ID="+sessionEvent.getSession().getId());
+		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User("1", "Anonymous", LOGIN_TYPES.anonymous, "Anonymous"));
+	}
+	 
+	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
+//		System.out.println("Session Destroyed: ID="+sessionEvent.getSession().getId());
+//		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User("1", "Anonymous", LOGIN_TYPES.anonymous, "Anonymous"));
+	}
+}
