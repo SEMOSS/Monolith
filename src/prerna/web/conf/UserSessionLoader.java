69a730a1552bb6f5f20b6fe240f068aa13a0eaa5
mahkhalil <mahkhalil@deloitte.com>
2015-07-24 19:39:27 +0000

    clearing QuestionPlaySheetStore and ITableDataFrameStore when a session is invalidated
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@8904 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/UserSessionLoader.java
+++ src/prerna/web/conf/UserSessionLoader.java
@@ -1,12 +1,16 @@
 package prerna.web.conf;
 
+import java.util.Set;
+
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import prerna.auth.User;
 import prerna.auth.User.LOGIN_TYPES;
+import prerna.ds.ITableDataFrameStore;
 import prerna.util.Constants;
+import prerna.util.QuestionPlaySheetStore;
 
 @WebListener
 public class UserSessionLoader implements HttpSessionListener {
@@ -19,5 +23,27 @@ public class UserSessionLoader implements HttpSessionListener {
 	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
 //		System.out.println("Session Destroyed: ID="+sessionEvent.getSession().getId());
 //		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User("1", "Anonymous", LOGIN_TYPES.anonymous, "Anonymous"));
+		String sessionID = sessionEvent.getSession().getId();
+		
+		// clear up question store
+		QuestionPlaySheetStore qStore = QuestionPlaySheetStore.getInstance();
+		Set<String> quesitonIDsToDelete = qStore.getPlaySheetIDsForSession(sessionID);
+		if(quesitonIDsToDelete != null) {
+			for(String questionID : quesitonIDsToDelete) {
+				qStore.remove(questionID);
+				qStore.removeFromSessionHash(sessionID, questionID);
+			}
+		}
+		
+		// clear up ITable store
+		ITableDataFrameStore tableStore = ITableDataFrameStore.getInstance();
+		Set<String> tableIDsToDelete = tableStore.getTableIDsForSession(sessionID);
+		if(tableIDsToDelete != null) {
+			for(String tableID : tableIDsToDelete) {
+				tableStore.remove(tableID);
+				tableStore.removeFromSessionHash(sessionID, tableID);
+			}
+		}
+		
 	}
 }
