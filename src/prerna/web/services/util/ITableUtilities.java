2905e580f745dfda602d5c416ada98470af15f95
rluthar <rluthar@deloitte.com>
2015-10-01 21:17:38 +0000

    optimization updates to filtering
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@10242 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 632861681701aeb3e9d98c57c4400a44e4c44604

--- src/prerna/web/services/util/ITableUtilities.java
+++ src/prerna/web/services/util/ITableUtilities.java
@@ -2,9 +2,13 @@ package prerna.web.services.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.Response;
 
@@ -22,7 +26,8 @@ public class ITableUtilities {
 	}
 	
 	public static void filterData(ITableDataFrame mainTree, Map<String, List<Object>> filterValuesArrMap) {
-		mainTree.unfilter();
+//		mainTree.unfilter();
+		//boolean unfiltered = false;
 		for(String concept: filterValuesArrMap.keySet()) {
 
 			List<Object> filterValuesArr = filterValuesArrMap.get(concept);
@@ -37,22 +42,21 @@ public class ITableUtilities {
 				}
 				filterValuesArr = values;
 			}
-//			if(filterValuesArr == null || filterValuesArr.isEmpty()) {
-//				Map<String, Object> retMap = new HashMap<String, Object>();
-//				retMap.put("tableID", tableID);
-//				return Response.status(200).entity(WebUtility.getSO(retMap)).build();
-//			}
+			if(filterValuesArr.isEmpty()) {
+				mainTree.filter(concept, Arrays.asList(mainTree.getUniqueValues(concept)));
+				return;
+			}
 
 			//if filterValuesArr not a subset of superSet, then unfilter
-//			Object[] superSet = mainTree.getUniqueValues(concept);
-//
-//			int n = filterValuesArr.size();
-//			int m = superSet.length;
-//
-//			if(m < n) {
-//				mainTree.unfilter();
-//				unfiltered = true;
-//			} else {
+			Object[] superSet = mainTree.getUniqueValues(concept);
+
+			int n = filterValuesArr.size();
+			int m = superSet.length;
+
+			if(m < n) {
+				mainTree.unfilter();
+				//unfiltered = true;
+			} else {
 //				Comparator<Object> comparator = new Comparator<Object>() {
 //					public int compare(Object o1, Object o2) {
 //						return o1.toString().compareTo(o2.toString());
@@ -62,30 +66,30 @@ public class ITableUtilities {
 //				//check if filterValuesArr is a subset of superSet
 //				Arrays.sort(superSet, comparator);
 //				Collections.sort(filterValuesArr, comparator);
-//
-//				int i = 0;
-//				int j = 0;
-//				while(i < n && j < m) {
-//					int compareTo = superSet[i].toString().compareToIgnoreCase(filterValuesArr.get(i).toString());
-//					if(compareTo < 0) {
-//						j++;
-//					} else if(compareTo == 0) {
-//						j++; i++;
-//					} else if(compareTo > 0) {
-//						mainTree.unfilter();
-//						unfiltered = true;
-//						break;
-//					}
-//				}
-//			}
 
-			List<Object> setDiff = new ArrayList<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
-//			Set<Object> totalSet = new HashSet<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
-//			for(Object o : filterValuesArr) {
-//				totalSet.remove(o);
-//			}
-			setDiff.removeAll(filterValuesArr);
-			mainTree.filter(concept, setDiff);
+				int i = 0;
+				int j = 0;
+				while(i < n && j < m) {
+					int compareTo = superSet[i].toString().compareToIgnoreCase(filterValuesArr.get(i).toString());
+					if(compareTo < 0) {
+						j++;
+					} else if(compareTo == 0) {
+						j++; i++;
+					} else if(compareTo > 0) {
+						mainTree.unfilter();
+						//unfiltered = true;
+						break;
+					}
+				}
+			}
+
+//			List<Object> setDiff = new ArrayList<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
+			Set<Object> totalSet = new HashSet<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
+			for(Object o : filterValuesArr) {
+				totalSet.remove(o);
+			}
+//			setDiff.removeAll(filterValuesArr);
+			mainTree.filter(concept, new ArrayList<Object>(totalSet));
 		}
 	}
 	
