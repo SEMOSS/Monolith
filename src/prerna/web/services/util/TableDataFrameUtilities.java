2925a306937db1bb7290b5b011a4639e6f7c8eb0
rluthar <rluthar@deloitte.com>
2015-10-08 21:53:52 +0000

    changes to filter to match front end changes
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@10414 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/services/util/TableDataFrameUtilities.java
+++ src/prerna/web/services/util/TableDataFrameUtilities.java
@@ -24,7 +24,7 @@ public final class TableDataFrameUtilities {
 	}
 	
 	public static void filterData(ITableDataFrame mainTree, Map<String, List<Object>> filterValuesArrMap) {
-		mainTree.unfilter();
+//		mainTree.unfilter();
 		//boolean unfiltered = false;
 		for(String concept: filterValuesArrMap.keySet()) {
 
@@ -46,40 +46,40 @@ public final class TableDataFrameUtilities {
 			}
 
 //			//if filterValuesArr not a subset of superSet, then unfilter
-//			Object[] superSet = mainTree.getUniqueValues(concept);
-//
-//			int n = filterValuesArr.size();
-//			int m = superSet.length;
-//
-//			if(m < n) {
-//				mainTree.unfilter();
-//				//unfiltered = true;
-//			} else {
-//				Comparator<Object> comparator = new Comparator<Object>() {
-//					public int compare(Object o1, Object o2) {
-//						return o1.toString().compareTo(o2.toString());
-//					}
-//				};
-//
-//				//check if filterValuesArr is a subset of superSet
-//				Arrays.sort(superSet, comparator);
-//				Collections.sort(filterValuesArr, comparator);
-//
-//				int i = 0;
-//				int j = 0;
-//				while(i < n && j < m) {
-//					int compareTo = superSet[i].toString().compareToIgnoreCase(filterValuesArr.get(i).toString());
-//					if(compareTo < 0) {
-//						j++;
-//					} else if(compareTo == 0) {
-//						j++; i++;
-//					} else if(compareTo > 0) {
-//						mainTree.unfilter();
-//						//unfiltered = true;
-//						break;
-//					}
-//				}
-//			}
+			Object[] superSet = mainTree.getUniqueValues(concept);
+
+			int n = filterValuesArr.size();
+			int m = superSet.length;
+
+			if(m < n) {
+				mainTree.unfilter();
+				//unfiltered = true;
+			} else {
+				Comparator<Object> comparator = new Comparator<Object>() {
+					public int compare(Object o1, Object o2) {
+						return o1.toString().compareTo(o2.toString());
+					}
+				};
+
+				//check if filterValuesArr is a subset of superSet
+				Arrays.sort(superSet, comparator);
+				Collections.sort(filterValuesArr, comparator);
+
+				int i = 0;
+				int j = 0;
+				while(i < n && j < m) {
+					int compareTo = superSet[i].toString().compareToIgnoreCase(filterValuesArr.get(i).toString());
+					if(compareTo < 0) {
+						j++;
+					} else if(compareTo == 0) {
+						j++; i++;
+					} else if(compareTo > 0) {
+						mainTree.unfilter();
+						//unfiltered = true;
+						break;
+					}
+				}
+			}
 
 //			List<Object> setDiff = new ArrayList<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
 			Set<Object> totalSet = new HashSet<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
