656a8398e8e1146f066b7aa2e85f5e5444a658ac
rluthar <rluthar@deloitte.com>
2015-10-10 18:39:26 +0000

    Committing updates to filter to mimic front end changes in Overview
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@10444 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 731063a5c5daa1ca67c08fce8f50561480a826a7

--- src/prerna/web/services/util/TableDataFrameUtilities.java
+++ src/prerna/web/services/util/TableDataFrameUtilities.java
@@ -15,6 +15,7 @@ import prerna.ds.ITableWebAdapter;
 import prerna.ds.TableDataFrameStore;
 import prerna.ui.components.api.IPlaySheet;
 import prerna.ui.components.playsheets.BasicProcessingPlaySheet;
+import prerna.util.ArrayUtilityMethods;
 import prerna.util.QuestionPlaySheetStore;
 
 public final class TableDataFrameUtilities {
@@ -26,7 +27,16 @@ public final class TableDataFrameUtilities {
 	public static void filterData(ITableDataFrame mainTree, Map<String, List<Object>> filterValuesArrMap) {
 //		mainTree.unfilter();
 		//boolean unfiltered = false;
-		for(String concept: filterValuesArrMap.keySet()) {
+		Set<String> keySet = filterValuesArrMap.keySet();
+		String[] columnHeaders = mainTree.getColumnHeaders();
+		String[] concepts = keySet.toArray(new String[keySet.size()]);
+		for(String column : columnHeaders) {
+			if(!ArrayUtilityMethods.arrayContainsValue(concepts, column)) {
+				mainTree.unfilter(column);
+			}
+		}
+		
+		for(String concept: keySet) {
 
 			List<Object> filterValuesArr = filterValuesArrMap.get(concept);
 			if(mainTree.isNumeric(concept)) {
@@ -91,8 +101,99 @@ public final class TableDataFrameUtilities {
 		}
 	}
 	
-	public static void unfilterData() {
+	public static void filterData(ITableDataFrame mainTree, Map<String, List<Object>> filterValuesArrMap, Map<String, Object[]> storedValues) {
+		if(storedValues== null) {
+			for(String concept : filterValuesArrMap.keySet()) {
+				mainTree.filter(concept, filterValuesArrMap.get(concept));
+			}
+			return;
+		}
+		//need to find the which column is different from previous, then filter only that column
+		//when first different column is found, call filterColumn on that column
+		String[] columnHeaders = mainTree.getColumnHeaders();
+		for(String columnHeader : columnHeaders) {
+			Object[] storedValuesArr = storedValues.get(columnHeader);
+			if(filterValuesArrMap.containsKey(columnHeader)) {
+				List<Object> filterValuesArr = filterValuesArrMap.get(columnHeader);
+				if(!equals(filterValuesArr, storedValuesArr)) {
+					filterColumn(mainTree, columnHeader, filterValuesArr);
+					return;
+				}
+			} 
+			else {
+				int totalSize = mainTree.getUniqueRawValues(columnHeader).length + mainTree.getFilteredUniqueRawValues(columnHeader).length;
+				if(totalSize != storedValuesArr.length) {
+					mainTree.unfilter();
+				}
+			}
+		}
+	}
+	
+	private static void filterColumn(ITableDataFrame mainTree, String concept, List<Object> filterValuesArr) {
 		
+		if(mainTree.isNumeric(concept)) {
+			List<Object> values = new ArrayList<Object>(filterValuesArr.size());
+			for(Object o: filterValuesArr) {
+				try {
+					values.add(Double.parseDouble(o.toString()));
+				} catch(Exception e) {
+					values.add(o);
+				}
+			}
+			filterValuesArr = values;
+		}
+		if(filterValuesArr.isEmpty()) {
+			mainTree.filter(concept, Arrays.asList(mainTree.getUniqueValues(concept)));
+			return;
+		}
+
+//		//if filterValuesArr not a subset of superSet, then unfilter
+		Object[] superSet = mainTree.getUniqueValues(concept);
+
+		int n = filterValuesArr.size();
+		int m = superSet.length;
+
+		if(m < n) {
+			mainTree.unfilter();
+			//unfiltered = true;
+		} else {
+			Comparator<Object> comparator = new Comparator<Object>() {
+				public int compare(Object o1, Object o2) {
+					return o1.toString().compareTo(o2.toString());
+				}
+			};
+
+			//check if filterValuesArr is a subset of superSet
+			Arrays.sort(superSet, comparator);
+			Collections.sort(filterValuesArr, comparator);
+
+			int i = 0;
+			int j = 0;
+			while(i < n && j < m) {
+				int compareTo = superSet[i].toString().compareToIgnoreCase(filterValuesArr.get(i).toString());
+				if(compareTo < 0) {
+					j++;
+				} else if(compareTo == 0) {
+					j++; i++;
+				} else if(compareTo > 0) {
+					mainTree.unfilter();
+					//unfiltered = true;
+					break;
+				}
+			}
+		}
+
+//		List<Object> setDiff = new ArrayList<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
+		Set<Object> totalSet = new HashSet<Object>(Arrays.asList(mainTree.getUniqueValues(concept)));
+		for(Object o : filterValuesArr) {
+			totalSet.remove(o);
+		}
+//		setDiff.removeAll(filterValuesArr);
+		mainTree.filter(concept, new ArrayList<Object>(totalSet));
+	}
+	
+	private static boolean equals(List<Object> newColumn, Object[] oldColumn) {
+		return newColumn.size() == oldColumn.length;
 	}
 	
 	public static ITableDataFrame getTable(String tableID, String questionID) {
