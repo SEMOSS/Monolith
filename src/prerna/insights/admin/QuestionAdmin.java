d76dff99961935295708485e3c9206796daf822a
bisutton <bisutton@deloitte.com>
2015-11-12 21:04:27 +0000

    Moving a lot of param logic to question administrator. This seems like the logical place and prevents the issues arising with modifying the dmc list when saving.
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@11144 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/insights/admin/QuestionAdmin.java
+++ src/prerna/insights/admin/QuestionAdmin.java
@@ -56,7 +56,6 @@ import prerna.om.SEMOSSParam;
 import prerna.ui.components.playsheets.datamakers.DataMakerComponent;
 import prerna.ui.components.playsheets.datamakers.FilterTransformation;
 import prerna.ui.components.playsheets.datamakers.ISEMOSSTransformation;
-import prerna.ui.components.playsheets.datamakers.JoinTransformation;
 import prerna.util.Utility;
 import prerna.web.services.util.WebUtility;
 
@@ -92,13 +91,8 @@ public class QuestionAdmin {
 		try {
 			Insight insight = InsightStore.getInstance().get(insightID);
 			List<DataMakerComponent> dmcList = insight.getDataMakerComponents();
-			List<SEMOSSParam> params = null;
 			Vector<Map<String, String>> paramMapList = gson.fromJson(form.getFirst("parameterQueryList"), new TypeToken<Vector<Map<String, String>>>() {}.getType());
-			if(paramMapList != null && !paramMapList.isEmpty()) {
-				params = new Vector<SEMOSSParam>();
-				appendParametersToMetaModel(paramMapList, dmcList, params);
-			}
-			fillInSelectedParams(params, dmcList, insight.getParamHash());
+			List<SEMOSSParam> params = buildParameterList(paramMapList);
 			questionAdmin.addQuestion(insightName, perspective, dmcList, layout, order, insight.getDataMakerName(), isDbQuery, dataTableAlign, params);
 		}catch(RuntimeException e){
 			System.out.println("caught exception while adding question.................");
@@ -128,14 +122,8 @@ public class QuestionAdmin {
 		QuestionAdministrator questionAdmin = new QuestionAdministrator(this.coreEngine);
 		try{
 			Insight insight = InsightStore.getInstance().get(insightID);
-			List<DataMakerComponent> dmcList = insight.getDataMakerComponents();
-			List<SEMOSSParam> params = null;
 			Vector<Map<String, String>> paramMapList = gson.fromJson(form.getFirst("parameterQueryList"), new TypeToken<Vector<Map<String, String>>>() {}.getType());
-			if(paramMapList != null && !paramMapList.isEmpty()) {
-				params = new Vector<SEMOSSParam>();
-				appendParametersToMetaModel(paramMapList, dmcList, params);
-			}
-			fillInSelectedParams(params, dmcList, insight.getParamHash());
+			List<SEMOSSParam> params = buildParameterList(paramMapList);
 			questionAdmin.modifyQuestion(insight.getRdbmsId(), insightName, perspective, insight.getDataMakerComponents(), layout, order, insight.getDataMakerName(), isDbQuery, dataTableAlign, params);
 		}catch(RuntimeException e){
 			System.out.println("caught exception while modifying question.................");
@@ -144,53 +132,6 @@ public class QuestionAdmin {
 		}
 
 		return Response.status(200).entity(WebUtility.getSO("Success")).build();
-	}
-	
-	/**
-	 * Fills in the query for selected parameters
-	 * Example: if a user creates a question that takes in a parameter and wants to save the visualization created (with the selected parameter)
-	 * 			We need to fill in the metamodel data/query with the selected instance to re-create it properly
-	 * @param params					The SEMOSSParams that will stay as parameters for recreation of the insight
-	 * @param dmcList					The list of DataMakerComponents which contain the filters
-	 * @param paramHash					The selected parameters to get to the viz
-	 */
-	private void fillInSelectedParams(List<SEMOSSParam> params, List<DataMakerComponent> dmcList, Map<String, List<Object>> paramHash) {
-		if(paramHash != null) {
-			for(String selectedParamName : paramHash.keySet()) {
-				boolean stillParam = false;
-				// loop to make sure that the list of parameters that the user selected isn't still a parameter
-				// user can select a parameter value and re-save the visualization with that column still as a parameter
-				if(params != null) {
-					STILL_PARAMS : for(SEMOSSParam p : params) {
-						if(p.getName().equals(selectedParamName)) {
-							stillParam = true;
-							break STILL_PARAMS;
-						}
-					}
-				}
-				if(!stillParam) {
-					for(DataMakerComponent dmc : dmcList) {
-						Map<String, Object> mm = dmc.getMetamodelData();
-						// if there is no metamodel data, fill in the query
-						if(mm == null || mm.isEmpty()) {
-							String query = dmc.getQuery();
-							query = Utility.normalizeParam(query);
-							query = Utility.fillParam(query, paramHash);
-							dmc.setQuery(query);
-						} else {
-							// fill in the FilterTransformation with the selected values
-							List<ISEMOSSTransformation> preTrans = dmc.getPreTrans();
-							for(ISEMOSSTransformation trans : preTrans) {
-								if(trans.getProperties().get(FilterTransformation.COLUMN_HEADER_KEY).equals(selectedParamName)) {
-									trans.getProperties().put(FilterTransformation.VALUES_KEY, paramHash.get(selectedParamName));
-									break;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
 	}
 
 	@POST
@@ -259,146 +200,6 @@ public class QuestionAdmin {
 		return Response.status(200).entity(WebUtility.getSO("Success")).build();
 	}
 	
-	/**
-	 * This method appends the parameter options to the DataMakerComponent metamodel
-	 * @param paramMapList				The list of parameters to save.  Comes as a map with the URI and the parent if it is a property
-	 * @param dmcList					The list of the DataMakerComponents for the insight
-	 * @param params					A list of SEMOSSParams to store the parameters with the correct options
-	 */
-	private void appendParametersToMetaModel(Vector<Map<String, String>> paramMapList, List<DataMakerComponent> dmcList, List<SEMOSSParam> params) {
-		if(paramMapList != null && !paramMapList.isEmpty()) {
-			for(Map<String, String> paramMap : paramMapList) {
-				String paramURI = paramMap.get("value");
-				String paramParent = paramMap.get("parent");
-
-				SEMOSSParam p = new SEMOSSParam();
-				String paramName = Utility.getInstanceName(paramURI);
-				String varKey = "";
-				// need to add parameters to the metamodel data
-				// we decide which DataMakerComponent based on order
-				int compNum = 0;
-				FIND_DMC : for(; compNum < dmcList.size(); compNum++) {
-					DataMakerComponent dmc = dmcList.get(compNum);
-					Map<String, Object> metamodel = dmc.getMetamodelData();
-					Map<String, Object> queryData = (Map<String, Object>) metamodel.get("QueryData");
-					List<List<Object>> relTriples = (List<List<Object>>) queryData.get("relTriples");
-					List<Map<String, Object>> nodeProps = (List<Map<String, Object>>) metamodel.get("SelectedNodeProps");
-					boolean containsParam =  false;
-					if(relTriples != null) {
-						FIND_URI : for(int j = 0; j < relTriples.size(); j++) {
-							List<Object> triple = relTriples.get(j);
-							for(Object uri : triple) {
-								if(uri.equals(paramURI)) {
-									containsParam = true;
-									break FIND_URI;
-								}
-							}
-						}
-					}
-					if(nodeProps != null && !containsParam) {
-						FIND_NODE_URI : for(int j = 0; j < nodeProps.size(); j++) {
-							Object uri = nodeProps.get(j).get("uriKey");
-							if(uri != null && uri.equals(paramURI)) {
-								containsParam = true;
-								varKey = (String) nodeProps.get(j).get("varKey");
-								break FIND_NODE_URI;
-							}
-						}
-					}
-
-					if(containsParam) {
-						Map<String, String> paramHash = new Hashtable<String, String>();
-						//TODO: Bifurcation in processing logic if it is RDBMS vs. RDF
-						//Due to the fact that we do not store the parameters as metamodels but as queries
-						if(this.coreEngine.getEngineType() == ENGINE_TYPE.RDBMS) {
-							// rdbms will be stored as type queries if it is a "concept" or property
-							// in both situaitons will go through the getEntityOfType query
-							if(paramParent != null) {
-								String tableName = Utility.getInstanceName(paramParent);
-								paramHash.put(paramName, tableName + ":" + paramName);
-								p.setType(paramName + "-" + tableName + ":" + paramName);
-								p.setName(tableName + "__" + paramName);
-							} else {
-								paramHash.put(paramName, paramName + ":" + paramName);
-								p.setType(paramName);
-								p.setName(paramName);
-							}
-						} else {
-							if(!varKey.isEmpty()) {
-								paramName = varKey;
-							}
-							p.setName(paramName);
-							if(paramParent != null) {
-								// if it is a property, we need to define a unique query which pulls up values for the property based on the parent
-								paramHash.put(paramName, paramURI);
-								String query = "SELECT DISTINCT ?entity WHERE { {?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <" + paramParent + "> } "
-										+ "{ ?x <" + paramURI + "> ?entity} }";
-								p.setQuery(query);
-							} else {
-								// if it is a concept, just run getEntityOfType query with the concepts URI
-								paramHash.put(paramName, paramURI);
-								p.setType(paramURI);
-							}
-						}
-						//TODO: will be linking the parameter based on the dmc component and will modify with a preTransformation
-//						queryData.put("Parameters", paramHash);
-						// need to adjust the join to now be an inner join if it is not
-						// make this assumption since the user is defining what specific param value they want
-						adjustJoinComponentForParameter(dmc);
-						appendPreTransformationForParamFilter(dmc, p.getName());
-						p.setComponentFilterId(Insight.COMP + compNum + ":" + Insight.PRE_TRANS + (dmc.getPreTrans().size()-1));
-						break FIND_DMC;
-					}
-				}
-				params.add(p);
-				
-				// need to loop through and delete any filters on this parameter that might have occurred in later post transformations
-				for(; compNum < dmcList.size(); compNum++) {
-					DataMakerComponent dmc = dmcList.get(compNum);
-					Iterator<ISEMOSSTransformation> postTransIt = dmc.getPostTrans().iterator();
-					while(postTransIt.hasNext()) {
-						ISEMOSSTransformation trans = postTransIt.next();
-						if(trans instanceof FilterTransformation) {
-							if(trans.getProperties().get(FilterTransformation.COLUMN_HEADER_KEY).equals(paramName)) {
-								postTransIt.remove();
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	private void appendPreTransformationForParamFilter(DataMakerComponent dmc, String paramName) {
-		List<ISEMOSSTransformation> preTrans = dmc.getPreTrans();
-		if(preTrans == null) {
-			preTrans = new Vector<ISEMOSSTransformation>();
-		}
-		ISEMOSSTransformation pFilter = new FilterTransformation();
-		Map<String, Object> props = new Hashtable<String, Object>();
-		props.put(FilterTransformation.COLUMN_HEADER_KEY, paramName);
-		pFilter.setProperties(props);
-		preTrans.add(pFilter);
-		dmc.setPreTrans(preTrans);
-	}
-
-	/**
-	 * Modifies the first post transformation of a DataMakerComponent to be an inner join
-	 * @param dmc				The DataMakerComponent to modify the Join Transformation
-	 */
-	private void adjustJoinComponentForParameter(DataMakerComponent dmc) {
-		List<ISEMOSSTransformation> postTrans = dmc.getPostTrans();
-		// makes the assumption that the join is the first post transformation on the dmc
-		if(postTrans != null && !postTrans.isEmpty()) {
-			ISEMOSSTransformation trans = postTrans.get(0);
-			if(trans instanceof JoinTransformation) {
-				Map<String, Object> props = trans.getProperties();
-				props.put(JoinTransformation.JOIN_TYPE, "inner");
-				trans.setProperties(props);
-			}
-		}
-	}
-	
 	private List<SEMOSSParam> generateSEMOSSParamObjects(
 			Vector<String> parameterDependList, 
 			Vector<String> parameterQueryList, 
@@ -440,6 +241,54 @@ public class QuestionAdmin {
 			params.add(param);
 		}
 		
+		return params;
+	}
+
+	/**
+	 * This method appends the parameter options to the DataMakerComponent metamodel
+	 * @param paramMapList				The list of parameters to save.  Comes as a map with the URI and the parent if it is a property
+	 * @param dmcList					The list of the DataMakerComponents for the insight
+	 * @param params					A list of SEMOSSParams to store the parameters with the correct options
+	 */
+	private List<SEMOSSParam> buildParameterList(Vector<Map<String, String>> paramMapList) {
+		List<SEMOSSParam> params = new Vector<SEMOSSParam>();
+		if(paramMapList != null && !paramMapList.isEmpty()) {
+			for(Map<String, String> paramMap : paramMapList) {
+				String paramURI = paramMap.get("value");
+				String paramParent = paramMap.get("parent");
+				String paramName = paramMap.get("name");
+
+				SEMOSSParam p = new SEMOSSParam();
+				//TODO: Bifurcation in processing logic if it is RDBMS vs. RDF
+				//Due to the fact that we do not store the parameters as metamodels but as queries
+				if(this.coreEngine.getEngineType() == ENGINE_TYPE.RDBMS) {
+					// rdbms will be stored as type queries if it is a "concept" or property
+					// in both situaitons will go through the getEntityOfType query
+					if(paramParent != null) {
+						String tableName = Utility.getInstanceName(paramParent);
+						p.setType(paramName + "-" + tableName + ":" + paramName);
+						p.setName(tableName + "__" + paramName);
+					} else {
+						p.setType(paramName);
+						p.setName(paramName);
+					}
+				} else {
+					p.setName(paramName);
+					if(paramParent != null) {
+						// if it is a property, we need to define a unique query which pulls up values for the property based on the parent
+						String query = "SELECT DISTINCT ?entity WHERE { {?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <" + paramParent + "> } "
+								+ "{ ?x <" + paramURI + "> ?entity} }";
+						p.setQuery(query);
+					} else {
+						// if it is a concept, just run getEntityOfType query with the concepts URI
+						p.setType(paramURI);
+					}
+				}
+				params.add(p);
+				//Param still needs to have id set once we know what component it goes on
+				//this will happen in question administrator
+			}
+		}
 		return params;
 	}
 
