e42ea686feff981ab1cd91834e26b3bf08994db9
Maher Khalil <themaherkhalil@gmail.com>
2015-11-12 17:36:34 +0000

    fix for rdbms inserting via form builder
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@11130 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 00f3cc7a140fcc44d398b10bf9216fa3d94cb992

--- src/prerna/semoss/web/form/FormBuilder.java
+++ src/prerna/semoss/web/form/FormBuilder.java
@@ -7,15 +7,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -26,11 +22,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import prerna.engine.api.IEngine;
-import prerna.engine.api.ISelectWrapper;
-import prerna.engine.impl.AbstractEngine;
-import prerna.engine.impl.rdbms.RDBMSNativeEngine;
-import prerna.engine.impl.rdf.RDFFileSesameEngine;
-import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
@@ -275,20 +266,17 @@ public final class FormBuilder {
 		for(int j = 0; j < nodes.size(); j++) {
 			Map<String, Object> node = nodes.get(j);
 
+			// concept name passed to FE from metamodel so it comes back as a URI
 			tableName = node.get("conceptName").toString();
-			//			tableColumn = node.get("columnName").toString();
+			tableName = Utility.getInstanceName(tableName);
 			tableValue = node.get("conceptValue").toString();
-
-			//			HashMap<String, String> innerMap = new HashMap<String, String>();
-			//			innerMap.put(tableColumn, tableValue);
-			//			nodeMapping.put(tableName, innerMap);
-
-			Map<String, Object> templateOptions = (Map<String, Object>)node.get("templateOptions");
-			tableColumn = templateOptions.get("columnName").toString();
-			tableName = tableColumn;
-			List<HashMap<String, Object>> properties = (List<HashMap<String, Object>>)templateOptions.get("properties");
-
-			HashMap<String, String> innerMap = new HashMap<String, String>();
+			
+			//TODO: tableColumn will be the primary key of the table... need additional code to get this
+			//for now, assume tableColumn is tableName
+			tableColumn = tableName;
+			
+			List<Map<String, Object>> properties = (List<Map<String, Object>>)node.get("properties");
+			Map<String, String> innerMap = new HashMap<String, String>();
 			innerMap.put(tableColumn, tableValue);
 			nodeMapping.put(tableName, innerMap);
 
@@ -304,36 +292,35 @@ public final class FormBuilder {
 			insertQuery.append("INSERT INTO ");
 			insertQuery.append(tableName.toUpperCase());
 			insertQuery.append(" (");
-			insertQuery.append(tableColumn);
-			for(int i = 0; i < propTypes.size(); i++) {
-				insertQuery.append(propTypes.get(i).toUpperCase());
-				if(i != propTypes.size() - 1) {
-					insertQuery.append(",");
+			insertQuery.append(tableColumn.toUpperCase());
+			if(propTypes.size() > 0) {
+				insertQuery.append(",");
+				for(int i = 0; i < propTypes.size(); i++) {
+					insertQuery.append(propTypes.get(i).toUpperCase());
+					if(i != propTypes.size() - 1) {
+						insertQuery.append(",");
+					}
 				}
 			}
 
-			String dataType = checkColumnDataType(engine, tableColumn);
-			boolean useQuotes = useQuotes(dataType);
-			if(useQuotes) {
-				insertQuery.append(") VALUES ('");
-				insertQuery.append(tableValue);
-				insertQuery.append("'");
-			} else {
-				insertQuery.append(") VALUES (");
-				insertQuery.append(tableValue);
-			}
+			insertQuery.append(") VALUES ('");
+			insertQuery.append(tableValue);
+			insertQuery.append("'");
 
-			for(int i = 0; i < propValues.size(); i++) {
-				Object propertyValue = propValues.get(i);
-				if(propertyValue instanceof String) {
-					insertQuery.append("'");
-					insertQuery.append(propertyValue.toString().toUpperCase());
-					insertQuery.append("'");
-				} else {
-					insertQuery.append(propertyValue);
-				}
-				if(i != propTypes.size() - 1) {
-					insertQuery.append(", ");
+			if(propTypes.size() > 0) {
+				insertQuery.append(",");
+				for(int i = 0; i < propValues.size(); i++) {
+					Object propertyValue = propValues.get(i);
+					if(propertyValue instanceof String) {
+						insertQuery.append("'");
+						insertQuery.append(propertyValue.toString().toUpperCase());
+						insertQuery.append("'");
+					} else {
+						insertQuery.append(propertyValue);
+					}
+					if(i != propTypes.size() - 1) {
+						insertQuery.append(", ");
+					}
 				}
 			}
 			insertQuery.append(");");
@@ -342,144 +329,71 @@ public final class FormBuilder {
 		}
 
 		String startTable;
+		String startVal;
 		String startCol;
 		String endTable;
 		String endCol;
+		String endVal;
 		String _FK = "_FK";
 
-		if(engine instanceof AbstractEngine) {
-			// determine existing FKs in schema
-			Map<String, Set<String>> tableFKs = new HashMap<String, Set<String>>();
-			if(relationships != null && !(relationships.size()==0)) {
-				String relQuery = "SELECT DISTINCT ?x WHERE {?x <http://www.w3.org/2000/01/rdf-schema#subPropertyOf> <http://semoss.org/ontologies/Relation>}";
-				RDFFileSesameEngine baseEngine = ((AbstractEngine) engine).getBaseDataEngine();
-				ISelectWrapper wrapper = WrapperManager.getInstance().getSWrapper(baseEngine, relQuery);
-				String[] names = wrapper.getVariables();
-				while(wrapper.hasNext()) {
-					String relURI = wrapper.next().getRawVar(names[0]).toString();
-					String rel = Utility.getClassName(relURI);
-					String[] relVals = rel.split(".");
-					for(int i = 0; i < relVals.length; i++) {
-						if(relVals[1].endsWith(_FK)) {
-							Set<String> fks;
-							if(tableFKs.containsKey(relVals[0])) {
-								fks = tableFKs.get(relVals[0]);
-								fks.add(relVals[1]);
-							} else {
-								fks = new HashSet<String>();
-								fks.add(relVals[1]);
-								tableFKs.put(relVals[0], fks);
-							}
-						}
-						if(relVals[3].endsWith(_FK)) {
-							Set<String> fks;
-							if(tableFKs.containsKey(relVals[2])) {
-								fks = tableFKs.get(relVals[2]);
-								fks.add(relVals[3]);
-							} else {
-								fks = new HashSet<String>();
-								fks.add(relVals[3]);
-								tableFKs.put(relVals[2], fks);
-							}
-						}
-					}
+		for(int r = 0; r < relationships.size(); r++) {
+			Map<String, Object> relationship =  relationships.get(r);
+
+			startTable = relationship.get("startNodeType").toString();
+			startTable = Utility.getInstanceName(startTable);
+			//TODO: tableColumn will be the primary key of the table... need additional code to get this
+			//for now, assume tableColumn is tableName
+			startCol = startTable;
+			startVal = relationship.get("startNodeVal").toString();
+			
+			endTable = relationship.get("endNodeType").toString();
+			endTable = Utility.getInstanceName(endTable);
+			//TODO: tableColumn will be the primary key of the table... need additional code to get this
+			//for now, assume tableColumn is tableName
+			endCol = endTable;
+			endVal = relationship.get("endNodeVal").toString();
+
+			boolean addToStart = false;
+			String[] relVals = relationship.get("relType").toString().split("\\.");
+			if(relVals[1].endsWith(_FK)) {
+				if(relVals[1].equals(startTable + _FK)) {
+					addToStart = true;
+				} 
+			} if(relVals[3].endsWith(_FK)) {
+				if(relVals[1].equals(startTable + _FK)) {
+					addToStart = true;
 				}
 			}
-
-			for(int r = 0; r < relationships.size(); r++) {
-				Map<String, Object> relationship =  relationships.get(r);
-
-				startTable = relationship.get("startNode").toString();
-				startCol = relationship.get("startCol").toString();
-				endTable = relationship.get("endNode").toString();
-				endCol = relationship.get("endCol").toString();
-
-				for(String table : tableFKs.keySet()) {
-					Set<String> fks = tableFKs.get(table);
-					if(fks.contains(startCol + _FK) && fks.contains(endCol + _FK)) {
-						// add to external relationship table
-						StringBuilder insertQuery = new StringBuilder();
-						insertQuery.append("INSERT INTO ");
-						insertQuery.append(table.toUpperCase());
-						insertQuery.append(" (" );
-						insertQuery.append(startCol);
-						insertQuery.append(_FK);
-						insertQuery.append(",");
-						insertQuery.append(endCol);
-						insertQuery.append(_FK);
-						insertQuery.append(") VALUES '");
-						insertQuery.append(nodeMapping.get(startTable).get(startCol));
-						insertQuery.append("', '");
-						insertQuery.append(nodeMapping.get(endTable).get(endCol));
-						insertQuery.append("');");
-
-						engine.insertData(insertQuery.toString());
-					}
-				}
-
-				Set<String> fks = tableFKs.get(startTable);
-				if(fks.contains(endTable + _FK)) {
-					// update record in startTable with FK relationship to endTable
-					StringBuilder updateQuery = new StringBuilder();
-					updateQuery.append("UPDATE ");
-					updateQuery.append(startTable.toUpperCase());
-					updateQuery.append(" SET" );
-					updateQuery.append(endCol + _FK);
-					updateQuery.append("=");
-					updateQuery.append(nodeMapping.get(endTable).get(endCol));
-					updateQuery.append(" WHERE");
-					updateQuery.append(startCol);
-					updateQuery.append("='");
-					updateQuery.append(nodeMapping.get(startTable).get(startCol));
-					updateQuery.append("';");
-
-					engine.insertData(updateQuery.toString());
-				}
-
-				fks = tableFKs.get(endTable);
-				if(fks.contains(startTable + _FK)) {
-					// update record in endTable with FK relationship to startTable
-					StringBuilder updateQuery = new StringBuilder();
-					updateQuery.append("UPDATE ");
-					updateQuery.append(endTable.toUpperCase());
-					updateQuery.append(" SET" );
-					updateQuery.append(startCol + _FK);
-					updateQuery.append("=");
-					updateQuery.append(nodeMapping.get(startTable).get(startCol));
-					updateQuery.append(" WHERE");
-					updateQuery.append(endCol);
-					updateQuery.append("='");
-					updateQuery.append(nodeMapping.get(endTable).get(endCol));
-					updateQuery.append("';");
-
-					engine.insertData(updateQuery.toString());
-				}
+			
+			StringBuilder updateQuery = new StringBuilder();
+			
+			if(addToStart) {
+				updateQuery.append("UPDATE ");
+				updateQuery.append(startTable.toUpperCase());
+				updateQuery.append(" SET " );
+				updateQuery.append(endTable + _FK);
+				updateQuery.append("='");
+				updateQuery.append(endVal);
+				updateQuery.append("' WHERE ");
+				updateQuery.append(startCol);
+				updateQuery.append("='");
+				updateQuery.append(startVal);
+				updateQuery.append("';");
+				
+			} else {
+				updateQuery.append("UPDATE ");
+				updateQuery.append(endTable.toUpperCase());
+				updateQuery.append(" SET " );
+				updateQuery.append(startTable + _FK);
+				updateQuery.append("='");
+				updateQuery.append(startVal);
+				updateQuery.append("' WHERE ");
+				updateQuery.append(endCol);
+				updateQuery.append("='");
+				updateQuery.append(endVal);
+				updateQuery.append("';");
 			}
-		} else {
-			Map<String, String> errorHash = new HashMap<String, String>();
-			errorHash.put("errorMessage", "Base ontologogy (OWL File) not found for engine!");		
+			engine.insertData(updateQuery.toString());
 		}
-	}
-
-
-	private static String checkColumnDataType(IEngine engine, String tableName) {
-		String type = "";
-		try {
-
-			String getColumnTypeQuery = "SELECT * FROM" + tableName + "LIMIT 1";
-			Map<String, Object> map = (Map<String, Object>) engine.execQuery(getColumnTypeQuery);
-			ResultSet rs = (ResultSet) map.get(RDBMSNativeEngine.RESULTSET_OBJECT);
-			ResultSetMetaData rsmd = rs.getMetaData();
-			type = rsmd.getColumnTypeName(1);
-
-		} catch(Exception e) {
-
-		}
-		return type;
-	}
-
-	private static boolean useQuotes(String columnType) {
-		if(columnType.equals("FLOAT")) return false;
-		else return true;
 	}
 }
