c47c29a45297d747eef4d7c197a2336f4dbdcdbe
rluthar <rluthar@deloitte.com>
2015-12-02 16:42:18 +0000

    adding call to get filter model from a btreeDataFrame
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@11403 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/DataframeResource.java
+++ src/prerna/semoss/web/services/DataframeResource.java
@@ -1,17 +1,25 @@
 package prerna.semoss.web.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 
+import com.google.gson.Gson;
+
+import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.BTreeDataFrame;
 import prerna.ds.Probablaster;
 import prerna.om.Insight;
+import prerna.om.InsightStore;
 import prerna.ui.components.playsheets.datamakers.IDataMaker;
 import prerna.web.services.util.WebUtility;
 
@@ -51,5 +59,45 @@ public class DataframeResource {
 		
 		
 		return Response.status(200).entity(WebUtility.getSO(insights)).build();
-	}	
+	}
+	
+	
+	@GET
+	@Path("/getFilterModel")
+	@Produces("application/json")
+	public Response getFilterModel(MultivaluedMap<String, String> form,
+			@Context HttpServletRequest request)
+	{	
+		String insightID = form.getFirst("insightID");
+
+		Insight existingInsight = null;
+		if(insightID != null && !insightID.isEmpty()) {
+			existingInsight = InsightStore.getInstance().get(insightID);
+			if(existingInsight == null) {
+				Map<String, String> errorHash = new HashMap<String, String>();
+				errorHash.put("errorMessage", "Existing insight based on passed insightID is not found");
+				return Response.status(400).entity(WebUtility.getSO(errorHash)).build();
+			}
+		}
+
+		ITableDataFrame mainTree = (ITableDataFrame) existingInsight.getDataMaker();
+		if(mainTree == null) {
+			return Response.status(400).entity(WebUtility.getSO("table not found for insight id. Data not found")).build();
+		}
+		
+		if(!(mainTree instanceof BTreeDataFrame)) {
+			return Response.status(400).entity(WebUtility.getSO("table not instance of BTreeDataFrame, cannot grab filter model")).build();
+		}
+
+		Gson gson = new Gson();
+
+		Map<String, Object> retMap = new HashMap<String, Object>();
+
+		Object[] returnFilterModel = ((BTreeDataFrame)mainTree).getFilterModel();
+		((BTreeDataFrame)mainTree).printTree();
+		retMap.put("unfilteredValues", returnFilterModel[0]);
+		retMap.put("filteredValues", returnFilterModel[1]);
+		
+		return Response.status(200).entity(WebUtility.getSO(retMap)).build();
+	}
 }
