16e4a06cd0c20dae37b73b2f8d2c23c1b2bf6124
karverma <karverma@deloitte.com>
2015-12-08 21:20:42 +0000

    Setting Solr home variable on server start.
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@11532 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 923ddb9c0648aeef7ce852d73fc33c7bcef06293

--- src/prerna/web/conf/DBLoader.java
+++ src/prerna/web/conf/DBLoader.java
@@ -27,15 +27,13 @@
  *******************************************************************************/
 package prerna.web.conf;
 
+import java.io.File;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-import prerna.engine.api.IEngine;
-import prerna.engine.impl.rdf.SesameJenaSelectStatement;
-import prerna.engine.impl.rdf.SesameJenaSelectWrapper;
 import prerna.util.AbstractFileWatcher;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
@@ -45,60 +43,50 @@ import com.ibm.icu.util.StringTokenizer;
 public class DBLoader implements ServletContextListener {
 
 	public static final String RDFMAP = "RDF-MAP";
-	String dbFolder = null;
-	String rdfPropFile = null;
 	public Object monitor = new Object();
-	
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
-		
-
-	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
-		// this would load the DBs from the folders
-		// get the RDF Map prop
-		String workingDir = System.getProperty("user.dir");
+		System.out.println("Initializing application context...");
+		
+		//Set default file separator system variable
+		System.out.println("Changing file separator value to: '/'");
 		System.setProperty("file.separator", "/");
-
-		rdfPropFile = arg0.getServletContext().getInitParameter(RDFMAP);
+		
+		//Load RDF_Map.prop file
+		String rdfPropFile = arg0.getServletContext().getInitParameter(RDFMAP);
+		System.out.println("Loading RDF_Map.prop: " + rdfPropFile);
 		DIHelper.getInstance().loadCoreProp(rdfPropFile);
 		
-		Logger logger = Logger.getLogger(prerna.web.conf.DBLoader.class);
-		//Object monitor = new Object(); // stupid object for being a monitor		
-		//logger.setLevel(Level.INFO);
-		PropertyConfigurator.configure(DIHelper.getInstance().getProperty("LOG4J"));
-		dbFolder = DIHelper.getInstance().getProperty("BaseFolder");
+		//Set log4j prop
+		String log4jConfig = DIHelper.getInstance().getProperty("LOG4J");
+		System.out.println("Setting log4j property: " + log4jConfig);
+		PropertyConfigurator.configure(log4jConfig);
 		
-		// get the engine name
-		//String engines = DIHelper.getInstance().getProperty(Constants.ENGINES);
+		//Set Solr home variable (~SEMOSS_PROJ_HOME/Solr)
+		String solrHome = DIHelper.getInstance().getProperty("BaseFolder") + "/" + Constants.SOLR_HOME_DIR;
+		if((new File(solrHome)).exists()) {
+			System.out.println("Setting Solr home: " + solrHome);
+			System.setProperty("solr.solr.home", solrHome);
+		}
+		
+		//Load empty engine list into DIHelper, then load engines from db folder
+		System.out.println("Loading engines...");
 		String engines = "";
-		
 		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, engines);
-
 		loadEngines();
-		
-		
-		System.out.println("Initializing the context ");
 	}
 	
-	public void loadEngines()
-	{
+	public void loadEngines() {
 		String watcherStr = DIHelper.getInstance().getProperty(Constants.ENGINE_WEB_WATCHER);
 		StringTokenizer watchers = new StringTokenizer(watcherStr, ";");
-		try
-		{		
-			while(watchers.hasMoreElements())
-			{
+		try {		
+			while(watchers.hasMoreElements()) {
 				String watcher = watchers.nextToken();
 				String watcherClass = DIHelper.getInstance().getProperty(watcher);
 				String folder = DIHelper.getInstance().getProperty(watcher + "_DIR");
 				String ext = DIHelper.getInstance().getProperty(watcher + "_EXT");
-				AbstractFileWatcher watcherInstance = (AbstractFileWatcher)Class.forName(watcherClass).getConstructor(null).newInstance(null);
+				AbstractFileWatcher watcherInstance = (AbstractFileWatcher) Class.forName(watcherClass).getConstructor(null).newInstance(null);
 				watcherInstance.setMonitor(monitor);
 				watcherInstance.setFolderToWatch(folder);
 				watcherInstance.setExtension(ext);
@@ -108,65 +96,13 @@ public class DBLoader implements ServletContextListener {
 					thread.start();
 				}
 			}
-		}catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		// get this into a synchronized block
-		// so this guy will wait
-		// I do this so that I can get reference to the engine when I need it
-		/*synchronized(monitor)
-		{
-			watcherStr = DIHelper.getInstance().getProperty(Constants.ENGINE_WEB_WATCHER);
-			if(watcherStr != null )
-			{
-				watchers = new StringTokenizer(watcherStr, ";");
-				while(watchers.hasMoreElements())
-				{
-					String watcher = watchers.nextToken();
-					String watcherClass = DIHelper.getInstance().getProperty(watcher);
-					String folder = DIHelper.getInstance().getProperty(watcher + "_DIR");
-					String ext = DIHelper.getInstance().getProperty(watcher + "_EXT");
-					String engineName = DIHelper.getInstance().getProperty(watcher+"_ENGINE");
-					try
-					{
-						AbstractFileWatcher watcherInstance = (AbstractFileWatcher)Class.forName(watcherClass).getConstructor(null).newInstance(null);
-						// engines should be loaded by now
-						// hopefully :D
-						if(engineName != null && DIHelper.getInstance().getLocalProp(engineName) != null)
-						{
-							IEngine engine = (IEngine)DIHelper.getInstance().getLocalProp(engineName);
-							watcherInstance.setEngine(engine);
-						}
-						watcherInstance.setMonitor(monitor);
-						watcherInstance.setFolderToWatch(folder);
-						watcherInstance.setExtension(ext);
-						watcherInstance.loadFirst();
-						Thread thread = new Thread(watcherInstance);
-						thread.start();
-					}catch(Exception ex)
-					{
-						// ok dont do anything the file was not there
-					}
-				}
-			}
-		}*/
-
 	}
 	
-	public void testQuery(IEngine engine)
-	{
-		SesameJenaSelectWrapper sjsw = new SesameJenaSelectWrapper();
-		sjsw.setEngine(engine);
-		sjsw.setQuery("Select ?a ?b ?c where {?a ?b ?c} limit 2");
-		sjsw.executeQuery();
-		sjsw.getVariables();
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
 		
-		while(sjsw.hasNext())
-		{
-			SesameJenaSelectStatement stmt = sjsw.next();
-			System.out.println(stmt.getPropHash());
-		}
 	}
-
 }
