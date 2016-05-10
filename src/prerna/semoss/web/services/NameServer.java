4d91dd40e85cc35626e0c518be8f6c6c41a119e9
kepark <kepark@deloitte.com>
2016-05-10 21:56:42 +0000

    Reverting and removing loops.
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@14661 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/NameServer.java
+++ src/prerna/semoss/web/services/NameServer.java
@@ -674,9 +674,9 @@ public class NameServer {
 			String conceptURI = keywordURI.replace("Keyword/", "");
 			
 			IEngine eng = (IEngine) DIHelper.getInstance().getLocalProp(engine);
-			String returnURI = eng.getTransformedNodeName(conceptURI, true); // returns DisplayName uri
+			String returnURI = eng.getTransformedNodeName(conceptURI, true);
 			
-			String instanceName = conceptURI.replaceAll(".*/Concept/", "");
+			String instanceName = conceptURI.replaceAll(".*/Concept/", "");;
 			
 			Map<String, Map<String, String>> conceptSet = null;
 			if(retMap.containsKey(engine)) {
@@ -699,12 +699,11 @@ public class NameServer {
 			}
 			Map<String, String> nodeMap = new Hashtable<String, String>();
 			nodeMap.put("physicalName", instanceName);
-			nodeMap.put("displayName", returnURI);
 			if(parent != null) {
 				nodeMap.put("parent", parent);
 			}
 			
-			conceptSet.put(instanceName, nodeMap);
+			conceptSet.put(returnURI, nodeMap);
 			retMap.put(engine, conceptSet);
 		}
 		
