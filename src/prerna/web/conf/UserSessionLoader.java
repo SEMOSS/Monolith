fb7779827ae4a22e96b596b2e258e646f7d11ef7
Maher Khalil <themaherkhalil@gmail.com>
2017-03-07 03:53:33 +0000

    UPDATE ::: Adding logic to not drop read only insights and to keep them in insight store to return the data directly without rerunning them
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@19087 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cb9b922c73fa8293b2a452699a5c295ea244f20c

--- src/prerna/web/conf/UserSessionLoader.java
+++ src/prerna/web/conf/UserSessionLoader.java
@@ -1,14 +1,17 @@
 package prerna.web.conf;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import prerna.auth.User;
 import prerna.auth.User.LOGIN_TYPES;
+import prerna.auth.UserPermissionsMasterDB;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.r.RDataTable;
 import prerna.om.Dashboard;
@@ -22,13 +25,10 @@ import prerna.util.Constants;
 public class UserSessionLoader implements HttpSessionListener {
 	
 	public void sessionCreated(HttpSessionEvent sessionEvent) {
-//		System.out.println("Session Created: ID="+sessionEvent.getSession().getId());
 		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User(Constants.ANONYMOUS_USER_ID, "Anonymous", LOGIN_TYPES.anonymous, "Anonymous"));
 	}
 	 
 	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
-//		System.out.println("Session Destroyed: ID="+sessionEvent.getSession().getId());
-//		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User("1", "Anonymous", LOGIN_TYPES.anonymous, "Anonymous"));//
 		String sessionID = sessionEvent.getSession().getId();
 		
 		// clear up insight store
@@ -38,53 +38,61 @@ public class UserSessionLoader implements HttpSessionListener {
 			Set<String> copy = new HashSet<String>(insightIDs);
 			for(String id : copy) {
 				Insight in = inStore.get(id);
-				IDataMaker dm = in.getDataMaker();
-				if(dm instanceof H2Frame) {
-					H2Frame frame = (H2Frame)dm;
-					frame.closeRRunner();
-					frame.dropTable();
-					if(!frame.isInMem()) {
-						frame.dropOnDiskTemporalSchema();
-					}
-				} else if(dm instanceof RDataTable) {
-					RDataTable frame = (RDataTable)dm;
-					frame.closeConnection();
-				} else if(dm instanceof Dashboard) {
-					Dashboard dashboard = (Dashboard)dm;
-					dashboard.dropDashboard();
-				}
-				// also see if other variables in runner that need to be dropped
-				PKQLRunner runner = in.getPKQLRunner();
-				runner.cleanUp();
 				
-				inStore.remove(id);
-				inStore.removeFromSessionHash(sessionID, id);
+				// adding logic
+				// if insight is read only for user -1
+				// we will not remove it from memory
+				// and just keep it for other people to look at
+				// the output call will recognize these insights
+				// and just set those for the user on output call
+				
+				boolean remove = true;
+				String inEngine = in.getEngineName();
+				String inRdbmsId = in.getRdbmsId();
+				
+				if(inEngine != null && inRdbmsId != null) {
+					HttpSession session = sessionEvent.getSession();
+					User user = ((User) session.getAttribute(Constants.SESSION_USER));
+					String userId = "";
+					if(user!= null) {
+						userId = user.getId();
+					}
+					
+					UserPermissionsMasterDB permissions = new UserPermissionsMasterDB();
+					List<String[]> readInsights = permissions.getUserReadOnlyInsights(userId);
+					READ_INSIGHTS_LOOP : for(String[] engineIdCombo : readInsights) {
+						if(engineIdCombo[0].equals(inEngine) && engineIdCombo[1].equals(inRdbmsId)) {
+							remove = false;
+							break READ_INSIGHTS_LOOP;
+						}
+					}
+				}
+				
+				if(remove) {
+					IDataMaker dm = in.getDataMaker();
+					if(dm instanceof H2Frame) {
+						H2Frame frame = (H2Frame)dm;
+						frame.closeRRunner();
+						frame.dropTable();
+						if(!frame.isInMem()) {
+							frame.dropOnDiskTemporalSchema();
+						}
+					} else if(dm instanceof RDataTable) {
+						RDataTable frame = (RDataTable)dm;
+						frame.closeConnection();
+					} else if(dm instanceof Dashboard) {
+						Dashboard dashboard = (Dashboard)dm;
+						dashboard.dropDashboard();
+					}
+					// also see if other variables in runner that need to be dropped
+					PKQLRunner runner = in.getPKQLRunner();
+					runner.cleanUp();
+					
+					inStore.remove(id);
+					inStore.removeFromSessionHash(sessionID, id);
+				}
 			}
 			System.out.println("successfully removed qStore information for session");
 		}
-		
-//		// clear up question store
-//		QuestionPlaySheetStore qStore = QuestionPlaySheetStore.getInstance();
-//		Set<String> quesitonIDsToDelete = qStore.getPlaySheetIDsForSession(sessionID);
-//		if(quesitonIDsToDelete != null) {
-//			Set<String> copy = new HashSet<String>(quesitonIDsToDelete);
-//			for(String questionID : copy) {
-//				qStore.remove(questionID);
-//				qStore.removeFromSessionHash(sessionID, questionID);
-//			}
-//			System.out.println("successfully removed qStore information for session");
-//		}
-//		
-//		// clear up ITable store
-//		TableDataFrameStore tableStore = TableDataFrameStore.getInstance();
-//		Set<String> tableIDsToDelete = tableStore.getTableIDsForSession(sessionID);
-//		if(tableIDsToDelete != null) {
-//			Set<String> copy = new HashSet<String>(tableIDsToDelete);
-//			for(String tableID : copy) {
-//				tableStore.remove(tableID);
-//				tableStore.removeFromSessionHash(sessionID, tableID);
-//			}
-//			System.out.println("successfully removed tableStore information for session");
-//		}
 	}
 }
