a016c43993690c999548a9826474af9d45a747e2
rluthar <rluthar@deloitte.com>
2017-03-28 13:34:36 +0000

    UPDATE ::: WebUtility, adding parameter to take in response type for response, NameServer - removing legacy caching responsibility, DataframeResource - using new WebUtility method to get Response
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@19561 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2606db4b26c46a439478a95d528c57ea48315593

--- src/prerna/web/services/util/WebUtility.java
+++ src/prerna/web/services/util/WebUtility.java
@@ -81,7 +81,7 @@ public class WebUtility {
 		return null;
 	}
 	
-	public static Response getResponse(Object vec) {
+	public static Response getResponse(Object vec, int status) {
 		if(vec != null) {
 			Gson gson = getDefaultGson();
 			try {
