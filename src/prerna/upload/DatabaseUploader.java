f1b7960047220597e23ecc91c467e8f7e83c5c1e
mahkhalil <mahkhalil@deloitte.com>
2018-05-01 14:00:05 +0000

    UPDATE ::: Splitting delimiter by ";"
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@27758 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/upload/DatabaseUploader.java
+++ src/prerna/upload/DatabaseUploader.java
@@ -396,7 +396,7 @@ public class DatabaseUploader extends Uploader {
 			
 			// get the files
 			String[] files = inputData.get("file").split(";");
-			String[] delimiters = inputData.get("delimiter").split("\\+\\+\\+");
+			String[] delimiters = inputData.get("delimiter").split(";");
 			// get the property files if necessary
 			String props = inputData.get("propFile");
 			String[] propFiles = null;
