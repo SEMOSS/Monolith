d997634504746e701a46b7d11541ffcb763a9178
mahkhalil <mahkhalil@deloitte.com>
2018-05-30 23:13:02 +0000

    BUGFIX ::: Fixing errors with capturing the CAC user
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@28326 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 742dc9d214227b9688eb11dba3f44f75012ed552

--- src/prerna/web/conf/CACFilter.java
+++ src/prerna/web/conf/CACFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import prerna.auth.AccessToken;
@@ -24,40 +25,60 @@ public class CACFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
 		X509Certificate[] certs = (X509Certificate[]) arg0.getAttribute("javax.servlet.request.X509Certificate");
-		HttpSession session = ((HttpServletRequest)arg0).getSession(false);
+		HttpSession session = ((HttpServletRequest)arg0).getSession(true);
 
-		if(session != null && certs != null) {
+		if(certs != null) {
 			User2 user = (User2) session.getAttribute("semoss_user");
 			if(user == null) {
 				user = new User2();
-			}
-			
-			AccessToken token = new AccessToken();
 
-			for(int i = 0; i < certs.length; i++) {
-				X509Certificate cert = certs[i];
+				AccessToken token = new AccessToken();
+				token.setProvider(AuthProvider.CAC.toString());
+				for(int i = 0; i < certs.length; i++) {
+					X509Certificate cert = certs[i];
 
-				token.setToken_type(AuthProvider.CAC.toString());
-				token.setProvider(cert.getIssuerDN().getName());
-				token.setExpires_in((int) cert.getNotAfter().getTime());
-				
-				String fullName = cert.getSubjectX500Principal().getName();
-				LdapName ldapDN;
-				try {
-					ldapDN = new LdapName(fullName);
-					for(Rdn rdn: ldapDN.getRdns()) {
-						if(rdn.getType().equals("CN")) {
-							token.setName(rdn.getValue().toString());
+					String fullName = cert.getSubjectX500Principal().getName();
+					LdapName ldapDN;
+					try {
+						ldapDN = new LdapName(fullName);
+						for(Rdn rdn: ldapDN.getRdns()) {
+							if(rdn.getType().equals("CN")) {
+								String value = rdn.getValue().toString();
+								String[] split = value.split("\\.");
+								if(split.length == 3 || split.length == 4) {
+									// no idea if middle name is always there or not
+									// just gonna validate the cac has length 10
+									String cacId = split[split.length-1];
+									if(cacId.length() >= 10) {
+										// valid CAC!!!
+										token.setName(value);
+										// now set the other properties
+										token.setToken_type(cert.getIssuerDN().getName());
+										token.setExpires_in((int) cert.getNotAfter().getTime());
+									} else {
+										continue;
+									}
+								} else {
+									continue;
+								}
+							}
 						}
+					} catch (InvalidNameException e) {
+						token.setName(fullName);
+						e.printStackTrace();
 					}
-				} catch (InvalidNameException e) {
-					token.setName(fullName);
-					e.printStackTrace();
+				}
+
+				if(token.getName() != null) {
+					user.setAccessToken(token);
+					session.setAttribute("semoss_user", user);
 				}
 			}
-			
-			user.setAccessToken(token);
-			session.setAttribute("semoss-user", user);
+		} else {
+			//TOOD: figure out redirect
+//			HttpServletResponse httpResponse = (HttpServletResponse) arg1;
+//			httpResponse.sendRedirect( ((HttpServletRequest)arg0).getHeader("Referer") + "TestRedirect/test.html");
+//			return;
 		}
 
 		arg2.doFilter(arg0, arg1);
