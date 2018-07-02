b25084b48f31d623bc972dd5972e8930cdca3d53
mahkhalil <mahkhalil@deloitte.com>
2018-07-02 15:13:37 +0000

    UPDATE ::: Fixing /api/engine/all get request as it is used extensively for testing UPDATE ::: Robust removal of insight upon session closing
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@29063 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/UserSessionLoader.java
+++ src/prerna/web/conf/UserSessionLoader.java
@@ -1,92 +1,113 @@
 package prerna.web.conf;
 
+import java.io.File;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-//import prerna.auth.User;
-//import prerna.auth.User.LOGIN_TYPES;
-import prerna.auth.UserPermissionsMasterDB;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.r.RDataTable;
 import prerna.om.Insight;
 import prerna.om.InsightStore;
-import prerna.sablecc.PKQLRunner;
-import prerna.ui.components.playsheets.datamakers.IDataMaker;
-import prerna.util.Constants;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.VarStore;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.om.task.TaskStore;
+import prerna.sablecc2.reactor.imports.FileMeta;
 
 @WebListener
 public class UserSessionLoader implements HttpSessionListener {
 	
+	protected static final Logger LOGGER = LogManager.getLogger(UserSessionLoader.class.getName());
+
 	public void sessionCreated(HttpSessionEvent sessionEvent) {
-//		sessionEvent.getSession().setAttribute(Constants.SESSION_USER, new User(Constants.ANONYMOUS_USER_ID, "Anonymous", LOGIN_TYPES.ANONYMOUS, "Anonymous"));
+
 	}
-	 
+	
 	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
-		String sessionID = sessionEvent.getSession().getId();
+		String sessionId = sessionEvent.getSession().getId();
 		
 		// clear up insight store
 		InsightStore inStore = InsightStore.getInstance();
-		Set<String> insightIDs = inStore.getInsightIDsForSession(sessionID);
+		Set<String> insightIDs = inStore.getInsightIDsForSession(sessionId);
 		if(insightIDs != null) {
 			Set<String> copy = new HashSet<String>(insightIDs);
-			for(String id : copy) {
-				Insight in = inStore.get(id);
-				if(in == null) {
-					continue;
-				}
-				// adding logic
-				// if insight is read only for user -1
-				// we will not remove it from memory
-				// and just keep it for other people to look at
-				// the output call will recognize these insights
-				// and just set those for the user on output call
+			for(String insightId : copy) {
+				Insight insight = InsightStore.getInstance().get(insightId);
+
+				LOGGER.info("Trying to drop insight " + insightId);
 				
-				boolean isReadOnlyInsight = false;
-				String inEngine = in.getEngineName();
-				String inRdbmsId = in.getRdbmsId();
+				InsightStore.getInstance().remove(insightId);
+				LOGGER.info("Successfully removed insight from insight store");
+
+				// drop all the tasks that are currently running
+				TaskStore taskStore = insight.getTaskStore();
+				taskStore.clearAllTasks();
+				LOGGER.info("Successfully cleared all stored Tasks for the insight");
 				
-//				if(inEngine != null && inRdbmsId != null) {
-//					HttpSession session = sessionEvent.getSession();
-//					User user = ((User) session.getAttribute(Constants.SESSION_USER));
-//					String userId = "";
-//					if(user!= null) {
-//						userId = user.getId();
-//					}
-//					
-//					UserPermissionsMasterDB permissions = new UserPermissionsMasterDB();
-//					isReadOnlyInsight = permissions.isUserReadOnlyInsights(userId, inEngine, inRdbmsId);
-//				}
-//				
-//				if(!isReadOnlyInsight) {
-					IDataMaker dm = in.getDataMaker();
-					if(dm instanceof H2Frame) {
-						H2Frame frame = (H2Frame)dm;
-						frame.dropTable();
-						if(!frame.isInMem()) {
-							frame.dropOnDiskTemporalSchema();
+				// drop all the frame connections
+				VarStore varStore = insight.getVarStore();
+				Set<String> keys = varStore.getKeys();
+				// find all the vars which are frames
+				// and drop them
+				for(String key : keys) {
+					NounMetadata noun = varStore.get(key);
+					PixelDataType nType = noun.getNounType();
+					if(nType == PixelDataType.FRAME) {
+						ITableDataFrame dm = (ITableDataFrame) noun.getValue();
+						dm.setLogger(LOGGER);
+						//TODO: expose a delete on the frame to hide this crap
+						// drop the existing tables/connections if present
+						if(dm instanceof H2Frame) {
+							H2Frame frame = (H2Frame)dm;
+							frame.dropTable();
+							if(!frame.isInMem()) {
+								frame.dropOnDiskTemporalSchema();
+							}
+						} else if(dm instanceof RDataTable) {
+							RDataTable frame = (RDataTable)dm;
+							frame.closeConnection();
 						}
-					} else if(dm instanceof RDataTable) {
-						RDataTable frame = (RDataTable)dm;
-						frame.closeConnection();
-					} 
-//					else if(dm instanceof Dashboard) {
-//						Dashboard dashboard = (Dashboard)dm;
-//						dashboard.dropDashboard();
-//					}
-					// also see if other variables in runner that need to be dropped
-					PKQLRunner runner = in.getPkqlRunner();
-					runner.cleanUp();
-					
-					inStore.remove(id);
-					inStore.removeFromSessionHash(sessionID, id);
-//				}
+					}
+				}
+				LOGGER.info("Successfully removed all frames from insight");
+
+				insight.getVarStore().clear();
+				LOGGER.info("Successfully removed all variables from varstore");
+				
+				// also delete any files that were used
+				List<FileMeta> fileData = insight.getFilesUsedInInsight();
+				if (fileData != null && !fileData.isEmpty()) {
+					for (int fileIdx = 0; fileIdx < fileData.size(); fileIdx++) {
+						FileMeta file = fileData.get(fileIdx);
+						File f = new File(file.getFileLoc());
+						f.delete();
+						LOGGER.info("Successfully deleted File used in insight " + f.getName());
+					}
+				}
+				
+				Map<String, String> fileExports = insight.getExportFiles();
+				if (fileExports != null && !fileExports.isEmpty()) {
+					for (String fileKey : fileExports.keySet()){
+						File f = new File(fileExports.get(fileKey));
+						f.delete();
+						LOGGER.info("Successfully deleted File used in insight " + f.getName());
+					}
+				}
+				
+				LOGGER.info("Successfully removed insight");
 			}
-			System.out.println("successfully removed qStore information for session");
+			System.out.println("successfully removed insight information from session");
 		}
 	}
+	
 }
