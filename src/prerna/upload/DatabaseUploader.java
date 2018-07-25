f2894ac9bdfbf2cb2cfb0433810c4b777c081a3f
Maher Khalil <themaherkhalil@gmail.com>
2018-07-25 20:03:32 +0000

    BUGFIX ::: Fixing error with setting correct database id for add to existing
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@29366 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c850fddbdab292ca2d1efb8e35c8261b5b37d81c

--- src/prerna/upload/DatabaseUploader.java
+++ src/prerna/upload/DatabaseUploader.java
@@ -131,8 +131,12 @@ public class DatabaseUploader extends Uploader {
 			String errorMessage = "Database name \'" + dbName + "\' is invalid";
 			throw new IOException(errorMessage);
 		}
-		options.setDbName(Utility.makeAlphaNumeric(dbName));
-		options.setEngineID(UUID.randomUUID().toString());
+		if(importMethod == ImportOptions.IMPORT_METHOD.ADD_TO_EXISTING) {
+			options.setEngineID(dbName);
+		} else {
+			options.setDbName(Utility.makeAlphaNumeric(dbName));
+			options.setEngineID(UUID.randomUUID().toString());
+		}
 
 		// determine the db type
 		String dbType = inputData.get("dataOutputType");
