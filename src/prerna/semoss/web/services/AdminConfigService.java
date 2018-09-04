e4639b7bc2d2414c3d78fc990475cf0dcd054bf3
mahkhalil <mahkhalil@deloitte.com>
2018-09-04 18:22:14 +0000

    UPDATE ::: Finishing up the redirection after you add the admin users
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@29862 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/AdminConfigService.java
+++ src/prerna/semoss/web/services/AdminConfigService.java
@@ -1,31 +1,56 @@
 package prerna.semoss.web.services;
 
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
+
+import prerna.auth.SecurityUpdateUtils;
+import prerna.web.conf.AdminStartupFilter;
 import prerna.web.services.util.WebUtility;
 
 @Path("/adminconfig")
 public class AdminConfigService {
 
+	private static final Gson GSON = new Gson();
 	public static final String ADMIN_REDIRECT_KEY = "ADMIN_REDIRECT_KEY";
 	
 	@POST
 	@Path("/setInitialAdmins")
-	public Response setInitialAdmins(@Context HttpServletRequest request) {
+	public Response setInitialAdmins(@Context HttpServletRequest request, @Context HttpServletResponse response, MultivaluedMap<String, String> form) throws IOException {
 		HttpSession session = request.getSession(false);
-		System.out.println(session.getAttribute(ADMIN_REDIRECT_KEY));
-		System.out.println(session.getAttribute(ADMIN_REDIRECT_KEY));
-		System.out.println(session.getAttribute(ADMIN_REDIRECT_KEY));
-		System.out.println(session.getAttribute(ADMIN_REDIRECT_KEY));
-		System.out.println(session.getAttribute(ADMIN_REDIRECT_KEY));
-
 		
+		String idString = form.getFirst("ids");
+		if(idString == null || idString.isEmpty()) {
+			Map<String, String> errorMessage = new HashMap<String, String>();
+			errorMessage.put("errorMessage", "Need to send valid ids");
+			return WebUtility.getResponse(errorMessage, 200);
+		}
+		List<String> ids = GSON.fromJson(idString, List.class);
+		
+		for(String id : ids) {
+			SecurityUpdateUtils.registerUser(id, true);
+		}
+
+		if(session.getAttribute(ADMIN_REDIRECT_KEY) != null) {
+			String originalRedirect = session.getAttribute(ADMIN_REDIRECT_KEY) + "";
+			response.setHeader("redirect", originalRedirect);
+			response.sendError(302, "Need to redirect to " + originalRedirect);
+			AdminStartupFilter.setSuccessfulRedirectUrl(originalRedirect);
+		}
+
 		return WebUtility.getResponse("success", 200);
 	}
 	
