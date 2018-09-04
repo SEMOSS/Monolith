930faaf013b18070e2caef6be0f9698d5bbe9ad4
Maher Khalil <themaherkhalil@gmail.com>
2018-09-04 13:16:30 +0000

    UPDATE ::: Admin configuration routing pushed and necessary filters pushed but currently commented out
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@29844 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e120a47e1a88f9088e92575ca90cc7f3d8bbf844

---
+++ src/prerna/web/conf/AdminStartupFilter.java
@@ -0,0 +1,59 @@
+package prerna.web.conf;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.util.Constants;
+import prerna.util.Utility;
+
+public class AdminStartupFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+		boolean security = Boolean.parseBoolean(arg0.getServletContext().getInitParameter(Constants.SECURITY_ENABLED));
+		if(security) {
+			IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
+			String q = "SELECT * FROM USER LIMIT 1";
+			IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
+			try {
+				boolean hasUser = wrapper.hasNext();
+				// if there are users, redirect to the main semoss page
+				// we do not want to allow the person to make any admin requests
+				if(hasUser) {
+					((HttpServletRequest) arg0).getSession(true).setAttribute("user", true);
+					((HttpServletResponse) arg1).setStatus(302);
+					((HttpServletResponse) arg1).sendRedirect("http://localhost:8080/SemossWeb_AppUi/#!/");
+					return;
+				}
+			} finally {
+				wrapper.cleanUp();
+			}
+		}
+		
+		arg2.doFilter(arg0, arg1);
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
