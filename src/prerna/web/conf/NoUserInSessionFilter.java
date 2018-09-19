e1697c4f21530bf64a6a608098f0d21cc6869584
Maher Khalil <themaherkhalil@gmail.com>
2018-09-19 17:04:54 +0000

    UPDATE ::: simplifying the no user in session class
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@30140 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b4f59ed86c2ed289f65172558f2fe8608145e27b

--- src/prerna/web/conf/NoUserInSessionFilter.java
+++ src/prerna/web/conf/NoUserInSessionFilter.java
@@ -51,10 +51,8 @@ public class NoUserInSessionFilter implements Filter {
 				// since security is embedded w/ normal semoss and not standalone
 
 				HttpSession session = ((HttpServletRequest) arg0).getSession(true);
-				// users are registered but dont know who this specific user is
-				// take them to the login page
-				if(session.getAttribute("user") == null) {
-					session.setAttribute("user", true);
+				User user = (User) session.getAttribute(Constants.SESSION_USER);
+				if(user == null || user.getLogins().isEmpty()) {
 					((HttpServletResponse) arg1).setStatus(302);
 
 					String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
@@ -62,21 +60,6 @@ public class NoUserInSessionFilter implements Filter {
 					((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
 					((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
 					return;
-				}
-
-				// have the user redirect value
-				// need to make sure no one did any funny business and doesn't have an actual user object
-				else {
-					User user = (User) session.getAttribute(Constants.SESSION_USER);
-					if(user == null || user.getLogins().isEmpty()) {
-						((HttpServletResponse) arg1).setStatus(302);
-
-						String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
-						redirectUrl = redirectUrl + "#!/login";
-						((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
-						((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
-						return;
-					}
 				}
 			}
 		}
