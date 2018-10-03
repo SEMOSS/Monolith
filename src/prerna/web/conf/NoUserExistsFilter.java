80250880af0b69ba2e6ba35a32ff5d9d0aaedd45
mahkhalil <mahkhalil@deloitte.com>
2018-10-03 18:59:39 +0000

    UPDATE ::: Adding static boolean to limit security db querying constantly
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@30280 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/NoUserExistsFilter.java
+++ src/prerna/web/conf/NoUserExistsFilter.java
@@ -21,42 +21,53 @@ import prerna.util.Utility;
 
 public class NoUserExistsFilter implements Filter {
 
+	private static boolean userDefined = false;
+	
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
 		ServletContext context = arg0.getServletContext();
 		
 		boolean security = Boolean.parseBoolean(context.getInitParameter(Constants.SECURITY_ENABLED));
 		if(security) {
-			IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
-			String q = "SELECT * FROM USER LIMIT 1";
-			IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
-			try {
-				boolean hasUser = wrapper.hasNext();
+			
+			// i do not want to run this query for every single call
+			// just gets annoying 
+			if(!NoUserExistsFilter.userDefined) {
 				
-				// no users at all registered, we need to send to the admin page
-				if(!hasUser) {
+				IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
+				String q = "SELECT * FROM USER LIMIT 1";
+				IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
+				try {
+					boolean hasUser = wrapper.hasNext();
 					
-					// we need to store information in the session
-					// so that we can properly come back to the referer once an admin has been added
-					String referer = ((HttpServletRequest) arg0).getHeader("referer");
-					referer = referer + "#!/login";
-					((HttpServletRequest) arg0).getSession(true).setAttribute(AdminConfigService.ADMIN_REDIRECT_KEY, referer);
-
-					// this will be the deployment name of the app
-					String contextPath = context.getContextPath();
-					
-					// this will be the full path of the request
-					// like http://localhost:8080/Monolith_Dev/api/engine/runPixel
-					String fullUrl = ((HttpServletRequest) arg0).getRequestURL().toString();
+					// no users at all registered, we need to send to the admin page
+					if(!hasUser) {
+						
+						// we need to store information in the session
+						// so that we can properly come back to the referer once an admin has been added
+						String referer = ((HttpServletRequest) arg0).getHeader("referer");
+						referer = referer + "#!/login";
+						((HttpServletRequest) arg0).getSession(true).setAttribute(AdminConfigService.ADMIN_REDIRECT_KEY, referer);
 	
-					// we redirect to the index.html page where we have pushed the admin page
-					String redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length());
-					((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
-					((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
-					return;
+						// this will be the deployment name of the app
+						String contextPath = context.getContextPath();
+						
+						// this will be the full path of the request
+						// like http://localhost:8080/Monolith_Dev/api/engine/runPixel
+						String fullUrl = ((HttpServletRequest) arg0).getRequestURL().toString();
+		
+						// we redirect to the index.html page where we have pushed the admin page
+						String redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length());
+						((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
+						((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
+						return;
+					} else {
+						// set boolean so we dont keep querying all the time
+						NoUserExistsFilter.userDefined = true;
+					}
+				} finally {
+					wrapper.cleanUp();
 				}
-			} finally {
-				wrapper.cleanUp();
 			}
 		}
 		
