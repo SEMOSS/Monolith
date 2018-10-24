1ae1378f403ef0dead10e330d28ada3cc35583a7
mahkhalil <mahkhalil@deloitte.com>
2018-10-24 21:20:47 +0000

    UPDATE ::: If local master or security database do not pass the initialize steps, send the browser to a page that states that is the case.  Too many unexpected behaviors can occur if parts of local master or security are operational
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@30648 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f682a5bc5b7067abe2b1645b003c11e641644032

---
+++ src/prerna/web/conf/StartUpSuccessFilter.java
@@ -0,0 +1,60 @@
+package prerna.web.conf;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StartUpSuccessFilter implements Filter {
+
+	private static boolean startUpSuccess = true;
+	private static final String FAIL_HTML = "/startUpFail.html";
+	
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+		ServletContext context = arg0.getServletContext();
+		
+		if(!startUpSuccess) {
+			// this will be the deployment name of the app
+			String contextPath = context.getContextPath();
+			
+			// this will be the full path of the request
+			// like http://localhost:8080/Monolith_Dev/api/engine/runPixel
+			String fullUrl = ((HttpServletRequest) arg0).getRequestURL().toString();
+
+			if(!fullUrl.endsWith(FAIL_HTML)) {
+				// we redirect to the index.html page where we have pushed the admin page
+				String redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length()) + FAIL_HTML;
+				((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
+				((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
+				return;
+			}
+		}
+		
+		arg2.doFilter(arg0, arg1);
+	}
+	
+	static void setStartUpSuccess(boolean startUpSuccess) {
+		StartUpSuccessFilter.startUpSuccess = startUpSuccess;
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+}
