2618366fd5b38012a231bc1eea464a5d52784b66
mahkhalil <mahkhalil@deloitte.com>
2018-11-16 16:07:46 +0000

    UPDATE ::: Adding rest side of storage for theming
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@30936 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/semoss/web/services/local/ResourceUtility.java
@@ -0,0 +1,34 @@
+package prerna.semoss.web.services.local;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.Context;
+
+import prerna.auth.User;
+import prerna.util.Constants;
+
+public class ResourceUtility {
+
+	/**
+	 * Get the user
+	 * @param request
+	 * @return
+	 * @throws IOException
+	 */
+	public static User getUser(@Context HttpServletRequest request) throws IllegalAccessException {
+		HttpSession session = request.getSession(false);
+		if(session == null){
+			throw new IllegalAccessException("User session is invalid");
+		}
+		
+		User user = (User) session.getAttribute(Constants.SESSION_USER);
+		if(user == null) {
+			throw new IllegalAccessException("User session is invalid");
+		}
+		
+		return user;
+	}
+	
+}
