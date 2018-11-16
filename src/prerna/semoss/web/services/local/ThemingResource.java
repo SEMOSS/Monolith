b59bd56eda8ef8a9d769d70e6c9efdd10a8e2058
mahkhalil <mahkhalil@deloitte.com>
2018-11-16 16:07:46 +0000

    UPDATE ::: Adding rest side of storage for theming
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@30936 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a0e5e617afebda094b33a71408ec056b36bc643b

---
+++ src/prerna/semoss/web/services/local/ThemingResource.java
@@ -0,0 +1,167 @@
+package prerna.semoss.web.services.local;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import prerna.auth.User;
+import prerna.theme.AdminThemeUtils;
+import prerna.web.services.util.WebUtility;
+
+@Path("/theme")
+public class ThemingResource {
+
+	@GET
+	@Path("/getAdminThemes")
+	@Produces("application/json")
+	public Response getAdminThemes(@Context HttpServletRequest request) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		AdminThemeUtils instance = AdminThemeUtils.getInstance(user);
+		if(instance == null) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", "User is not an admin");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		List<Map<String, Object>> themes = instance.getAdminThemes();
+		return WebUtility.getResponse(themes, 200);
+	}
+	
+	@POST
+	@Path("/createAdminTheme")
+	@Produces("application/json")
+	public Response createAdminTheme(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		AdminThemeUtils instance = AdminThemeUtils.getInstance(user);
+		if(instance == null) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", "User is not an admin");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		String themeName = form.getFirst("name");
+		String themeMap = form.getFirst("json");
+		boolean isActive = Boolean.parseBoolean(form.getFirst("isActive"));
+		String themeId = instance.createAdminTheme(themeName, themeMap, isActive);
+		if (themeId != null) {
+			return WebUtility.getResponse(true, 200);
+		} else {
+			return WebUtility.getResponse(false, 400);
+		}
+	}
+	
+	@POST
+	@Path("/editAdminTheme")
+	@Produces("application/json")
+	public Response editAdminTheme(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		AdminThemeUtils instance = AdminThemeUtils.getInstance(user);
+		if(instance == null) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", "User is not an admin");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		String themeId = form.getFirst("id");
+		String themeName = form.getFirst("name");
+		String themeMap = form.getFirst("json");
+		boolean isActive = Boolean.parseBoolean(form.getFirst("isActive"));
+		boolean success = instance.editAdminTheme(themeId, themeName, themeMap, isActive);
+		if (success) {
+			return WebUtility.getResponse(success, 200);
+		} else {
+			return WebUtility.getResponse(success, 400);
+		}
+	}
+	
+	@POST
+	@Path("/deleteAdminTheme")
+	@Produces("application/json")
+	public Response deleteAdminTheme(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		AdminThemeUtils instance = AdminThemeUtils.getInstance(user);
+		if(instance == null) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", "User is not an admin");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		String themeId = form.getFirst("id");
+		boolean success = instance.deleteAdminTheme(themeId);
+		if (success) {
+			return WebUtility.getResponse(success, 200);
+		} else {
+			return WebUtility.getResponse(success, 400);
+		}
+	}
+	
+	@POST
+	@Path("/setActiveAdminTheme")
+	@Produces("application/json")
+	public Response setActiveAdminTheme(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		AdminThemeUtils instance = AdminThemeUtils.getInstance(user);
+		if(instance == null) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("error", "User is not an admin");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		String themeId = form.getFirst("id");
+		boolean success = instance.setActiveTheme(themeId);
+		if (success) {
+			return WebUtility.getResponse(success, 200);
+		} else {
+			return WebUtility.getResponse(success, 400);
+		}
+	}
+
+}
