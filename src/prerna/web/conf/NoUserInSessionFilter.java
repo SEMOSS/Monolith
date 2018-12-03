29e8a150ce523658956195624c1cc473bd1376f3
mahkhalil <mahkhalil@deloitte.com>
2018-12-03 16:43:37 +0000

    UPDATE ::: Exposing parameter to send the jsession id to redirect the browser to the correct session
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@31120 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d37e57fbe77ff37cacf6a69e36bfe35c849b8235

--- src/prerna/web/conf/NoUserInSessionFilter.java
+++ src/prerna/web/conf/NoUserInSessionFilter.java
@@ -1,6 +1,7 @@
 package prerna.web.conf;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
 
@@ -11,6 +12,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -23,7 +25,7 @@ public class NoUserInSessionFilter implements Filter {
 
 	private static final String NO_USER_HTML = "/noUserFail.html";
 	protected static List<String> ignoreDueToFE = new Vector<String>();
-	
+
 	static {
 		ignoreDueToFE.add("config");
 		ignoreDueToFE.add("auth/logins");
@@ -53,33 +55,86 @@ public class NoUserInSessionFilter implements Filter {
 				// since security is embedded w/ normal semoss and not standalone
 
 				HttpSession session = ((HttpServletRequest) arg0).getSession(false);
+				System.out.println("Session ID >> " + session.getId());
 				User user = null;
 				if(session != null) {
 					user = (User) session.getAttribute(Constants.SESSION_USER);
 				}
 				if(user == null || user.getLogins().isEmpty()) {
-					((HttpServletResponse) arg1).setStatus(302);
+					System.out.println("Url set to" + fullUrl);
+					printHeaders((HttpServletRequest) arg0);
 
-					String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
-					// if no referrer
-					// then a person hit the endpoint directly
-					if(redirectUrl == null) {
-						// this will be the deployment name of the app
-						String contextPath = context.getContextPath();
-						redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length()) + NO_USER_HTML;
-						((HttpServletResponse) arg1).setStatus(302);
-						((HttpServletResponse) arg1).sendRedirect(redirectUrl);
-					} else {
-						redirectUrl = redirectUrl + "#!/login";
-						((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
-						((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
+					// do a condition here if the session id request parameter is available
+					// eventually this will be that and the tableau
+					if(((HttpServletRequest)arg0).getParameter("JSESSIONID") != null) {
+						HttpServletRequest req = (HttpServletRequest)arg0; 
+						
+						String sessionId = req.getParameter("JSESSIONID");
+						// create the cookie add it and sent it back
+						Cookie k = new Cookie("JSESSIONID", sessionId);
+						// cool blink show if you enable the lower one
+						//k.setPath(request.getContextPath());
+						//k.setPath("/appui");
+						//response.addCookie(k);
+
+						k.setPath(req.getContextPath());
+						((HttpServletResponse)arg1).addCookie(k);
+
+						// I need some routine to remove this.. this is just a hack
+						fullUrl = fullUrl.replace("JSESSIONID", "random");
+
+						Cookie[] cookies = req.getCookies();
+						if (cookies != null) {
+							for (Cookie c : cookies) {
+								if (c.getName().equals("JSESSIONID")) {
+									c.setValue(sessionId);
+									System.out.println("Session id " + sessionId);
+									((HttpServletResponse)arg1).addCookie(c);
+								}
+							}
+						}
+						((HttpServletResponse)arg1).sendRedirect(fullUrl);
+						
+						// invalidate the current session to get rid of the session id
+						session.invalidate();
+						return;
 					}
-					return;
+					else
+					{
+						((HttpServletResponse) arg1).setStatus(302);
+						String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
+						// if no referrer
+						// then a person hit the endpoint directly
+						if(redirectUrl == null) {
+							// this will be the deployment name of the app
+							String contextPath = context.getContextPath();
+							redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length()) + NO_USER_HTML;
+							((HttpServletResponse) arg1).setStatus(302);
+							((HttpServletResponse) arg1).sendRedirect(redirectUrl);
+						} else {
+							redirectUrl = redirectUrl + "#!/login";
+							((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
+							((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
+						}
+						
+						// invalidate the current session to get rid of the session id
+						session.invalidate();
+						return;
+					}
 				}
 			}
 		}
 
 		arg2.doFilter(arg0, arg1);
+	}
+
+	private void printHeaders(HttpServletRequest req) {
+		Enumeration <String> headerNames = req.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+			String thisHeader = headerNames.nextElement();
+			String headerValue = req.getHeader(thisHeader);
+			System.out.println(thisHeader + "<<>> " + headerValue );
+		}
 	}
 
 	@Override
