12923fa6c34543fda1625da1598d06919ef313e2
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-12-05 15:03:17 +0000

    Updates to do the check on session
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@31178 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/NoUserInSessionFilter.java
+++ src/prerna/web/conf/NoUserInSessionFilter.java
@@ -1,6 +1,8 @@
 package prerna.web.conf;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import prerna.auth.AuthProvider;
+import prerna.auth.InsightToken;
 import prerna.auth.User;
 import prerna.util.Constants;
 
@@ -57,16 +60,21 @@ public class NoUserInSessionFilter implements Filter {
 				HttpSession session = ((HttpServletRequest) arg0).getSession(false);
 				User user = null;
 				if(session != null) {
-					System.out.println("Session ID >> " + session.getId());
+					//System.out.println("Session ID >> " + session.getId());
 					user = (User) session.getAttribute(Constants.SESSION_USER);
 				}
-				if(user == null || user.getLogins().isEmpty()) {
-					System.out.println("Url set to" + fullUrl);
-					printHeaders((HttpServletRequest) arg0);
+				
+				if(user == null || user.getLogins().isEmpty()) 
+				{
+					//System.out.println("Url set to" + fullUrl);
+					//printHeaders((HttpServletRequest) arg0);
 
 					// do a condition here if the session id request parameter is available
 					// eventually this will be that and the tableau
 					if(((HttpServletRequest)arg0).getParameter("JSESSIONID") != null) {
+						
+						// need to check the secret here
+						
 						HttpServletRequest req = (HttpServletRequest)arg0; 
 						
 						String sessionId = req.getParameter("JSESSIONID");
@@ -81,19 +89,31 @@ public class NoUserInSessionFilter implements Filter {
 						((HttpServletResponse)arg1).addCookie(k);
 
 						// I need some routine to remove this.. this is just a hack
-						fullUrl = fullUrl.replace("JSESSIONID", "random");
+						//fullUrl = fullUrl.replace("JSESSIONID", "random");
 
 						Cookie[] cookies = req.getCookies();
 						if (cookies != null) {
 							for (Cookie c : cookies) {
 								if (c.getName().equals("JSESSIONID")) {
 									c.setValue(sessionId);
-									System.out.println("Session id " + sessionId);
+									//System.out.println("Session id " + sessionId);
 									((HttpServletResponse)arg1).addCookie(c);
 								}
 							}
 						}
-						((HttpServletResponse)arg1).sendRedirect(fullUrl);
+						// lastly add the hash cookie
+						String hash = req.getParameter("hash");
+						Cookie h = new Cookie("HASH", hash);
+						// cool blink show if you enable the lower one
+						//k.setPath(request.getContextPath());
+						//k.setPath("/appui");
+						//response.addCookie(k);
+
+						h.setPath(req.getContextPath());
+						((HttpServletResponse)arg1).addCookie(h);
+						
+						
+						((HttpServletResponse)arg1).sendRedirect(fullUrl+"?"+req.getQueryString());
 						
 						// invalidate the current session to get rid of the session id
 						session.invalidate();
@@ -121,6 +141,63 @@ public class NoUserInSessionFilter implements Filter {
 						session.invalidate();
 						return;
 					}
+				}
+				// else
+				// see if the cookie is there and if so
+				else if(user.isShareSession(session.getId()) )
+				{
+					// the user is valid now see if this user was the one who gave access to do this 
+					// if the request has param has a secret 
+					// if not redirect to another url.. 
+					HttpServletRequest req = (HttpServletRequest)arg0; 
+					
+					String insightId = req.getParameter("i");
+					String secret = req.getParameter("s");
+					String hashId = null;
+					
+					Cookie[] cookies = req.getCookies();
+					if (cookies != null) {
+						for (Cookie c : cookies) {
+							if (c.getName().equals("HASH")) {
+								hashId = c.getValue();
+								break;
+							}
+						}
+					}
+					
+					// ok now is the hasid routine
+					InsightToken token = user.getInsight(insightId);
+					
+					try {
+						//response.sendRedirect("http://localhost:9090/Monolith/api/engine/all");
+						MessageDigest md = MessageDigest.getInstance("MD5");
+						
+						String finalData = token.getSalt()+secret;
+						
+						byte [] digest = md.digest(finalData.getBytes()) ;//.toString().getBytes();
+
+						StringBuffer sb = new StringBuffer();
+						for (int i = 0; i < digest.length; i++) {
+						  sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+						}
+						
+						if(hashId == null || !hashId.equals(sb+""))
+						{
+							// user has failed
+							System.out.println("This should send the redirect to password wrong page so may be back to itself");
+						}
+						else // this session has been ratified so remove the session and move the user forward
+							user.removeShare(session.getId());
+					} catch (NoSuchAlgorithmException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				// else if the session is still on.. and they are trying to game
+				// send to error
+				else
+				{
+					System.out.println("Need to catch this.. ");
 				}
 			}
 		}
