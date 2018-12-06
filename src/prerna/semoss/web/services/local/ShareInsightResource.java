85ef070808b33bf87451e8bd23ef0790a66af315
mahkhalil <mahkhalil@deloitte.com>
2018-12-06 17:50:36 +0000

    UPDATE ::: Adding new share resource to expose insight data as a GET. To be used with refactored tableau wdc request page UPDATE ::: Adding new OverrideParametersServletRequest to be able to wrap an incoming request and append post parameters for other method calls
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@31229 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/semoss/web/services/local/ShareInsightResource.java
@@ -0,0 +1,48 @@
+package prerna.semoss.web.services.local;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.web.services.util.WebUtility;
+
+@Path("share")
+public class ShareInsightResource {
+
+	@Path("/i-{insightId}")
+	public Object validInsight(@Context HttpServletRequest request, @PathParam("insightId") String insightId) {
+		HttpSession session = request.getSession(false);
+		if(session == null) {
+			Map<String, String> errorHash = new HashMap<String, String>();
+			errorHash.put("errorMessage", "Invalid session to retrieve insight data");
+			return WebUtility.getResponse(errorHash, 400);
+		}
+		
+		Insight in = InsightStore.getInstance().get(insightId);
+		if(in == null) {
+			Map<String, String> errorHash = new HashMap<String, String>();
+			errorHash.put("errorMessage", "Invalid insight id");
+			return WebUtility.getResponse(errorHash, 400);
+		}
+		
+		String sessionId = session.getId();
+		Set<String> sessionStore = InsightStore.getInstance().getInsightIDsForSession(sessionId);
+		if(sessionStore == null || !sessionStore.contains(insightId)) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("errorMessage", "Invaid session to retrieve insight data");
+			return WebUtility.getResponse(errorMap, 400);
+		}
+		
+		ShareInsight share = new ShareInsight(in);
+		return share;
+	}
+	
+}
