4a4bed63f054d65af62af70db58ea4b98a9c38e1
Maher Khalil <themaherkhalil@gmail.com>
2019-04-08 22:24:32 +0000

    UPDATE ::: Expiring old session ids on the browser when initially loading the app when making the server config rest endpoint call
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@32633 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8a66851c1330e8fec0035a391fe88c0fd729349c

--- src/prerna/semoss/web/services/config/ServerConfigurationResource.java
+++ src/prerna/semoss/web/services/config/ServerConfigurationResource.java
@@ -1,14 +1,19 @@
 package prerna.semoss.web.services.config;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import prerna.auth.User;
@@ -20,6 +25,7 @@ import prerna.semoss.web.services.local.UserResource;
 import prerna.theme.AdminThemeUtils;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.web.conf.DBLoader;
 import prerna.web.services.util.WebUtility;
 
 @Path("/config")
@@ -34,6 +40,8 @@ public class ServerConfigurationResource {
 	 * @return
 	 */
 	private static Map<String, Object> getConfig(@Context HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+
 		User user = null;
 		try {
 			user = ResourceUtility.getUser(request);
@@ -47,7 +55,6 @@ public class ServerConfigurationResource {
 				if(config == null) {
 					config = new HashMap<String, Object>();
 					// session timeout
-					HttpSession session = request.getSession(true);
 					config.put("timeout", (double) session.getMaxInactiveInterval() / 60);
 					
 					// r enabled
@@ -96,14 +103,14 @@ public class ServerConfigurationResource {
 						localMode = Boolean.parseBoolean(localModeStr);
 					}
 					config.put("localDeployment", localMode);
-					
-					// do not keep this session
-					// if no user and it is new
-					if(session.isNew() && user == null) {
-						session.invalidate();
-					}
 				}
 			}
+		}
+		
+		// do not keep this session
+		// if no user and it is new
+		if(session.isNew() && user == null) {
+			session.invalidate();
 		}
 		
 		Map<String, Object> myConfiguration = new HashMap<String, Object>();
@@ -121,8 +128,27 @@ public class ServerConfigurationResource {
 	@GET
 	@Path("/")
 	@Produces("application/json")
-	public Response getServerConfig(@Context HttpServletRequest request) {
-		return WebUtility.getResponse(getConfig(request), 200);
+	public Response getServerConfig(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+		List<NewCookie> newCookies = new Vector<NewCookie>();
+
+		try {
+			ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			// this errors when no user exists in the session
+			// should happen every time this is called
+			// since FE only calls this method on browser startup
+			// clean up any invalid cookies on the browser
+			Cookie[] cookies = request.getCookies();
+			for(Cookie c : cookies) {
+				if(DBLoader.getSessionIdKey().equals(c.getName())) {
+					// we need to null this out
+					NewCookie nullC = new NewCookie(c.getName(), c.getValue(), c.getPath(), c.getDomain(), c.getComment(), 0, c.getSecure());
+					newCookies.add(nullC);
+				}
+			}
+		}
+		
+		return WebUtility.getResponse(getConfig(request), 200, newCookies.toArray(new NewCookie[]{}));
 	}
 	
 }
