11bf9937ddeffdcebf874e49ef3ebc3c970d8d2d
Maher Khalil <themaherkhalil@gmail.com>
2019-06-10 12:16:03 +0000

    UPDATE ::: Adding logic to keep track of anonymous users via a cookie so they keep the same id BUGFIX ::: No user workspace caching when anonymous user
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@33266 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4c78039936518d1c1cbeebac438d67a322e47f09

--- src/prerna/web/conf/AnonymousUserFilter.java
+++ src/prerna/web/conf/AnonymousUserFilter.java
@@ -1,6 +1,7 @@
 package prerna.web.conf;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,7 +9,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import prerna.auth.User;
@@ -27,7 +30,35 @@ public class AnonymousUserFilter implements Filter {
 			if(user == null) {
 				semossUser = new User();
 				semossUser.setAnonymous(true);
-	
+				
+				String cookieToFind = DBLoader.getSessionIdKey() + "_unk";
+				Cookie[] cookies = ((HttpServletRequest) arg0).getCookies();
+				if(cookies != null) {
+					// loop through and see if we have a cookie for this user
+					for(Cookie c : cookies) {
+						if(c.getName().equals(cookieToFind)) {
+							String uId = c.getValue();
+							semossUser.setAnonymousId(uId);
+							// found the cookie
+							// no need to continue loop
+							break;
+						}
+					}
+				}
+				
+				if(semossUser.getAnonymousId() == null) {
+					// set a new id + add a cookie
+					String uId = "UNK_" + UUID.randomUUID().toString();
+					semossUser.setAnonymousId(uId);
+					
+					Cookie c = new Cookie(cookieToFind, uId);
+					// max age of 10years...
+					c.setMaxAge(60 * 60 * 24 * 365 * 10);
+					c.setPath(((HttpServletRequest) arg0).getContextPath());
+					c.setHttpOnly(true);
+					c.setSecure(true);
+					((HttpServletResponse)arg1).addCookie(c);
+				}
 				// add to session
 				session.setAttribute(Constants.SESSION_USER, semossUser);
 			}
