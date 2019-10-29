042c895e5c475f663a86a517ba472101cfd056a4
mahkhalil <mahkhalil@deloitte.com>
2019-10-29 12:17:56 +0000

    UPDATE ::: Minor import cleanup + correct parameter ordering in comments
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@35693 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/NoUserInSessionFilter.java
+++ src/prerna/web/conf/NoUserInSessionFilter.java
@@ -21,7 +21,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.core.Context;
 
 import prerna.auth.AuthProvider;
 import prerna.auth.InsightToken;
