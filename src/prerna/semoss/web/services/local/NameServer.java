488fd2691a5f2f1dd21691afc1507c7fb293eea8
Maher Khalil <themaherkhalil@gmail.com>
2020-02-11 16:13:31 +0000

    UPDATE ::: Adding session resource UDPATE ::: Adding request to invalidate the session when the FE timer times out UPDATE ::: Moving session requests to the session resource
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@37148 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2597e88921ce83657bc9c063ee4824b0fe685b2a

--- src/prerna/semoss/web/services/local/NameServer.java
+++ src/prerna/semoss/web/services/local/NameServer.java
@@ -34,14 +34,11 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
@@ -71,8 +68,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 
 import com.google.gson.Gson;
@@ -80,7 +75,6 @@ import com.google.gson.internal.StringMap;
 
 import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
-import prerna.auth.utils.SecurityAdminUtils;
 import prerna.auth.utils.SecurityAppUtils;
 import prerna.auth.utils.SecurityInsightUtils;
 import prerna.auth.utils.SecurityQueryUtils;
@@ -110,9 +104,6 @@ import prerna.util.DIHelper;
 import prerna.util.PlaySheetRDFMapBasedEnum;
 import prerna.util.Utility;
 import prerna.util.gson.GsonUtility;
-import prerna.util.insight.InsightUtility;
-import prerna.web.conf.DBLoader;
-import prerna.web.conf.SessionCounter;
 import prerna.web.services.util.ResponseHashSingleton;
 import prerna.web.services.util.SemossExecutorSingleton;
 import prerna.web.services.util.SemossThread;
@@ -124,196 +115,9 @@ public class NameServer {
 	// get the directory separator
 	protected static final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
 	
-	private static final Logger LOGGER = LogManager.getLogger(NameServer.class.getName());
-	private static final String CANCEL_INVALIDATION = "cancelInvalidation";
-	private Object lock = new Object();
-	
 	@Context
 	protected ServletContext context;
 
-	////////////////////////////////////////////////////////////////////////////////
-
-	@GET
-	@Path("/active")
-	@Produces("application/json;charset=utf-8")
-	public Response getActiveSessions(@Context HttpServletRequest request) {
-		if(AbstractSecurityUtils.securityEnabled()) {
-			User user;
-			try {
-				user = ResourceUtility.getUser(request);
-			} catch (IllegalAccessException e) {
-				Map<String, String> errorMap = new HashMap<String, String>();
-				errorMap.put("error", "User session is invalid");
-				return WebUtility.getResponse(errorMap, 401);
-			}
-			if(!SecurityAdminUtils.userIsAdmin(user)) {
-				Map<String, String> errorMap = new HashMap<String, String>();
-				errorMap.put("error", "User is not an admin");
-				return WebUtility.getResponse(errorMap, 401);
-			}
-		}
-
-		Map<String, Integer> ret = new HashMap<String, Integer>();
-		ret.put("activeSessions", SessionCounter.getActiveSessions());
-		return WebUtility.getResponse(ret, 200);
-	}
-	
-	/*
-	 * End points for cleanup of the thread to account for closing/opening
-	 */
-
-	@POST
-	@Path("/cleanSession")
-	@Produces("application/json;charset=utf-8")
-	public Response cleanSession(@Context HttpServletRequest request) {
-		// need to compare when this method was called
-		// to a potential cancellation
-		Date execTime = new Date();
-
-		HttpSession session = request.getSession(false);
-		if(session == null) {
-			LOGGER.info("Invalid session for cleaning");
-			Map<String, String> ret = new HashMap<String, String>();
-			ret.put("output", "Invalid session");
-			return WebUtility.getResponse(ret, 400);
-		}
-		LOGGER.info("Start invalidation of session");
-		String sessionId = session.getId();
-		// clear up insight store
-		InsightStore inStore = InsightStore.getInstance();
-		Set<String> insightIDs = inStore.getInsightIDsForSession(sessionId);
-		if(insightIDs != null) {
-			Set<String> copy = new HashSet<String>(insightIDs);
-			for(String insightId : copy) {
-				Insight insight = InsightStore.getInstance().get(insightId);
-				if(insight == null) {
-					continue;
-				}
-				InsightUtility.dropInsight(insight);
-			}
-			LOGGER.info("Successfully removed insight information from session");
-
-			// clear the current session store
-			insightIDs.removeAll(copy);
-		}
-
-		// wait 10 seconds
-		try {
-			Thread.sleep(10_000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-
-		String output = null;
-		if(request.isRequestedSessionIdValid()) {
-			// did the FE at any point cancel this clean?
-			Date cancelTime = null;
-			synchronized(lock) {
-				cancelTime = (Date) session.getAttribute(CANCEL_INVALIDATION);
-			}
-			if(cancelTime == null) {
-				// kill the entire session
-				LOGGER.info("Invalidating session");
-				session.invalidate();
-				output = "invalidated";
-			} else {
-				boolean isCancelled = execTime.before(cancelTime);
-				if(isCancelled) {
-					LOGGER.info("Cancelled invalidating session");
-					output = "cancelled";
-				} else {
-					// kill the entire session
-					LOGGER.info("Invalidating session");
-					session.invalidate();
-					output = "invalidated";
-				}
-			}
-		} else {
-			// in case during the time this was called
-			// the session has been invalidated by some other means
-			// like a logout
-			LOGGER.info("Session has already been invalidated");
-			output = "invalidated";
-		}
-
-		Map<String, String> ret = new HashMap<String, String>();
-		ret.put("output", output);
-		return WebUtility.getResponse(ret, 200);
-	}
-
-	@POST
-	@Path("/cancelCleanSession")
-	@Produces("application/json;charset=utf-8")
-	public Response cancelCleanSession(@Context HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-		if(session == null) {
-			Map<String, String> ret = new HashMap<String, String>();
-			ret.put("output", "Invalid session");
-			return WebUtility.getResponse(ret, 400);
-		}
-		LOGGER.info("Cancelling invalidation...");
-		Date d = new Date();
-		synchronized(lock) {
-			session.setAttribute(CANCEL_INVALIDATION, d);
-		}
-		
-		Map<String, String> ret = new HashMap<String, String>();
-		ret.put("output", "cancel");
-		return WebUtility.getResponse(ret, 200);
-	}
-	
-	@GET
-	@Path("/invalidateSession")
-	@Produces("application/json;charset=utf-8")
-	public void invalidateSession(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException {
-		String redirectUrl = request.getHeader("referer");
-		response.setStatus(302);
-
-		HttpSession session = request.getSession(false);
-		if(session == null) {
-			// redirect to the login page
-			redirectUrl = redirectUrl + "#!/login";
-			response.setHeader("redirect", redirectUrl);
-			response.sendError(302, "Need to redirect to " + redirectUrl);
-		} else {
-			// redirect to login/logout page
-			if(DBLoader.useLogoutPage()) {
-				String scheme = request.getScheme();             // http
-			    String serverName = request.getServerName();     // hostname.com
-			    int serverPort = request.getServerPort();        // 8080
-			    String contextPath = request.getContextPath();   // /Monolith
-				
-			    redirectUrl = "";
-			    redirectUrl += scheme + "://" + serverName;
-			    if (serverPort != 80 && serverPort != 443) {
-			    	redirectUrl += ":" + serverPort;
-			    }
-			    redirectUrl += contextPath + "/logout/";
-				response.setHeader("redirect", redirectUrl);
-				response.sendError(302, "Need to redirect to " + redirectUrl);
-			} else {
-				redirectUrl = redirectUrl + "#!/login";
-				response.setHeader("redirect", redirectUrl);
-				response.sendError(302, "Need to redirect to " + redirectUrl);
-			}
-		}
-	}
-	
-	@GET
-	@Path("/insights")
-	@Produces("application/json;charset=utf-8")
-	public Response getInsights(@Context HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-		if(session == null) {
-			Map<String, String> ret = new HashMap<String, String>();
-			ret.put("output", "Invalid session");
-			return WebUtility.getResponse(ret, 400);
-		}
-		String sessionId = session.getId();
-		Set<String> ids = InsightStore.getInstance().getInsightIDsForSession(sessionId);
-		return WebUtility.getResponse(ids, 200);
-	}
-	
 	////////////////////////////////////////////////////////////////////////////////
 
 	// uploader functionality
