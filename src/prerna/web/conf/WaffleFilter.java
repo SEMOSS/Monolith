f121295cd19a155e63665deae2d83d49b4aa3fcd
mahkhalil <mahkhalil@deloitte.com>
2020-03-29 20:32:09 +0000

    UPDATE ::: Adding ability to log users as they come into the session into a file
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@38032 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 68b9125f1402020b75af5894a4fa9715568432ea

--- src/prerna/web/conf/WaffleFilter.java
+++ src/prerna/web/conf/WaffleFilter.java
@@ -2,6 +2,9 @@ package prerna.web.conf;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,23 +15,40 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.auth.utils.SecurityUpdateUtils;
 import prerna.util.Constants;
+import prerna.web.conf.util.CACTrackingUtil;
+import prerna.web.conf.util.UserFileLogUtil;
 
 public class WaffleFilter implements Filter {
 
+	private static final Logger LOGGER = LogManager.getLogger(WaffleFilter.class.getName()); 
+
+	// filter init params
 	private static final String AUTO_ADD = "autoAdd";
+	private static final String COUNT_USER_ENTRY = "countUserEntry";
+	private static final String COUNT_USER_ENTRY_DATABASE = "countUserEntryDb";
+	private static final String LOG_USER_INFO = "logUserInfo";
+	private static final String LOG_USER_INFO_PATH = "logUserInfoPath";
+	private static final String LOG_USER_INFO_SEP = "logUserInfoSep";
+
+	// realization of init params
 	private static Boolean autoAdd = null;
+	private CACTrackingUtil tracker = null;
+	private UserFileLogUtil userLogger = null;
 
 	private FilterConfig filterConfig;
-	
+
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
 		setInitParams(arg0);
-		
+
 		HttpSession session = ((HttpServletRequest)arg0).getSession(true);
 		User user = (User) session.getAttribute(Constants.SESSION_USER);
 		if(user == null) {
@@ -38,16 +58,27 @@ public class WaffleFilter implements Filter {
 			Principal principal = ((HttpServletRequest) arg0).getUserPrincipal();
 			String id = principal.getName();
 			String name = id.substring(id.lastIndexOf('\\')+1);
-			
+
 			AccessToken token = new AccessToken();
 			token.setProvider(AuthProvider.WINDOWS_USER);
 			token.setId(id);
 			token.setName(name);
+			LOGGER.info("Valid request coming from user " + token.getName());
 			// add the user if they do not exist
 			if(WaffleFilter.autoAdd) {
 				SecurityUpdateUtils.addOAuthUser(token);
 			}
-			
+
+			// do we need to count?
+			if(tracker != null) {
+				tracker.addToQueue(LocalDate.now());
+			}
+
+			// are we logging their information?
+			if(userLogger != null) {
+				userLogger.addToQueue(new String[] {id, name, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))});
+			}
+
 			user.setAccessToken(token);
 			session.setAttribute(Constants.SESSION_USER, user);
 		}
@@ -65,7 +96,7 @@ public class WaffleFilter implements Filter {
 	public void init(FilterConfig arg0) throws ServletException {
 		this.filterConfig = arg0;
 	}
-	
+
 	private void setInitParams(ServletRequest arg0) {
 		if(WaffleFilter.autoAdd == null) {
 			String autoAddStr = this.filterConfig.getInitParameter(AUTO_ADD);
@@ -74,6 +105,56 @@ public class WaffleFilter implements Filter {
 			} else {
 				// Default value is true
 				WaffleFilter.autoAdd = true;
+			}
+
+			boolean logUsers = false;
+			String logUserInfoStr = this.filterConfig.getInitParameter(LOG_USER_INFO);
+			if(logUserInfoStr != null) {
+				logUsers = Boolean.parseBoolean(logUserInfoStr);
+			}
+			if(logUsers) {
+				String logInfoPath = this.filterConfig.getInitParameter(LOG_USER_INFO_PATH);
+				String logInfoSep = this.filterConfig.getInitParameter(LOG_USER_INFO_SEP);
+				if(logInfoPath == null) {
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A USER FILE LOG BUT NOT FILE PATH HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A USER FILE LOG BUT NOT FILE PATH HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A USER FILE LOG BUT NOT FILE PATH HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A USER FILE LOG BUT NOT FILE PATH HAS BEEN ENTERED!!!");
+				}
+				try {
+					userLogger = UserFileLogUtil.getInstance(logInfoPath, logInfoSep);
+				} catch(Exception e) {
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+				}
+			}
+
+			boolean countUsers = false;
+			String countUsersStr = this.filterConfig.getInitParameter(COUNT_USER_ENTRY);
+			if(countUsersStr != null) {
+				countUsers = Boolean.parseBoolean(countUsersStr);
+			} else {
+				countUsers = false;
+			}
+
+			if(countUsers) {
+				String countDatabaseId = this.filterConfig.getInitParameter(COUNT_USER_ENTRY_DATABASE);
+				if(countDatabaseId == null) {
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A COUNT BUT NO DATABASE ID HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A COUNT BUT NO DATABASE ID HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A COUNT BUT NO DATABASE ID HAS BEEN ENTERED!!!");
+					LOGGER.info("SYSTEM HAS REGISTERED TO PERFORM A COUNT BUT NO DATABASE ID HAS BEEN ENTERED!!!");
+				}
+				try {
+					tracker = CACTrackingUtil.getInstance(countDatabaseId);
+				} catch(Exception e) {
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+					LOGGER.info(e.getMessage());
+				}
 			}
 		}
 	}
---
+++ src/prerna/web/conf/util/UserFileLogUtil.java
@@ -0,0 +1,117 @@
+package prerna.web.conf.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class UserFileLogUtil {
+
+	private static Map<String, UserFileLogUtil> singletonStore = new HashMap<String, UserFileLogUtil>();
+
+	/*
+	 * Creating a class to write to a file the users who are signing in
+	 */
+
+	private String filePath;
+	private String sep;
+	private BlockingQueue<String[]> queue;
+	private FileAppender fileLogger;
+
+	public UserFileLogUtil(String filePath, String sep) throws IOException {
+		this.filePath = filePath;
+		this.sep = sep;
+		this.queue = new ArrayBlockingQueue<String[]>(50);
+		this.fileLogger = new FileAppender(this.filePath, this.sep, this.queue);
+
+		new Thread(this.fileLogger).start();
+	}
+
+	public static UserFileLogUtil getInstance(String filePath, String sep) throws IOException {
+		if(filePath == null || filePath.trim().isEmpty()) {
+			throw new IOException("Must pass in a valid filePath");
+		}
+		if(!singletonStore.containsKey(filePath)) {
+			synchronized (UserFileLogUtil.class) {
+				if(!singletonStore.containsKey(filePath)) {
+					UserFileLogUtil trackingUtil = new UserFileLogUtil(filePath, sep);
+					singletonStore.put(filePath, trackingUtil);
+				}
+			}
+		}
+		return singletonStore.get(filePath);
+	}
+
+	public void addToQueue(String[] row) {
+		queue.add(row);
+	}
+
+}
+
+class FileAppender implements Runnable {
+
+	private File f = null;
+	private FileWriter fw = null;
+	private String filePath = null;
+	private String sep = null;
+	private BlockingQueue<String[]> queue = null;
+
+	public FileAppender(String filePath, String sep, BlockingQueue<String[]> queue) throws IOException {
+		this.filePath = filePath;
+		this.sep = sep;
+		this.queue = queue;
+
+		f = new File(this.filePath);
+		if(!f.getParentFile().exists()) {
+			f.getParentFile().mkdirs();
+		}
+		
+		// set to append mode
+		// to account for restarts of the service
+		this.fw = new FileWriter(f, true);
+	}
+
+	public void run() {
+		try {
+			String[] row = null;
+			while( (row = queue.take()) != null) {
+				if(fw == null) {
+					this.fw = new FileWriter(f, true);
+				}
+				if(row == null || row.length == 0) {
+					continue;
+				}
+				int size = row.length;
+				StringBuilder builder = new StringBuilder();
+				builder.append(row[0]);
+				for(int i = 1; i < size; i++) {
+					builder.append(this.sep).append(row[i]);
+				}
+				builder.append("\n");
+				try {
+					fw.write(builder.toString());
+					fw.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			fw = null;
+		}
+	}
+
+}
