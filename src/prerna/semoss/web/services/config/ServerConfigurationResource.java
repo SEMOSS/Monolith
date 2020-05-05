6f7bcfef29e2c6f2111621111116fa053c272e44
Maher Khalil <themaherkhalil@gmail.com>
2020-05-05 17:08:21 +0000

    UPDATE ::: Resolving double-checked locking
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@38543 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c82f9e65ac36380f3901bbbbdc54577d74882545

--- src/prerna/semoss/web/services/config/ServerConfigurationResource.java
+++ src/prerna/semoss/web/services/config/ServerConfigurationResource.java
@@ -31,7 +31,7 @@ import prerna.web.services.util.WebUtility;
 @Path("/config")
 public class ServerConfigurationResource {
 
-	private static Map<String, Object> config = null;
+	private static volatile Map<String, Object> config = null;
 
 	/**
 	 * Generate the configuration options for this instance Only need to make this
@@ -49,85 +49,97 @@ public class ServerConfigurationResource {
 		} catch (IllegalAccessException e) {
 			// ignore
 		}
-
-		if (config == null) {
-			// make thread safe
-			synchronized (ServerConfigurationResource.class) {
-				if (config == null) {
-					config = new HashMap<>();
-					// session timeout
-					config.put("timeout", (double) session.getMaxInactiveInterval() / 60);
-
-					// r enabled
-					boolean useR = true;
-					String useRStr = DIHelper.getInstance().getProperty(Constants.USE_R);
-					if (useRStr != null) {
-						useR = Boolean.parseBoolean(useRStr);
-					}
-					config.put("r", useR);
-
-					// python enabled
-					config.put("python", PyUtils.pyEnabled());
-
-					// security enabled
-					config.put("security", AbstractSecurityUtils.securityEnabled());
-					config.put("anonymousUsers", AbstractSecurityUtils.anonymousUsersEnabled());
-					config.put("anonymousUserUploadData", AbstractSecurityUtils.anonymousUserUploadData());
-					// admin set public enabled
-					boolean adminSetPublic = false;
-					String adminSetPublicStr = (String) DIHelper.getInstance().getLocalProp(Constants.ADMIN_SET_PUBLIC);
-					if (adminSetPublicStr != null) {
-						adminSetPublic = Boolean.parseBoolean(adminSetPublicStr);
-					}
-					config.put("adminSetPublic", adminSetPublic);
-
-					// return a boolean if we want to use a dedicated logout page
-					// instead of redirecting to the login page
-					config.put("useLogoutPage", DBLoader.useLogoutPage());
-
-					// max file transfer size
-					String fileTransferMax = DIHelper.getInstance().getProperty(Constants.FILE_TRANSFER_LIMIT);
-					if (fileTransferMax != null) {
-						try {
-							config.put("file-limit", Integer.parseInt(fileTransferMax));
-						} catch (Exception e) {
-							// ignore
-						}
-					}
-
-					// version of the application
-					try {
-						Map<String, String> versionMap = VersionReactor.getVersionMap();
-						config.put("version", versionMap);
-					} catch (Exception e) {
-						// ignore
-					}
-
-					// send the default frame type
-					String defaultFrameType = DIHelper.getInstance().getProperty(Constants.DEFAULT_FRAME_TYPE);
-					if (defaultFrameType == null) {
-						defaultFrameType = "GRID";
-					}
-					config.put("defaultFrameType", defaultFrameType);
-
-					String defaultScriptingLanguage = DIHelper.getInstance()
-							.getProperty(Constants.DEFAULT_SCRIPTING_LANGUAGE);
-					if (defaultScriptingLanguage == null) {
-						defaultScriptingLanguage = "R";
-					}
-					config.put("defaultScriptingLanguage", defaultScriptingLanguage);
-
-					// local mode
-					boolean localMode = false;
-					String localModeStr = DIHelper.getInstance().getProperty(Constants.LOCAL_DEPLOYMENT);
-					if (localModeStr != null) {
-						localMode = Boolean.parseBoolean(localModeStr);
-					}
-					config.put("localDeployment", localMode);
-				}
+		
+		if(config != null) {
+			return getConfiguration(session, user);
+		}
+		
+		// make thread safe
+		synchronized (ServerConfigurationResource.class) {
+			if (config == null) {
+				loadConfig(session);
 			}
 		}
 
+		return getConfiguration(session, user);
+	}
+	
+	private static void loadConfig(HttpSession session) {
+		Map<String, Object> loadConfig = new HashMap<>();
+		// session timeout
+		loadConfig.put("timeout", (double) session.getMaxInactiveInterval() / 60);
+
+		// r enabled
+		boolean useR = true;
+		String useRStr = DIHelper.getInstance().getProperty(Constants.USE_R);
+		if (useRStr != null) {
+			useR = Boolean.parseBoolean(useRStr);
+		}
+		loadConfig.put("r", useR);
+
+		// python enabled
+		loadConfig.put("python", PyUtils.pyEnabled());
+
+		// security enabled
+		loadConfig.put("security", AbstractSecurityUtils.securityEnabled());
+		loadConfig.put("anonymousUsers", AbstractSecurityUtils.anonymousUsersEnabled());
+		loadConfig.put("anonymousUserUploadData", AbstractSecurityUtils.anonymousUserUploadData());
+		// admin set public enabled
+		boolean adminSetPublic = false;
+		String adminSetPublicStr = (String) DIHelper.getInstance().getLocalProp(Constants.ADMIN_SET_PUBLIC);
+		if (adminSetPublicStr != null) {
+			adminSetPublic = Boolean.parseBoolean(adminSetPublicStr);
+		}
+		loadConfig.put("adminSetPublic", adminSetPublic);
+
+		// return a boolean if we want to use a dedicated logout page
+		// instead of redirecting to the login page
+		loadConfig.put("useLogoutPage", DBLoader.useLogoutPage());
+
+		// max file transfer size
+		String fileTransferMax = DIHelper.getInstance().getProperty(Constants.FILE_TRANSFER_LIMIT);
+		if (fileTransferMax != null) {
+			try {
+				loadConfig.put("file-limit", Integer.parseInt(fileTransferMax));
+			} catch (Exception e) {
+				// ignore
+			}
+		}
+
+		// version of the application
+		try {
+			Map<String, String> versionMap = VersionReactor.getVersionMap();
+			loadConfig.put("version", versionMap);
+		} catch (Exception e) {
+			// ignore
+		}
+
+		// send the default frame type
+		String defaultFrameType = DIHelper.getInstance().getProperty(Constants.DEFAULT_FRAME_TYPE);
+		if (defaultFrameType == null) {
+			defaultFrameType = "GRID";
+		}
+		loadConfig.put("defaultFrameType", defaultFrameType);
+
+		String defaultScriptingLanguage = DIHelper.getInstance()
+				.getProperty(Constants.DEFAULT_SCRIPTING_LANGUAGE);
+		if (defaultScriptingLanguage == null) {
+			defaultScriptingLanguage = "R";
+		}
+		loadConfig.put("defaultScriptingLanguage", defaultScriptingLanguage);
+
+		// local mode
+		boolean localMode = false;
+		String localModeStr = DIHelper.getInstance().getProperty(Constants.LOCAL_DEPLOYMENT);
+		if (localModeStr != null) {
+			localMode = Boolean.parseBoolean(localModeStr);
+		}
+		loadConfig.put("localDeployment", localMode);
+		
+		ServerConfigurationResource.config = loadConfig;
+	}
+
+	private static Map<String, Object> getConfiguration(HttpSession session, User user) {
 		// do not keep this session
 		// if no user and it is new
 		if (session.isNew() && user == null) {
@@ -145,7 +157,7 @@ public class ServerConfigurationResource {
 		myConfiguration.put("theme", AdminThemeUtils.getActiveAdminTheme());
 		return myConfiguration;
 	}
-
+	
 	@GET
 	@Path("/")
 	@Produces("application/json")
