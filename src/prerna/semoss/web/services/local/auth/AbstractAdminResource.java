d13ef606d3e0a76d332868c933a65cc0ca5fd263
mahkhalil <mahkhalil@deloitte.com>
2020-06-02 22:46:42 +0000

    UPDATE ::: Adding logging when admin is going through and updating user permissions
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@38975 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/local/auth/AbstractAdminResource.java
+++ src/prerna/semoss/web/services/local/auth/AbstractAdminResource.java
@@ -5,12 +5,10 @@ import javax.ws.rs.core.Context;
 
 import prerna.auth.User;
 import prerna.auth.utils.SecurityAdminUtils;
-import prerna.semoss.web.services.local.ResourceUtility;
 
 public class AbstractAdminResource {
-
-	SecurityAdminUtils performAdminCheck(@Context HttpServletRequest request) throws IllegalAccessException {
-		User user = ResourceUtility.getUser(request);
+	
+	SecurityAdminUtils performAdminCheck(@Context HttpServletRequest request, User user) throws IllegalAccessException {
 		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
 		if(adminUtils == null) {
 			throw new IllegalArgumentException("User is not an admin");
