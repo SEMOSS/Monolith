2a266284dd7cebcee56623a9e345b5fe48e4af3c
mahkhalil <mahkhalil@deloitte.com>
2020-07-12 19:26:32 +0000

    UPDATE ::: Removing unused rest endpoints UPDATE ::: Adding new end point to get the state of an existing insight
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@39427 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/local/RunInsight.java
+++ src/prerna/semoss/web/services/local/RunInsight.java
@@ -1,6 +1,12 @@
 package prerna.semoss.web.services.local;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PushbackReader;
+import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +16,32 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.google.gson.stream.JsonWriter;
+
 import prerna.om.Insight;
+import prerna.om.InsightPanel;
+import prerna.om.InsightSheet;
+import prerna.sablecc2.PixelPreProcessor;
+import prerna.sablecc2.PixelRunner;
+import prerna.sablecc2.PixelStreamUtility;
+import prerna.sablecc2.lexer.Lexer;
+import prerna.sablecc2.lexer.LexerException;
+import prerna.sablecc2.node.Start;
+import prerna.sablecc2.parser.Parser;
+import prerna.sablecc2.parser.ParserException;
+import prerna.sablecc2.translations.OptimizeRecipeTranslation;
+import prerna.util.Constants;
+import prerna.util.gson.InsightPanelAdapter;
+import prerna.util.gson.InsightSheetAdapter;
 import prerna.web.requests.OverrideParametersServletRequest;
 
 public class RunInsight {
 
+	private static final Logger logger = LogManager.getLogger(RunInsight.class);
+	
 	private Insight in = null;
 	private boolean drop = false;
 	
@@ -32,6 +59,78 @@ public class RunInsight {
 	public Response getInsightData(@Context HttpServletRequest request) {
 		String pixel = "QueryAll()|Collect(-1);FrameHeaders();";
 		return runPixel(request, pixel);
+	}
+	
+	@POST
+	@Path("/getInsightState")
+	@Produces("application/json")
+	public Response recreateInsightState(@Context HttpServletRequest request) {
+		List<String> recipe = in.getPixelRecipe();
+		
+		OptimizeRecipeTranslation opTrans = getOptimizedRecipe(recipe);
+		Insight rerunInsight = new Insight();
+		rerunInsight.setVarStore(in.getVarStore());
+		rerunInsight.setUser(in.getUser());
+		
+		try {
+			// add a copy of all the insight sheets
+			Map<String, InsightSheet> sheets = in.getInsightSheets();
+			for(String sheetId : sheets.keySet()) {
+				InsightSheetAdapter adapter = new InsightSheetAdapter();
+				StringWriter writer = new StringWriter();
+				JsonWriter jWriter = new JsonWriter(writer);
+				adapter.write(jWriter, sheets.get(sheetId));
+				String sheetStr = writer.toString();
+				InsightSheet sheetClone = adapter.fromJson(sheetStr);
+				rerunInsight.addNewInsightSheet(sheetClone);
+			}
+			
+			// add a copy of all the insight panels
+			Map<String, InsightPanel> panels = in.getInsightPanels();
+			for(String panelId : panels.keySet()) {
+				InsightPanelAdapter adapter = new InsightPanelAdapter();
+				StringWriter writer = new StringWriter();
+				JsonWriter jWriter = new JsonWriter(writer);
+				adapter.write(jWriter, panels.get(panelId));
+				String panelStr = writer.toString();
+				InsightPanel panelClone = adapter.fromJson(panelStr);
+				rerunInsight.addNewInsightPanel(panelClone);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		PixelRunner pixelRunner = rerunInsight.runPixel(opTrans.getCachedPixelRecipeSteps());
+		return Response.status(200).entity(PixelStreamUtility.collectPixelData(pixelRunner))
+				.header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0")
+				.header("Pragma", "no-cache")
+				.build();
+	}
+	
+	private OptimizeRecipeTranslation getOptimizedRecipe(List<String> recipe) {
+		OptimizeRecipeTranslation translation = new OptimizeRecipeTranslation();
+		for (int i = 0; i < recipe.size(); i++) {
+			String expression = recipe.get(i);
+			// fill in the encodedToOriginal with map for the current expression
+			expression = PixelPreProcessor.preProcessPixel(expression.trim(), translation.encodingList, translation.encodedToOriginal);
+			try {
+				Parser p = new Parser(
+						new Lexer(
+								new PushbackReader(
+										new InputStreamReader(
+												new ByteArrayInputStream(expression.getBytes("UTF-8")), "UTF-8"), expression.length())));
+				// parsing the pixel - this process also determines if expression is syntactically correct
+				Start tree = p.parse();
+				// apply the translation
+				// when we apply the translation, we will change encoded expressions back to their original form
+				tree.apply(translation);
+				// reset translation.encodedToOriginal for each expression
+				translation.encodedToOriginal = new HashMap<String, String>();
+			} catch (ParserException | LexerException | IOException e) {
+				logger.error(Constants.STACKTRACE, e);
+			}
+		}
+		return translation;
 	}
 	
 	/**
