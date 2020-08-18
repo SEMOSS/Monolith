92b1d46a83adef057c44e09a95f9326e4a3f07e7
mahkhalil <mahkhalil@deloitte.com>
2020-08-18 15:47:35 +0000

    UPDATE ::: Adding unimplemented fields
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@39759 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 793d2451f037e40358b3b25966ed84310c637e43

--- src/prerna/web/conf/SchedulerFilter.java
+++ src/prerna/web/conf/SchedulerFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -75,5 +76,16 @@ public class SchedulerFilter implements Filter {
 		arg2.doFilter(arg0, arg1);
 	}
 
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
