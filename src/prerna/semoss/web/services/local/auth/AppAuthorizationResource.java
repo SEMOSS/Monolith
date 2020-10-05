f2d671516ee6be73403617e22075420a76253526
rramirezjimenez <rramirezjimenez@deloitte.com>
2020-10-05 18:37:40 +0000

    UPDATE ::: Adding rest endpoints to see users that don't have access to an insight or app
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@40472 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 733a30013e2b24ddfee6b2be791ad6a86094186f

--- src/prerna/semoss/web/services/local/auth/AppAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/AppAuthorizationResource.java
@@ -364,4 +364,40 @@ public class AppAuthorizationResource {
 		
 		return WebUtility.getResponse(true, 200);
 	}
+	
+	/**
+	 * Get users with no access to a given app
+	 * @param request
+	 * @param form
+	 * @return
+	 */
+	@GET
+	@Produces("application/json")
+	@Path("getAppUsersNoCredentials")
+	public Response getAppUsersNoCredentials(@Context HttpServletRequest request, @QueryParam("appId") String appId) {
+		User user = null;
+		try {
+			user = ResourceUtility.getUser(request);
+		} catch (IllegalAccessException e) {
+			logger.warn(ResourceUtility.getLogMessage(request, request.getSession(), User.getSingleLogginName(user), " invalid user session trying to access authorization resources"));
+			logger.error(Constants.STACKTRACE, e);
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put(ResourceUtility.ERROR_KEY, "User session is invalid");
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		List<Map<String, Object>> ret = null;
+		try {
+			ret = SecurityAppUtils.getAppUsersNoCredentials(user, appId);
+		} catch (IllegalAccessException e) {
+			logger.warn(ResourceUtility.getLogMessage(request, request.getSession(), User.getSingleLogginName(user), " is trying to pull users for " + appId + " that do not have credentials without having proper access"));
+			logger.error(Constants.STACKTRACE, e);
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put(ResourceUtility.ERROR_KEY, e.getMessage());
+			return WebUtility.getResponse(errorMap, 401);
+		}
+		
+		return WebUtility.getResponse(ret, 200);
+	}
+		
 }
