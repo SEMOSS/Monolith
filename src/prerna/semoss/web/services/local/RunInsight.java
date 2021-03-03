5cd37a03a9b92ba893d0d8d2cc88d3bf4d0057d4
mahkhalil <mahkhalil@deloitte.com>
2021-03-03 22:05:26 +0000

    UPDATE ::: Pushing logic to allow for OpenInsight the extra parameter useExistingIfOpen="true" which would be useful on iframes when the user is doing lots of toggling and we dont want the insights to reload every time
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@42899 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be6fbb3d9138c0f1713b3a8c176eea30277da32e

--- src/prerna/semoss/web/services/local/RunInsight.java
+++ src/prerna/semoss/web/services/local/RunInsight.java
@@ -1,9 +1,6 @@
 package prerna.semoss.web.services.local;
 
-import java.io.IOException;
-import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,23 +13,9 @@ import javax.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.gson.stream.JsonWriter;
-
-import prerna.algorithm.api.ITableDataFrame;
 import prerna.om.Insight;
-import prerna.om.InsightPanel;
-import prerna.om.InsightSheet;
-import prerna.om.ThreadStore;
 import prerna.sablecc2.PixelRunner;
 import prerna.sablecc2.PixelStreamUtility;
-import prerna.sablecc2.PixelUtility;
-import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
-import prerna.sablecc2.om.VarStore;
-import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.job.JobReactor;
-import prerna.util.gson.InsightPanelAdapter;
-import prerna.util.gson.InsightSheetAdapter;
 import prerna.util.insight.InsightUtility;
 import prerna.web.requests.OverrideParametersServletRequest;
 
@@ -63,72 +46,7 @@ public class RunInsight {
 	@Path("/getInsightState")
 	@Produces("application/json")
 	public Response recreateInsightState(@Context HttpServletRequest request) {
-		List<String> recipe = PixelUtility.getCachedInsightRecipe(in);
-		
-		Insight rerunInsight = new Insight();
-		rerunInsight.setVarStore(in.getVarStore());
-		rerunInsight.setUser(in.getUser());
-		InsightUtility.transferDefaultVars(in, rerunInsight);
-		
-		// set in thread
-		ThreadStore.setInsightId(in.getInsightId());
-		ThreadStore.setSessionId(in.getVarStore().get(JobReactor.SESSION_KEY).getValue() + "");
-		ThreadStore.setUser(in.getUser());
-		
-		try {
-			// add a copy of all the insight sheets
-			Map<String, InsightSheet> sheets = in.getInsightSheets();
-			for(String sheetId : sheets.keySet()) {
-				InsightSheetAdapter adapter = new InsightSheetAdapter();
-				StringWriter writer = new StringWriter();
-				JsonWriter jWriter = new JsonWriter(writer);
-				adapter.write(jWriter, sheets.get(sheetId));
-				String sheetStr = writer.toString();
-				InsightSheet sheetClone = adapter.fromJson(sheetStr);
-				rerunInsight.addNewInsightSheet(sheetClone);
-			}
-			
-			// add a copy of all the insight panels
-			Map<String, InsightPanel> panels = in.getInsightPanels();
-			for(String panelId : panels.keySet()) {
-				InsightPanelAdapter adapter = new InsightPanelAdapter();
-				StringWriter writer = new StringWriter();
-				JsonWriter jWriter = new JsonWriter(writer);
-				adapter.write(jWriter, panels.get(panelId));
-				String panelStr = writer.toString();
-				InsightPanel panelClone = adapter.fromJson(panelStr);
-				rerunInsight.addNewInsightPanel(panelClone);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		PixelRunner pixelRunner = new PixelRunner();
-		// add all the frame headers to the payload first
-		try {
-			VarStore vStore = in.getVarStore();
-			List<String> keys = vStore.getFrameKeys();
-			for(String k : keys) {
-				NounMetadata noun = vStore.get(k);
-				PixelDataType type = noun.getNounType();
-				if(type == PixelDataType.FRAME) {
-					try {
-						ITableDataFrame frame = (ITableDataFrame) noun.getValue();
-						pixelRunner.addResult("CACHED_FRAME_HEADERS", 
-								new NounMetadata(frame.getFrameHeadersObject(), PixelDataType.CUSTOM_DATA_STRUCTURE, 
-										PixelOperationType.FRAME_HEADERS), true);
-					} catch(Exception e) {
-						e.printStackTrace();
-						// ignore
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// now rerun the recipe and append to the runner
-		in.runPixel(pixelRunner, recipe);
-		
+		PixelRunner pixelRunner = InsightUtility.recreateInsightState(in);
 		return Response.status(200).entity(PixelStreamUtility.collectPixelData(pixelRunner, null))
 				.header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0")
 				.header("Pragma", "no-cache")
