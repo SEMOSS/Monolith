52b1799123370d3a5ec8a61cfb343b4742b60d7e
mahkhalil <mahkhalil@deloitte.com>
2021-04-16 12:57:19 +0000

    UPDATE ::: Adding first configuration format for the SAML login
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@43460 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/semoss/web/services/saml/SamlDataObject.java
@@ -0,0 +1,55 @@
+package prerna.semoss.web.services.saml;
+
+public class SamlDataObject {
+
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String uid;
+	private String mail;
+
+	public SamlDataObject() {
+
+	}
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+}
