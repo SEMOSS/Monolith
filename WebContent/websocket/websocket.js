0169981b4d8a83c6741ab2f9d312e3e676cdeb89
mahkhalil <mahkhalil@deloitte.com>
2021-07-28 12:06:32 +0000

    UPDATE ::: Adding sample html and js for websocket testing
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@44275 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ WebContent/websocket/websocket.js
@@ -0,0 +1,29 @@
+
+var socket = new WebSocket("ws://localhost:9090/Monolith_Dev/pixelSocket");
+socket.onmessage = onMessage;
+
+function onMessage(event) {
+    var pixelResponse = JSON.parse(event.data);
+    displayPixelResponse(pixelResponse);
+}
+
+function displayPixelResponse(pixelResponse) {
+    var content = document.getElementById("content");
+    var div = document.createElement("div");
+    
+    var html = "<h3>New Pixel</h3>";
+    html += "<p>" +  JSON.stringify(pixelResponse) + "</p>";
+
+    div.innerHTML= html;
+    content.appendChild(div);
+}
+
+function sendSocket() {
+	var insightId = document.getElementById("insightId").value;
+	var pixel = document.getElementById("pixel").value;
+    var message = { 
+    		"insightId" : insightId,
+    		"pixel" : pixel
+    };
+    socket.send(JSON.stringify(message));
+}
