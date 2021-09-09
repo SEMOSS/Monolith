b45d3899d1e35c4189bea23bbc991ff3dc605c4c
Maher Khalil <themaherkhalil@gmail.com>
2021-09-09 02:26:50 +0000

    UPDATE ::: Trying to get csrf to work with scheduling.  TODO: need to determine how to properly get the domain for the necessary cookies to work
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@44834 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: fdf463e31db7769c8709abde1b20e849d839be67

--- src/prerna/web/conf/CustomRestCsrfPreventionFilter.java
+++ src/prerna/web/conf/CustomRestCsrfPreventionFilter.java
@@ -9,12 +9,15 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import prerna.rpa.quartz.jobs.insight.RunPixelJobFromDB;
+
 public class CustomRestCsrfPreventionFilter extends org.apache.catalina.filters.RestCsrfPreventionFilter {
 
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
 		HttpSession session = ((HttpServletRequest) arg0).getSession();
 		session.setAttribute("csrf", true);
+		RunPixelJobFromDB.setFetchCsrf(true);
 		super.doFilter(arg0, arg1, arg2);
 	}
 }
