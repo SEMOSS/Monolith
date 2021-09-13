911400f43cea3a7505744c8a46aa1cdb6858666c
Maher Khalil <themaherkhalil@gmail.com>
2021-09-13 18:18:17 +0000

    UPDATE ::: Adding redundant check in admin config to not allow set initial admin request multiple times
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@44873 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b0543b8350d030e8a7ee335c0663354a4efd752b

--- src/prerna/semoss/web/services/config/AdminConfigService.java
+++ src/prerna/semoss/web/services/config/AdminConfigService.java
@@ -14,16 +14,26 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.owasp.encoder.Encode;
 
 import com.google.gson.Gson;
 
 import prerna.auth.utils.SecurityUpdateUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.util.Constants;
+import prerna.util.Utility;
 import prerna.web.conf.AdminStartupFilter;
 import prerna.web.services.util.WebUtility;
 
 @Path("/")
 public class AdminConfigService {
+	
+	private static final Logger logger = LogManager.getLogger(AdminConfigService.class);
+	
 	private static final Gson GSON = new Gson();
 	public static final String ADMIN_REDIRECT_KEY = "ADMIN_REDIRECT_KEY";
 
@@ -33,6 +43,27 @@ public class AdminConfigService {
 			MultivaluedMap<String, String> form) throws IOException {
 		HttpSession session = request.getSession(false);
 
+		IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
+		String q = "SELECT * FROM SMSS_USER LIMIT 1";
+		IRawSelectWrapper wrapper = null;
+		try {
+			wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
+			boolean hasUser = wrapper.hasNext();
+			// if there are users, redirect to the main semoss page
+			// we do not want to allow the person to make any admin requests
+			if (hasUser) {
+				Map<String, String> errorMap = new HashMap<>();
+				errorMap.put(Constants.ERROR_MESSAGE, "Initial admin has already been set");
+				return WebUtility.getResponse(errorMap, 400);
+			}
+		} catch (Exception e) {
+			logger.error(Constants.STACKTRACE, e);
+		} finally {
+			if (wrapper != null) {
+				wrapper.cleanUp();
+			}
+		}
+		
 		String idString = form.getFirst("ids");
 		if (idString == null || idString.isEmpty()) {
 			Map<String, String> errorMessage = new HashMap<>();
