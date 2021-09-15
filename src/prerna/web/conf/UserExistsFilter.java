dac7e5a0257de8a8f0a71081d056183cc01c59a0
mahkhalil <mahkhalil@deloitte.com>
2021-09-15 14:24:00 +0000

    BUGFIX ::: Fixing the infinite loop that happens when user exists filter is enabled and the user is logging in through some SSO which creates the user object
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@44909 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/UserExistsFilter.java
+++ src/prerna/web/conf/UserExistsFilter.java
@@ -1,6 +1,8 @@
 package prerna.web.conf;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -15,6 +17,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.owasp.encoder.Encode;
 
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.engine.api.IEngine;
@@ -28,12 +32,25 @@ public class UserExistsFilter extends NoUserInSessionFilter {
 	
 	private static final Logger logger = LogManager.getLogger(UserExistsFilter.class);
 
+	// these login types
+	// create a user through SSO
+	// so if we have a user object
+	private static List<AuthProvider> checkLogins = new ArrayList<>();
+	static { 
+		checkLogins.add(AuthProvider.CAC);
+		checkLogins.add(AuthProvider.WINDOWS_USER);
+		checkLogins.add(AuthProvider.SAML);
+	}
+	
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
 			throws IOException, ServletException {
 		if (AbstractSecurityUtils.securityEnabled()) {
-			HttpSession session = ((HttpServletRequest) arg0).getSession(true);
-			User user = (User) session.getAttribute(Constants.SESSION_USER);
+			HttpSession session = ((HttpServletRequest) arg0).getSession(false);
+			User user = null;
+			if(session != null) {
+				user = (User) session.getAttribute(Constants.SESSION_USER);
+			}
 
 			// this will be the full path of the request
 			// like http://localhost:8080/Monolith_Dev/api/engine/runPixel
@@ -55,28 +72,32 @@ public class UserExistsFilter extends NoUserInSessionFilter {
 					return;
 				} else {
 					// okay, need to make sure the user is a valid one
-					IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
-					String q = "SELECT * FROM SMSS_USER WHERE ID='" + user.getAccessToken(user.getLogins().get(0)).getId()
-							+ "'";
-					IRawSelectWrapper wrapper = null;
-					try {
-						wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
-						boolean hasUser = wrapper.hasNext();
-						// this user is not registered
-						// just take them to the login page again
-						if (!hasUser) {
-							((HttpServletResponse) arg1).setStatus(302);
-							String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
-							redirectUrl = redirectUrl + "#!/login";
-							String encodedRedirectUrl = Encode.forHtml(redirectUrl);
-							((HttpServletResponse) arg1).setHeader("redirect", encodedRedirectUrl);
-							((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + encodedRedirectUrl);
-						}
-					} catch (Exception e) {
-						logger.error(Constants.STACKTRACE, e);
-					} finally {
-						if (wrapper != null) {
-							wrapper.cleanUp();
+					AccessToken token = user.getAccessToken(user.getLogins().get(0));
+					boolean userExists = userExists(token);
+					if (!userExists) {
+						session.removeAttribute(Constants.SESSION_USER);
+						((HttpServletResponse) arg1).setStatus(302);
+						String redirectUrl = ((HttpServletRequest) arg0).getHeader("referer");
+						redirectUrl = redirectUrl + "#!/login";
+						String encodedRedirectUrl = Encode.forHtml(redirectUrl);
+						((HttpServletResponse) arg1).setHeader("redirect", encodedRedirectUrl);
+						((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + encodedRedirectUrl);
+						
+						// log the user login
+						logger.info(ResourceUtility.getLogMessage((HttpServletRequest)arg0, session, User.getSingleLogginName(user), "is trying to login BUT doesn't have access with provider " +  token.getProvider()));
+
+						return;
+					}
+				}
+			} else {
+				// for some providers
+				// we need to check if they are valid since its SSO
+				if(user != null && user.getLogins().size() == 1) {
+					AccessToken token = user.getAccessToken(user.getLogins().get(0));
+					if(checkLogins.contains(token.getProvider())) { 
+						boolean userExists = userExists(token);
+						if(!userExists) {
+							session.removeAttribute(Constants.SESSION_USER);
 						}
 					}
 				}
@@ -86,6 +107,24 @@ public class UserExistsFilter extends NoUserInSessionFilter {
 		arg2.doFilter(arg0, arg1);
 	}
 
+	private boolean userExists(AccessToken token) {
+		IEngine engine = Utility.getEngine(Constants.SECURITY_DB);
+		String q = "SELECT * FROM SMSS_USER WHERE ID='" + token.getId() + "'";
+		IRawSelectWrapper wrapper = null;
+		try {
+			wrapper = WrapperManager.getInstance().getRawWrapper(engine, q);
+			return wrapper.hasNext();
+		} catch (Exception e) {
+			logger.error(Constants.STACKTRACE, e);
+		} finally {
+			if (wrapper != null) {
+				wrapper.cleanUp();
+			}
+		}
+		
+		return false;
+	}
+	
 	@Override
 	public void destroy() {
 		// destroy
