4d5033a6e45741d635d54cc58ef55cea703a3f09
mahkhalil <mahkhalil@deloitte.com>
2021-12-10 20:25:02 +0000

    UPDATE ::: Also adding empty init
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@45344 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a1dbf371db55eb21a9851d81b94ad2bc74d20f55

--- src/prerna/web/conf/TrustedTokenFilter.java
+++ src/prerna/web/conf/TrustedTokenFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -71,6 +72,11 @@ public class TrustedTokenFilter implements Filter {
 	public void destroy() {
 		// destroy
 
+	}
+	
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// init
 	}
 
 }
