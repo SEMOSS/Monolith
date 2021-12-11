15083cc228d419fa13b86b456386cc42f675c1cf
mahkhalil <mahkhalil@deloitte.com>
2021-12-11 16:10:57 +0000

    UPDATE ::: Adding logic for trusted token logic to work across containers by utilizing a table in security db
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@45348 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d3caceebb0cdfbb0c0c3901f484e4265e31f6fd9

--- src/prerna/semoss/web/services/config/TrustedTokenService.java
+++ src/prerna/semoss/web/services/config/TrustedTokenService.java
@@ -1,6 +1,9 @@
 package prerna.semoss.web.services.config;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -19,6 +22,8 @@ import org.apache.logging.log4j.Logger;
 
 import com.google.common.cache.CacheBuilder;
 
+import prerna.auth.utils.SecurityTokenUtils;
+import prerna.cluster.util.ClusterUtil;
 import prerna.semoss.web.services.local.ResourceUtility;
 import prerna.web.services.util.WebUtility;
 
@@ -27,10 +32,11 @@ public class TrustedTokenService {
 	
 	private static final Logger logger = LogManager.getLogger(TrustedTokenService.class);
 	
+	private static long expirationMinutes = 120L;
 	private static ConcurrentMap<String, String> tokenStorage = null;
 	static {
 		TrustedTokenService.tokenStorage = CacheBuilder.newBuilder().maximumSize(1000L)
-				.expireAfterWrite(120L, TimeUnit.MINUTES)
+				.expireAfterWrite(expirationMinutes, TimeUnit.MINUTES)
 				.<String, String>build().asMap();
 	}
 	
@@ -38,6 +44,53 @@ public class TrustedTokenService {
 	@Path("/getToken")
 	public Response getToken(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException {
 		String ip = ResourceUtility.getClientIp(request);
+		String token = null;
+		String dateAdded = null;
+		if(ClusterUtil.IS_CLUSTER) {
+			String[] val = getClusterToken(ip);
+			token = val[0];
+			dateAdded = val[1];
+		} else {
+			token = getLocalToken(ip);
+		}
+		
+		Map<String, String> retMap = new HashMap<>();
+		retMap.put("token", token);
+		retMap.put("dateAdded", dateAdded);
+		return WebUtility.getResponse(retMap, 200);
+	}
+	
+	/**
+	 * Store and get the ip in a clustered location
+	 * @param ip
+	 * @return
+	 */
+	private static String[] getClusterToken(String ip) {
+		SecurityTokenUtils.clearExpiredTokens(TrustedTokenService.expirationMinutes);
+		String token = SecurityTokenUtils.getToken(ip);
+		if(token == null) {
+			logger.info("IP = " + ip + ", generating new token id");
+			Object[] genRet = SecurityTokenUtils.generateToken(ip);
+			token = (String) genRet[0];
+			
+			LocalDateTime dateAdded = (LocalDateTime) genRet[1];
+			Calendar cal = (Calendar) genRet[2];
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			
+			String formattedDate = dateAdded.atZone(cal.getTimeZone().toZoneId()).format(formatter);
+			return new String[] {token, formattedDate};
+		}
+
+		logger.info("IP = " + ip + ", requesting existing token id");
+		return new String[] {token, null};
+	}
+	
+	/**
+	 * Store and get the ip locally on the pod
+	 * @param ip
+	 * @return
+	 */
+	private static String getLocalToken(String ip) {
 		String token = null;
 		
 		if(tokenStorage.containsKey(ip)) {
@@ -48,10 +101,8 @@ public class TrustedTokenService {
 			tokenStorage.put(ip, token);
 			logger.info("IP = " + ip + ", generating new token id");
 		}
-
-		Map<String, String> retMap = new HashMap<>();
-		retMap.put("token", token);
-		return WebUtility.getResponse(retMap, 200);
+		
+		return token;
 	}
 	
 	/**
@@ -60,6 +111,11 @@ public class TrustedTokenService {
 	 * @return
 	 */
 	public static String getTokenForIp(String ip) {
+		if(ClusterUtil.IS_CLUSTER) {
+			SecurityTokenUtils.clearExpiredTokens(TrustedTokenService.expirationMinutes);
+			return SecurityTokenUtils.getToken(ip);
+		}
+		
 		return tokenStorage.get(ip);
 	}
 
