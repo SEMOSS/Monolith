f673b0ecb085e4920a83ccdf379cd17fe3f8fac7
mahkhalil <mahkhalil@deloitte.com>
2022-03-03 21:09:19 +0000

    UPDATE ::: Adding logic for locking accounts
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@45759 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/web/conf/AccountLockedFilter.java
@@ -0,0 +1,71 @@
+package prerna.web.conf;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.AccessToken;
+import prerna.auth.User;
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.util.Constants;
+import prerna.util.Utility;
+
+public class AccountLockedFilter implements Filter {
+
+	private static final Logger logger = LogManager.getLogger(AccountLockedFilter.class);
+
+	private static final String SET_ACCOUNT_LOCKED_HTML = "/accountLocked/";
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+		if(AbstractSecurityUtils.securityEnabled()) {
+			HttpSession session = ((HttpServletRequest) arg0).getSession(false);
+			User user = null;
+			if (session != null) {
+				user = (User) session.getAttribute(Constants.SESSION_USER);
+			}
+			
+			if(user != null && !user.isAnonymous()) {
+				AccessToken token = user.getAccessToken(user.getPrimaryLogin());
+				boolean isLocked = token.isLocked();
+				if(isLocked) {
+					logger.info("User " + token.getId() + " is locked and being redirected");
+					// this will be the deployment name of the app
+					String contextPath = arg0.getServletContext().getContextPath();
+					String fullUrl = Utility.cleanHttpResponse(((HttpServletRequest) arg0).getRequestURL().toString());
+
+					// we redirect to the index.html page where we have pushed the admin page
+					String redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length()) + SET_ACCOUNT_LOCKED_HTML;
+					((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
+					((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
+					return;
+				}
+			}
+		}
+
+		arg2.doFilter(arg0, arg1);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+}
