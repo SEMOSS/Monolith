44ce2db2534743c41b79a94f878fedde05ad93e1
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2022-03-27 21:22:45 +0000

    EndPoint - APIResource, Filter - APIFilter API related filters and REST API Main Path - /data/get to instrument API Filter on web.xml.     <filter>       <filter-name>APIFilter</filter-name>       <filter-class>prerna.web.conf.APIFilter</filter-class>    </filter>    <filter-mapping>       <filter-name>APIFilter</filter-name>       <url-pattern>/data/*</url-pattern>    </filter-mapping> Once the insight API is added - use the API key either as a header / through the URI Example : http://server:port/Monolith/data/get?key=8d077288220ea1771a03c32543d06c921d5eca63d7d03085ddf4733a9c6436e8&pass=abcd&sql=select%20*%20from%20diabetes2_FRAME156274 TBD - POST API, iFRAME UI
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@45891 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/semoss/web/services/local/APIResource.java
@@ -0,0 +1,168 @@
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
+package prerna.semoss.web.services.local;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ResourceContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User;
+import prerna.engine.api.IRDBMSEngine;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.util.Constants;
+import prerna.util.Utility;
+import prerna.web.requests.OverrideParametersServletRequest;
+import prerna.web.services.util.WebUtility;
+
+@Path("/")
+public class APIResource {
+
+	private static final Logger logger = LogManager.getLogger(APIResource.class);
+	@Context
+	protected ServletContext context;
+
+	@Path("/get")
+	//@POST
+	@GET
+	//@Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	// gets to this point after a filter
+	// no need to worry about security at this point
+	public StreamingOutput getJSONOutput(
+//			@QueryParam("key") String apiKey, 
+//			@QueryParam("pass") String pass,
+			@Context HttpServletRequest request, 
+			@Context HttpServletResponse response,
+			@Context ResourceContext resourceContext) 
+	{
+		String sql = request.getParameter("sql");
+
+		// get the insight from the session
+		if (sql == null) 
+		{
+			try {
+				sql = IOUtils.toString(request.getReader());
+				if (sql != null && sql.length() != 0) {
+					sql = sql.replace("'", "\\\'");
+					sql = sql.replace("\"", "\\\"");
+					// System.err.println(sql2);
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		String projectId = request.getParameter("projectId");
+		String insightId = request.getParameter("insightId");
+		String outputFormat = request.getParameter("format");
+		
+		if(outputFormat == null)
+			outputFormat = "json";
+		
+		NameServer server = resourceContext.getResource(NameServer.class);
+		OverrideParametersServletRequest requestWrapper = new OverrideParametersServletRequest(request);
+		Map<String, String> paramMap = new HashMap<String, String>();
+
+		// open this insight
+		String pixel = "META | OpenInsight(project=[\"" + projectId + "\"], id=[\"" + insightId
+				+ "\"], additionalPixels=[\"ReadInsightTheme();\"]);";
+		paramMap.put("insightId", "new");
+		paramMap.put("expression", pixel);
+		requestWrapper.setParameters(paramMap);
+		Response resp = server.runPixelSync(requestWrapper);
+
+		StreamingOutput utility = (StreamingOutput) resp.getEntity();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		try 
+		{
+			utility.write(output);
+			String s = new String(output.toByteArray());
+			System.out.println(s);
+			JSONObject obj = new JSONObject(s);
+			// pixelReturn[0].output.insightData.insightId
+			insightId = obj.getJSONArray("pixelReturn").getJSONObject(0).getJSONObject("output")
+					.getJSONObject("insightData").getString("insightID");
+			System.err.println("Insight ID is " + insightId);
+		} 
+		catch (WebApplicationException | IOException e) 
+		{
+			e.printStackTrace();
+		}
+
+		Insight insight = InsightStore.getInstance().get(insightId);
+		if(insight!=null)
+		{
+			Object retOutput = null;
+			if(outputFormat.equalsIgnoreCase("json"))
+			{
+				retOutput = insight.queryJSON(sql, null);
+					return WebUtility.getSOFile(retOutput+"");					
+			}
+			else if(outputFormat.equalsIgnoreCase("csv"))
+			{
+				retOutput = insight.queryCSV(sql, null);
+				if(retOutput != null)
+					return WebUtility.getSOFile(retOutput+"");					
+			}
+		}
+		else 
+			return WebUtility.getSO("No such insight");
+
+		return null;
+	}
+}
