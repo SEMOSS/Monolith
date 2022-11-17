d18117a136c8afc1484c9d74908745fd01a6fdf5
mahkhalil <mahkhalil@deloitte.com>
2022-11-17 20:37:07 +0000

    UPDATE ::: Cleaning up class and allowing public home to work when no security enabled
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@47367 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 493e658aad4d8c78dc3decfe293452790b40c9ad

--- src/prerna/web/conf/PublicHomeCheckFilter.java
+++ src/prerna/web/conf/PublicHomeCheckFilter.java
@@ -16,106 +16,101 @@ import javax.servlet.http.HttpSession;
 import com.google.common.base.Strings;
 
 import prerna.auth.User;
+import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityProjectUtils;
+import prerna.project.api.IProject;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Settings;
 import prerna.util.Utility;
 
 public class PublicHomeCheckFilter implements Filter {
-	
-	public static final String NO_SUCH_APP = "/noSuchApp/";
-	public static final String APP_NOT_PUBLISHED = "/appPublishFail/";
-	
-	
+
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
-		
 		// check to see if the user is logged in
 		// if so pick the user object from session
 		// isolate the index of public_home/ to the next /
 		// see if this user has permission by using the engine map
 		// if yes allow
-		
 		ServletContext context = arg0.getServletContext();
-		
-		
+
 		HttpSession session = ((HttpServletRequest) arg0).getSession(false);
 		String fullUrl = Utility.cleanHttpResponse(((HttpServletRequest) arg0).getRequestURL().toString());
-		
+
 		String public_home = "/public_home";
 		if(DIHelper.getInstance().getProperty(Settings.PUBLIC_HOME) != null) {
 			public_home = DIHelper.getInstance().getProperty(Settings.PUBLIC_HOME);
 		}
-		
+
 		// this will be the deployment name of the app
 		//  Context Path  - this is already / Monolith
 		String contextPath = context.getContextPath() + public_home ;
 		String realPath = context.getRealPath(File.separator);
-		
+
 		// try to get the project id
 		String projectId = fullUrl.substring(fullUrl.indexOf(contextPath) + contextPath.length() + 1);
 		projectId = projectId.substring(0, projectId.indexOf("/"));
-		
-	
+
 		if(!Strings.isNullOrEmpty(projectId)) {
-			// check if the alias exists, if it does the project probably exists
-			String alias = SecurityProjectUtils.getProjectAliasForId(projectId);
+			// check if user can access
+			// first, need security on
+			// second, if public, allow them to go - rest runPixel endpoint still have security enabled 
+			// so this is the case when we want user to go to portal and have a login page there
+			// third, if not public - you must have access
 			
-			if(!Strings.isNullOrEmpty(alias)) {
-				// check if the project is global				
-				boolean allowed = SecurityProjectUtils.projectIsGlobal(projectId);
-				
-				// check if there is a user and the user can access the project
-				if (!allowed && session != null) {
-					User user = (User) session.getAttribute(Constants.SESSION_USER);
-					
-					allowed = user.checkProjectAccess(alias, projectId);
-					
-				}
-				
-
-				if(allowed) {
-					// try to create the public home from scratch
-					File phomeFile = new File(realPath + public_home); 
-					
-					// make the directory if it doesn't exist
-					if(!phomeFile.exists()) {
-						phomeFile.mkdir();
+			if(AbstractSecurityUtils.securityEnabled()) {
+				if(!SecurityProjectUtils.projectIsGlobal(projectId)) {
+					if(session != null) {
+						User user = (User) session.getAttribute(Constants.SESSION_USER);
+						if(!SecurityProjectUtils.userCanViewProject(user, projectId)) {
+							arg1.getWriter().write("User does not have access to this project" );
+							return;	
+						}
+					} else {
+						arg1.getWriter().write("User must be logged in to access this project");
+						return;
 					}
-
-						boolean mapComplete = Utility.getProject(projectId).publish(realPath + public_home);
-						if(mapComplete) {
-							arg2.doFilter(arg0, arg1);
-							return;
-						} else {
-							arg1.getWriter().write("Publish is not enabled on this project or there was an error publishing this project" );
-//							String redirectUrl = fullUrl.substring(0, fullUrl.indexOf(contextPath) + contextPath.length()) + APP_NOT_PUBLISHED;
-//							((HttpServletResponse) arg1).setHeader("redirect", redirectUrl);
-//							((HttpServletResponse) arg1).sendError(302, "Need to redirect to " + redirectUrl);
-							return;
-						}	
-				} else {
-					arg1.getWriter().write("You do not have access to this project" );
-					return;
 				}
 			}
+
+			IProject project = Utility.getProject(projectId);
+			if(project == null) {
+				arg1.getWriter().write("Unable to load project with id='" + projectId + "'");
+				return;	
+			}
+			
+			// try to create the public home from scratch
+			File publicHomeDir = new File(realPath + public_home); 
+			// make the directory if it doesn't exist
+			if(!publicHomeDir.exists()) {
+				publicHomeDir.mkdir();
+			}
+			
+			boolean mapComplete = project.publish(realPath + public_home);
+			if(mapComplete) {
+				arg2.doFilter(arg0, arg1);
+				return;
+			} else {
+				arg1.getWriter().write("Publish is not enabled on this project or there was an error publishing this project" );
+				return;
+			}	
 		}
 
-		arg1.getWriter().write("Cannot find project");
+		arg1.getWriter().write("Improper portal URL - unable to find project ID for the portal");
 		return;
 	}
-		
+
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
