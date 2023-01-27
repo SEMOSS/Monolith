ac738e282487f27987b5b463809a867f3ff87d3b
mahkhalil <mahkhalil@deloitte.com>
2023-01-27 20:25:50 +0000

    UPDATE ::: Adding ability to chnage ad password w/o logging in since endpoint requires a valid user/current pass
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@47818 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/NoUserInSessionFilter.java
+++ src/prerna/web/conf/NoUserInSessionFilter.java
@@ -72,6 +72,7 @@ public class NoUserInSessionFilter implements Filter {
 		ignoreDueToFE.add("auth/loginsAllowed");
 		ignoreDueToFE.add("auth/login");
 		ignoreDueToFE.add("auth/loginLDAP");
+		ignoreDueToFE.add("auth/changeADPassword");
 		ignoreDueToFE.add("auth/loginLinOTP");
 		ignoreDueToFE.add("auth/createUser");
 		ignoreDueToFE.add("auth/whoami");
