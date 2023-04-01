4581d11f7c6e8d2f24accdb9eac8a7fbbd358861
mahkhalil <mahkhalil@deloitte.com>
2023-04-01 13:59:52 +0000

    UPDATE ::: Allowing basic authentication for login
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@48208 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/web/conf/TrustedTokenFilter.java
+++ src/prerna/web/conf/TrustedTokenFilter.java
@@ -1,6 +1,7 @@
 package prerna.web.conf;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -17,32 +18,27 @@ import org.apache.logging.log4j.Logger;
 import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
+import prerna.auth.utils.SecurityAPIUserUtils;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.semoss.web.services.config.TrustedTokenService;
 import prerna.semoss.web.services.local.ResourceUtility;
 import prerna.util.Constants;
 import prerna.util.SocialPropertiesUtil;
+import prerna.util.Utility;
 
 public class TrustedTokenFilter implements Filter {
 
 	private static final Logger logger = LogManager.getLogger(RDBMSNativeEngine.class);
-	
+
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) arg0;
-		String authValue = request.getHeader("Authorization");
-		if(authValue == null || !authValue.contains("Bearer")) {
-			// no token? just go through and other filters will validate
-			arg2.doFilter(arg0, arg1);
-			return;
-		}
-		
 		HttpSession session = request.getSession(false);
 		User user = null;
 		if(session != null) {
 			user = (User) session.getAttribute(Constants.SESSION_USER);
 		}
-		
+
 		if(user == null) {
 			SocialPropertiesUtil socialData = SocialPropertiesUtil.getInstance();
 			if(socialData.getLoginsAllowed().get("api_user") == null || !socialData.getLoginsAllowed().get("api_user")) {
@@ -51,58 +47,101 @@ public class TrustedTokenFilter implements Filter {
 				return;
 			}
 			
-			// okay, we have a token
-			// and no current user
-			// we have to validate this stuff
-			String ip = ResourceUtility.getClientIp(request);
-			Object[] tokenDetails = TrustedTokenService.getTokenForIp(ip);
-			if(tokenDetails == null) {
-				// token not found for this ip
+			
+			boolean dynamicToken = Utility.getApplicationRequireDynamicToken();
+			String authValue = request.getHeader("authorization");
+			if(authValue == null) {
+				// no token? just go through and other filters will validate
+				arg2.doFilter(arg0, arg1);
+				return;
+			}
+			if(dynamicToken && !authValue.contains("Bearer")) {
+				// no bearer token
+				arg2.doFilter(arg0, arg1);
+				return;
+			} else if(!dynamicToken && !authValue.contains("Basic")) {
+				// no basic token
 				arg2.doFilter(arg0, arg1);
 				return;
 			}
 			
-			String ipToken = (String) tokenDetails[0];
-			authValue = authValue.replace("Bearer ", "");
-			
-			String userId = (String) tokenDetails[2];
-			
-			// error handling
-			if(ipToken == null) {
-				logger.error(Constants.STACKTRACE, "This application does not have a valid trusted token or token has expired");
-				arg2.doFilter(arg0, arg1);
-				return;
+			if(!dynamicToken) {
+				authValue = authValue.replace("Basic ", "");
+				// this is a base64 encoded username:password
+				byte[] decodedBytes = Base64.getDecoder().decode(authValue);
+				String userpass = new String(decodedBytes);
+				String[] split = userpass.split(":");
+				String clientId = split[0];
+				String secretKey = split[1];
+				
+				// can you login?
+				if(SecurityAPIUserUtils.validCredentials(clientId, secretKey)) {
+					
+					AccessToken token = new AccessToken();
+					token.setId(clientId);
+					token.setProvider(AuthProvider.API_USER);
+					user = new User();
+					user.setAccessToken(token);
+					session = request.getSession(true);
+					session.setAttribute(Constants.SESSION_USER, user);
+					session.setAttribute(Constants.SESSION_USER_ID_LOG, token.getId());
+					
+					logger.info(ResourceUtility.getLogMessage(request, session, User.getSingleLogginName(user), "is logging in with provider " +  token.getProvider()));
+				} else {
+					logger.error(ResourceUtility.getLogMessage(request, request.getSession(false), null, "is trying to login as API_USER with invalid credentails using client id = '" + clientId + "'"));
+				}
+				
+			} else {
+				authValue = authValue.replace("Bearer ", "");
+
+				// okay, we have a token
+				// and no current user
+				// we have to validate this stuff
+				String ip = ResourceUtility.getClientIp(request);
+				Object[] tokenDetails = TrustedTokenService.getTokenForIp(ip);
+				if(tokenDetails == null) {
+					// token not found for this ip
+					arg2.doFilter(arg0, arg1);
+					return;
+				}
+	
+				String ipToken = (String) tokenDetails[0];
+	
+				String userId = (String) tokenDetails[2];
+	
+				// error handling
+				if(ipToken == null) {
+					logger.error(ResourceUtility.getLogMessage(request, request.getSession(false), null, "is trying to login as API_USER but does not have a valid trust token or token has expired"));
+					arg2.doFilter(arg0, arg1);
+					return;
+				}
+				if(!ipToken.equals(authValue)) {
+					logger.error(ResourceUtility.getLogMessage(request, request.getSession(false), null, "is trying to login as API_USER but token value is invalid"));
+					arg2.doFilter(arg0, arg1);
+					return;
+				}
+	
+				AccessToken token = new AccessToken();
+				token.setId(userId);
+				token.setProvider(AuthProvider.API_USER);
+				user = new User();
+				user.setAccessToken(token);
+				session = request.getSession(true);
+				session.setAttribute(Constants.SESSION_USER, user);
+				session.setAttribute(Constants.SESSION_USER_ID_LOG, token.getId());
+				logger.info(ResourceUtility.getLogMessage(request, session, User.getSingleLogginName(user), "is logging in with provider " +  token.getProvider()));
 			}
-			if(!ipToken.equals(authValue)) {
-				logger.error(Constants.STACKTRACE, "The token value is invalid");
-				arg2.doFilter(arg0, arg1);
-				return;
-			}
-			if(userId == null || (userId = userId.trim()).isEmpty()) {
-				logger.error(Constants.STACKTRACE, "The client id must be defined");
-				arg2.doFilter(arg0, arg1);
-				return;
-			}
-			
-			AccessToken token = new AccessToken();
-			token.setId(userId);
-			token.setProvider(AuthProvider.API_USER);
-			user = new User();
-			user.setAccessToken(token);
-			session = request.getSession(true);
-			session.setAttribute(Constants.SESSION_USER, user);
-			session.setAttribute(Constants.SESSION_USER_ID_LOG, token.getId());
 		}
-		
+
 		arg2.doFilter(arg0, arg1);
 	}
-	
+
 	@Override
 	public void destroy() {
 		// destroy
 
 	}
-	
+
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		// init
