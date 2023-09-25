0441e6de28d0996ed2b6d90c7d983c0887a6acb8
mahkhalil <mahkhalil@deloitte.com>
2023-09-25 23:57:53 +0000

    UPDATE ::: Additional consolidation for engine level methods and removing implementation specific implementations
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@49592 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/local/DatabaseEngineResource.java
+++ src/prerna/semoss/web/services/local/DatabaseEngineResource.java
@@ -1,33 +1,22 @@
 package prerna.semoss.web.services.local;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -39,12 +28,7 @@ import prerna.cluster.util.ClusterUtil;
 import prerna.engine.api.IDatabaseEngine;
 import prerna.engine.api.IEngine;
 import prerna.engine.impl.SmssUtilities;
-import prerna.io.connector.couch.CouchException;
-import prerna.io.connector.couch.CouchUtil;
-import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.util.Constants;
-import prerna.util.DIHelper;
-import prerna.util.DefaultImageGeneratorUtil;
 import prerna.util.Utility;
 import prerna.web.services.util.WebUtility;
 
@@ -53,7 +37,7 @@ public class DatabaseEngineResource {
 
 	private static final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
 	
-	private static final Logger logger = LogManager.getLogger(DatabaseEngineResource.class);
+	private static final Logger classLogger = LogManager.getLogger(DatabaseEngineResource.class);
 	
 	private boolean canViewDatabase(User user, String databaseId) throws IllegalAccessException {
 		databaseId = SecurityQueryUtils.testUserEngineIdForAlias(user, databaseId);
@@ -135,20 +119,20 @@ public class DatabaseEngineResource {
 			}
 			engine.open(currentSmssFileLocation);
 		} catch(Exception e) {
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			// reset the values
 			try {
 				// close the database again
 				engine.close();
 			} catch (IOException e1) {
-				logger.error(Constants.STACKTRACE, e1);
+				classLogger.error(Constants.STACKTRACE, e1);
 			}
 			currentSmssFile.delete();
 			try (FileWriter fw = new FileWriter(currentSmssFile, false)){
 				fw.write(currentSmssContent);
 				engine.open(currentSmssFileLocation);
 			} catch(Exception e2) {
-				logger.error(Constants.STACKTRACE, e2);
+				classLogger.error(Constants.STACKTRACE, e2);
 				Map<String, String> errorMap = new HashMap<>();
 				errorMap.put(Constants.ERROR_MESSAGE, "A fatal error occurred and could not revert the database to an operational state. Detailed message = " + e2.getMessage());
 				return WebUtility.getResponse(errorMap, 400);
@@ -166,156 +150,4 @@ public class DatabaseEngineResource {
 		return WebUtility.getResponse(success, 200);
 	}
 	
-	
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	/*
-	 * Code below is around database images
-	 */
-	
-	@GET
-	@Path("/appImage/download")
-	@Produces({MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_SVG_XML})
-	public Response imageDownload(@Context final Request coreRequest, @Context HttpServletRequest request, @PathParam("databaseId") String databaseId) {
-		User user = null;
-		try {
-			user = ResourceUtility.getUser(request);
-		} catch (IllegalAccessException e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put("error", "User session is invalid");
-			return WebUtility.getResponse(errorMap, 401);
-		}
-		try {
-			canViewDatabase(user, databaseId);
-		} catch (IllegalAccessException e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put("error", e.getMessage());
-			return WebUtility.getResponse(errorMap, 401);
-		}
-		
-		if(CouchUtil.COUCH_ENABLED) {
-			try {
-				String actualDatabaseId = MasterDatabaseUtility.testDatabaseIdIfAlias(databaseId);
-				Map<String, String> selectors = new HashMap<>();
-				selectors.put(CouchUtil.DATABASE, actualDatabaseId);
-				return CouchUtil.download(CouchUtil.DATABASE, selectors);
-			} catch (CouchException e) {
-				logger.error(Constants.STACKTRACE, e);
-			}
-		}
-		
-		File exportFile = getDatabaseImageFile(databaseId);
-		if(exportFile != null && exportFile.exists()) {
-			String exportName = databaseId + "_Image." + FilenameUtils.getExtension(exportFile.getAbsolutePath());
-			// want to cache this on browser if user has access
-//			CacheControl cc = new CacheControl();
-//			cc.setMaxAge(86400);
-//			cc.setPrivate(true);
-//			cc.setMustRevalidate(true);
-		    EntityTag etag = new EntityTag(Long.toString(exportFile.lastModified()));
-		    ResponseBuilder builder = coreRequest.evaluatePreconditions(etag);
-
-		    // cached resource did not change
-		    if(builder != null) {
-		        return builder.build();
-		    }
-		    
-			return Response.status(200).entity(exportFile).header("Content-Disposition", "attachment; filename=" + exportName)
-//					.cacheControl(cc)
-					.tag(etag)
-//					.lastModified(new Date(exportFile.lastModified()))
-					.build();
-		} else {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put(Constants.ERROR_MESSAGE, "error sending image file");
-			return WebUtility.getResponse(errorMap, 400);
-		}
-	}
-	
-	/**
-	 * Use to find the file for the image
-	 * @param databaseId
-	 * @return
-	 */
-	protected File getDatabaseImageFile(String databaseId) {
-		databaseId = MasterDatabaseUtility.testDatabaseIdIfAlias(databaseId);
-		if(ClusterUtil.IS_CLUSTER){
-			return ClusterUtil.getDatabaseImage(databaseId);
-		}
-		String propFileLoc = (String) DIHelper.getInstance().getEngineProperty(databaseId + "_" + Constants.STORE);
-		if(propFileLoc == null && !databaseId.equals("NEWSEMOSSAPP")) {
-			String imageDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/images/stock/";
-			return new File(imageDir + "color-logo.png");
-		}
-		Properties prop = Utility.loadProperties(propFileLoc);
-		String databaseName = prop.getProperty(Constants.ENGINE_ALIAS);
-		
-		String baseFolder = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
-		String fileLocation = baseFolder 
-				+ DIR_SEPARATOR + Constants.DB_FOLDER 
-				+ DIR_SEPARATOR + SmssUtilities.getUniqueName(databaseName, databaseId) 
-				+ DIR_SEPARATOR + "app_root" 
-				+ DIR_SEPARATOR + "version";
-
-		File f = findImageFile(fileLocation);
-		if(f != null) {
-			return f;
-		} else {
-			// make the image
-			f = new File(fileLocation);
-			if(!f.exists()) {
-			Boolean success = f.mkdirs();
-			if(!success) {
-				logger.info("Unable to make direction at location: " + Utility.cleanLogString(fileLocation));
-			}
-			}
-			fileLocation = fileLocation + DIR_SEPARATOR + "image.png";
-			return DefaultImageGeneratorUtil.pickRandomImage(fileLocation);
-		}
-	}
-	
-	/////////////////////////////////////////////////////////////////
-	
-	/*
-	 * Image utility methods
-	 */
-	
-	/**
-	 * Find an image in the directory
-	 * @param baseDir
-	 * @return
-	 */
-	private File findImageFile(String baseDir) {
-		List<String> extensions = new Vector<>();
-		extensions.add("image.png");
-		extensions.add("image.jpeg");
-		extensions.add("image.jpg");
-		extensions.add("image.gif");
-		extensions.add("image.svg");
-		FileFilter imageExtensionFilter = new WildcardFileFilter(extensions);
-		File baseFolder = new File(baseDir);
-		File[] imageFiles = baseFolder.listFiles(imageExtensionFilter);
-		if(imageFiles != null && imageFiles.length > 0) {
-			return imageFiles[0];
-		}
-		return null;
-	}
-	
-	/**
-	 * Close a file stream
-	 * @param fis
-	 */
-	protected void closeStream(FileInputStream fis) {
-		if(fis != null) {
-			try {
-				fis.close();
-			} catch (IOException e) {
-	    		logger.error(Constants.STACKTRACE, e);
-			}
-		}
-	}
 }
