714225982031c95d27b718373122e7117d693c34
Maher Khalil <themaherkhalil@gmail.com>
2023-10-11 23:08:03 +0000

    UPDATE ::: Removing necessity for specific engine methods for updating smss file and moving to general EngineRouteResource UPDATE ::: Adding proper null pointer checks on close() for AbstractModelEngine
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@49715 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e9d78cfc9fab483497992faf4ec1159dc25c3200

--- src/prerna/semoss/web/services/local/StorageEngineResource.java
+++ src/prerna/semoss/web/services/local/StorageEngineResource.java
@@ -1,43 +1,20 @@
 package prerna.semoss.web.services.local;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import prerna.auth.User;
-import prerna.auth.utils.SecurityAdminUtils;
 import prerna.auth.utils.SecurityEngineUtils;
 import prerna.auth.utils.SecurityQueryUtils;
-import prerna.cluster.util.ClusterUtil;
-import prerna.engine.api.IEngine;
-import prerna.engine.api.IStorageEngine;
-import prerna.engine.impl.SmssUtilities;
-import prerna.util.Constants;
-import prerna.util.Utility;
-import prerna.web.services.util.WebUtility;
 
 @Path("/storage-{storageId}")
 public class StorageEngineResource {
 
 	private static final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
 	
-	private static final Logger logger = LogManager.getLogger(StorageEngineResource.class);
+	private static final Logger classLogger = LogManager.getLogger(StorageEngineResource.class);
 	
 	private boolean canViewStorage(User user, String storageId) throws IllegalAccessException {
 		storageId = SecurityQueryUtils.testUserEngineIdForAlias(user, storageId);
@@ -55,99 +32,5 @@ public class StorageEngineResource {
 	///////////////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	@POST
-	@Path("/updateSmssFile")
-	@Produces("application/json;charset=utf-8")
-	public Response updateSmssFile(@Context HttpServletRequest request, @PathParam("storageId") String storageId) {
-		User user = null;
-		try {
-			user = ResourceUtility.getUser(request);
-		} catch (IllegalAccessException e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put("error", "User session is invalid");
-			return WebUtility.getResponse(errorMap, 401);
-		}
-		try {
-			boolean isAdmin = SecurityAdminUtils.userIsAdmin(user);
-			if(!isAdmin) {
-				boolean isOwner = SecurityEngineUtils.userIsOwner(user, storageId);
-				if(!isOwner) {
-					throw new IllegalAccessException("Storage " + storageId + " does not exist or user does not have permissions to update the smss. User must be the owner to perform this function.");
-				}
-			}
-		} catch (IllegalAccessException e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put("error", e.getMessage());
-			return WebUtility.getResponse(errorMap, 401);
-		}
-
-		IStorageEngine engine = Utility.getStorage(storageId);
-		String currentSmssFileLocation = engine.getSmssFilePath();
-		File currentSmssFile = new File(currentSmssFileLocation);
-		if(!currentSmssFile.exists() || !currentSmssFile.isFile()) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put(Constants.ERROR_MESSAGE, "Could not find current storage smss file");
-			return WebUtility.getResponse(errorMap, 400);
-		}
-		
-		// using the current smss properties
-		// and the new file contents
-		// unconceal any hidden values that have not been altered
-		Properties currentSmssProperties = engine.getSmssProp();
-		String newSmssContent = request.getParameter("smss");
-		String unconcealedNewSmssContent = SmssUtilities.unconcealSmssSensitiveInfo(newSmssContent, currentSmssProperties);
-		
-		// read the current smss as text in case of an error
-		String currentSmssContent = null;
-		try {
-			currentSmssContent = new String(Files.readAllBytes(Paths.get(currentSmssFile.toURI())));
-		} catch (IOException e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put(Constants.ERROR_MESSAGE, "An error occurred reading the current storage smss details. Detailed message = " + e.getMessage());
-			return WebUtility.getResponse(errorMap, 400);
-		}
-		try {
-			engine.close();
-		} catch (Exception e) {
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put(Constants.ERROR_MESSAGE, "An error occurred closing the connection to the storage. Detailed message = " + e.getMessage());
-			return WebUtility.getResponse(errorMap, 400);
-		}
-		try {
-			try (FileWriter fw = new FileWriter(currentSmssFile, false)){
-				fw.write(unconcealedNewSmssContent);
-			}
-			engine.open(currentSmssFileLocation);
-		} catch(Exception e) {
-			logger.error(Constants.STACKTRACE, e);
-			// reset the values
-			try {
-				// close the storage engine again
-				engine.close();
-			} catch (IOException e1) {
-				logger.error(Constants.STACKTRACE, e1);
-			}
-			currentSmssFile.delete();
-			try (FileWriter fw = new FileWriter(currentSmssFile, false)){
-				fw.write(currentSmssContent);
-				engine.open(currentSmssFileLocation);
-			} catch(Exception e2) {
-				logger.error(Constants.STACKTRACE, e2);
-				Map<String, String> errorMap = new HashMap<>();
-				errorMap.put(Constants.ERROR_MESSAGE, "A fatal error occurred and could not revert the storage to an operational state. Detailed message = " + e2.getMessage());
-				return WebUtility.getResponse(errorMap, 400);
-			}
-			Map<String, String> errorMap = new HashMap<>();
-			errorMap.put(Constants.ERROR_MESSAGE, "An error occurred initializing the new storage details. Detailed message = " + e.getMessage());
-			return WebUtility.getResponse(errorMap, 400);
-		}
-		
-		// push to cloud
-		ClusterUtil.pushEngineSmss(storageId, IEngine.CATALOG_TYPE.STORAGE);
-		
-		Map<String, Object> success = new HashMap<>();
-		success.put("success", true);
-		return WebUtility.getResponse(success, 200);
-	}
 	
 }
