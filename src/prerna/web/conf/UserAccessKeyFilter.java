24d97aebbe6ffb31d7c33ce13357aa8de3bd4d27
Maher Khalil <themaherkhalil@gmail.com>
2024-02-07 00:47:12 +0000

    UPDATE ::: Flushing the user on any request instead of only those which are blocked by security
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@50327 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b5ddd5fb5d3302c62a7982f040389893ec0ba8ae

--- src/prerna/web/conf/UserAccessKeyFilter.java
+++ src/prerna/web/conf/UserAccessKeyFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -22,7 +23,6 @@ import prerna.auth.utils.SecurityUserAccessKeyUtils;
 import prerna.semoss.web.services.local.ResourceUtility;
 import prerna.util.Constants;
 import prerna.util.SocialPropertiesUtil;
-import prerna.util.Utility;
 
 public class UserAccessKeyFilter implements Filter {
 
@@ -36,71 +36,76 @@ public class UserAccessKeyFilter implements Filter {
 		if(session != null) {
 			user = (User) session.getAttribute(Constants.SESSION_USER);
 		}
+		
+		if(user != null) {
+			arg2.doFilter(arg0, arg1);
+			return;
+		} 
+		
+		// see if there is an auth value
+		String authValue = request.getHeader("Authorization");
+		if(authValue == null) {
+			authValue = request.getHeader("authorization");
+			if(authValue == null) {
+				// no token? just go through and other filters will validate
+				arg2.doFilter(arg0, arg1);
+				return;
+			}
+		}
 
-		if(user == null) {
-			String fullUrl = Utility.cleanHttpResponse(request.getRequestURL().toString());
-			if (!ResourceUtility.allowAccessWithoutUsers(fullUrl)) {
-				String authValue = request.getHeader("Authorization");
-				if(authValue == null) {
-					authValue = request.getHeader("authorization");
-					if(authValue == null) {
-						// no token? just go through and other filters will validate
-						arg2.doFilter(arg0, arg1);
-						return;
+		authValue = authValue.replace("Basic", "").trim();
+		// this is a base64 encoded username:password
+		byte[] decodedBytes = Base64.getDecoder().decode(authValue);
+		String userpass = new String(decodedBytes);
+		if(userpass != null && !userpass.isEmpty()) {
+			String[] split = userpass.split(":");
+			if(split != null && split.length == 2) {
+				String accessKey = split[0];
+				String secretKey = split[1];
+
+				try {
+					user = SecurityUserAccessKeyUtils.validateKeysAndReturnUser(accessKey, secretKey);
+				} catch (IllegalAccessException e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				}
+				if(user == null) {
+					classLogger.error(ResourceUtility.getLogMessage(request, request.getSession(false), null, "could not login using user access key '"+accessKey+"' with invalid secret key"));
+				}
+
+				AccessToken token = null;
+				if(user != null) {
+					token = user.getPrimaryLoginToken();
+					// let us make sure this login type is still allowed to login via access/secret key
+					{
+						AuthProvider provider = token.getProvider();
+						String prefix = provider.toString().toLowerCase();
+						boolean accessKeysAllowed = Boolean.parseBoolean(SocialPropertiesUtil.getInstance().getProperty(prefix + "_access_keys_allowed")+"");
+						if(!accessKeysAllowed) {
+							classLogger.error(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to login using access/secret key but administrator has disabeled for provider "+provider.name()));
+							user = null;
+							token = null;
+						}
 					}
 				}
-				
-				authValue = authValue.replace("Basic", "").trim();
-				// this is a base64 encoded username:password
-				byte[] decodedBytes = Base64.getDecoder().decode(authValue);
-				String userpass = new String(decodedBytes);
-				if(userpass != null && !userpass.isEmpty()) {
-					String[] split = userpass.split(":");
-					if(split != null && split.length == 2) {
-						String accessKey = split[0];
-						String secretKey = split[1];
-						
-						try {
-							user = SecurityUserAccessKeyUtils.validateKeysAndReturnUser(accessKey, secretKey);
-						} catch (IllegalAccessException e) {
-							classLogger.error(Constants.STACKTRACE, e);
-						}
-						if(user == null) {
-							classLogger.error(ResourceUtility.getLogMessage(request, request.getSession(false), null, "could not login using user access key '"+accessKey+"' with invalid secret key"));
-						}
-						
-						AccessToken token = null;
-						if(user != null) {
-							token = user.getPrimaryLoginToken();
-							// let us make sure this login type is still allowed to login via access/secret key
-							{
-								AuthProvider provider = token.getProvider();
-								String prefix = provider.toString().toLowerCase();
-								boolean accessKeysAllowed = Boolean.parseBoolean(SocialPropertiesUtil.getInstance().getProperty(prefix + "_access_keys_allowed")+"");
-								if(!accessKeysAllowed) {
-									classLogger.error(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to login using access/secret key but administrator has disabeled for provider "+provider.name()));
-									user = null;
-									token = null;
-								}
-							}
-						}
-						
-						if(user != null && token != null) {
-							SecurityUserAccessKeyUtils.updateAccessTokenLastUsed(accessKey);
-							session = request.getSession(true);
-							session.setAttribute(Constants.SESSION_USER, user);
-							session.setAttribute(Constants.SESSION_USER_ID_LOG, token.getId());
-							classLogger.info(ResourceUtility.getLogMessage(request, session, User.getSingleLogginName(user), "is logging in with provider " +  token.getProvider() + " with user access key"));
-						}
-					}
+
+				if(user != null && token != null) {
+					SecurityUserAccessKeyUtils.updateAccessTokenLastUsed(accessKey);
+					session = request.getSession(true);
+					session.setAttribute(Constants.SESSION_USER, user);
+					session.setAttribute(Constants.SESSION_USER_ID_LOG, token.getId());
+					classLogger.info(ResourceUtility.getLogMessage(request, session, User.getSingleLogginName(user), "is logging in with provider " +  token.getProvider() + " with user access key"));
 				}
 			}
 		}
+		
 
 		// doesn't matter if we made a user or didn't
 		// we will continue the filter chain because the {@link NoUserInSessionFilter} 
 		// will catch unauthorized access
-		arg2.doFilter(arg0, arg1);
+		
+		// wrap the request to allow subsequent reading
+        HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(request);
+		arg2.doFilter(requestWrapper, arg1);
 	}
 
 	@Override
