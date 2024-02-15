c4a4393765bbfe6955dd7373173e5614c9647431
mahkhalil <mahkhalil@deloitte.com>
2024-02-15 22:04:49 +0000

    UPDATE ::: Adding in IS_CUSTOM_GROUP to differentiate between IdP groups vs. custom groups UPDATE ::: Pushing updated OWLs
    git-svn-id: svn://107.170.10.188:8080/Monolith/dev@50367 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/semoss/web/services/local/auth/AdminGroupAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/AdminGroupAuthorizationResource.java
@@ -11,7 +11,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
@@ -27,51 +26,61 @@ import prerna.web.services.util.WebUtility;
 @Path("/auth/admin/group")
 public class AdminGroupAuthorizationResource extends AbstractAdminResource {
 	
-	private static final Logger logger = LogManager.getLogger(AdminGroupAuthorizationResource.class);
+	private static final Logger classLogger = LogManager.getLogger(AdminGroupAuthorizationResource.class);
 	
 	@POST
 	@Produces("application/json")
 	@Path("/addGroup")
-	public Response addGroup(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
-		Hashtable<String, String> errorRet = new Hashtable<>();
+	public Response addGroup(@Context HttpServletRequest request) {
+		Map<String, String> errorRet = new Hashtable<>();
 		User user = null;
 		try {
 			user = ResourceUtility.getUser(request);
 		} catch (IllegalAccessException e) {
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to add a group but couldn't find user session"));
 			Map<String, String> errorMap = new HashMap<>();
 			errorMap.put(Constants.ERROR_MESSAGE, "User session is invalid");
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
 		if(!SecurityAdminUtils.userIsAdmin(user)){
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to add a group but is not an admin"));
 			errorRet.put(Constants.ERROR_MESSAGE, "The user doesn't have the permissions to perform this action.");
 			return WebUtility.getResponse(errorRet, 400);
 		}
 		
 		boolean success = false;
 		try {
-			String newGroupId = form.getFirst("groupId");
+			String newGroupId = request.getParameter("groupId");
 			if(newGroupId == null || (newGroupId = newGroupId.trim()).isEmpty()) {
 				throw new IllegalArgumentException("The group id cannot be null or empty");
 			}
-			String type = form.getFirst("type");
-			if(type == null || (type = type.trim()).isEmpty()) {
-				throw new IllegalArgumentException("The group type cannot be null or empty");
+			String type = request.getParameter("type");
+			if(type == null) {
+				type = "";
 			}
-			String description = form.getFirst("description");
+			String description = request.getParameter("description");
 			if(description == null) {
 				description = "";
 			}
 			description = description.trim();
+			boolean isCustomGroup = Boolean.parseBoolean(request.getParameter("isCustomGroup")+"");
 			
-			SecurityGroupUtils.getInstance(user).addGroup(newGroupId, type, description);
+			// you can have a type or be a custom group
+			// cannot be both
+			if(!isCustomGroup && (type == null || (type = type.trim()).isEmpty()) ) {
+				throw new IllegalArgumentException("The group type cannot be null or empty if this is not a custom group");
+			} else if(isCustomGroup && (type != null && !(type = type.trim()).isEmpty()) ) {
+				throw new IllegalArgumentException("A custom group cannot have a login type passed in");
+			}
 			
+			SecurityGroupUtils.getInstance(user).addGroup(newGroupId, type, description, isCustomGroup);
 		} catch (IllegalArgumentException e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, e.getMessage());
 			return WebUtility.getResponse(errorRet, 400);
 		} catch (Exception e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, "An unexpected error happened. Please try again.");
 			return WebUtility.getResponse(errorRet, 500);
 		}
@@ -81,40 +90,42 @@ public class AdminGroupAuthorizationResource extends AbstractAdminResource {
 	@POST
 	@Produces("application/json")
 	@Path("/deleteGroup")
-	public Response deleteGroup(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
-		Hashtable<String, String> errorRet = new Hashtable<>();
+	public Response deleteGroup(@Context HttpServletRequest request) {
+		Map<String, String> errorRet = new Hashtable<>();
 		User user = null;
 		try {
 			user = ResourceUtility.getUser(request);
 		} catch (IllegalAccessException e) {
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to remove a group but couldn't find user session"));
 			Map<String, String> errorMap = new HashMap<>();
 			errorMap.put(Constants.ERROR_MESSAGE, "User session is invalid");
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
 		if(!SecurityAdminUtils.userIsAdmin(user)){
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to remove a group but is not an admin"));
 			errorRet.put(Constants.ERROR_MESSAGE, "The user doesn't have the permissions to perform this action.");
 			return WebUtility.getResponse(errorRet, 400);
 		}
 		
 		boolean success = false;
 		try {
-			String groupId = form.getFirst("groupId");
+			String groupId = request.getParameter("groupId");
 			if(groupId == null || (groupId = groupId.trim()).isEmpty()) {
 				throw new IllegalArgumentException("The group id cannot be null or empty");
 			}
-			String type = form.getFirst("type");
-			if(type == null || (type = type.trim()).isEmpty()) {
-				throw new IllegalArgumentException("The group type cannot be null or empty");
+			String type = request.getParameter("type");
+			if(type == null) {
+				type = "";
 			}
 			
 			SecurityGroupUtils.getInstance(user).deleteGroupAndPropagate(groupId, type);
 		} catch (IllegalArgumentException e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, e.getMessage());
 			return WebUtility.getResponse(errorRet, 400);
 		} catch (Exception e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, "An unexpected error happened. Please try again.");
 			return WebUtility.getResponse(errorRet, 500);
 		}
@@ -124,54 +135,62 @@ public class AdminGroupAuthorizationResource extends AbstractAdminResource {
 	@POST
 	@Produces("application/json")
 	@Path("/editGroup")
-	public Response editGroup(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
-		Hashtable<String, String> errorRet = new Hashtable<>();
+	public Response editGroup(@Context HttpServletRequest request) {
+		Map<String, String> errorRet = new Hashtable<>();
 		User user = null;
 		try {
 			user = ResourceUtility.getUser(request);
 		} catch (IllegalAccessException e) {
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to edit a group but couldn't find user session"));
 			Map<String, String> errorMap = new HashMap<>();
 			errorMap.put(Constants.ERROR_MESSAGE, "User session is invalid");
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
 		if(!SecurityAdminUtils.userIsAdmin(user)){
+			classLogger.warn(ResourceUtility.getLogMessage(request, request.getSession(false), User.getSingleLogginName(user), "is trying to edit a group but is not an admin"));
 			errorRet.put(Constants.ERROR_MESSAGE, "The user doesn't have the permissions to perform this action.");
 			return WebUtility.getResponse(errorRet, 400);
 		}
 		
 		boolean success = false;
 		try {
-			String groupId = form.getFirst("groupId");
+			String groupId = request.getParameter("groupId");
 			if(groupId == null || (groupId = groupId.trim()).isEmpty()) {
 				throw new IllegalArgumentException("The group id cannot be null or empty");
 			}
-			String type = form.getFirst("type");
-			if(type == null || (type = type.trim()).isEmpty()) {
-				throw new IllegalArgumentException("The group type cannot be null or empty");
+			String type = request.getParameter("type");
+			if(type == null) {
+				type = "";
 			}
-			String newGroupId = form.getFirst("newGroupId");
+			boolean isCustomGroup = Boolean.parseBoolean(request.getParameter("isCustomGroup")+"");
+
+			String newGroupId = request.getParameter("newGroupId");
 			if(newGroupId == null || (newGroupId = newGroupId.trim()).isEmpty()) {
 				throw new IllegalArgumentException("The new group id cannot be null or empty");
 			}
-			String newType = form.getFirst("newType");
-			if(newType == null || (newType = newType.trim()).isEmpty()) {
-				throw new IllegalArgumentException("The new group type cannot be null or empty");
+			String newType = request.getParameter("newType");
+			if(newType == null) {
+				newType = "";
 			}
-			String description = form.getFirst("description");
-			if(description == null) {
-				description = "";
+			boolean newCustomGroup = Boolean.parseBoolean(request.getParameter("newIsCustomGroup")+"");
+			String newDescription = request.getParameter("newDescription");
+			
+			// you can have a type or be a custom group
+			// cannot be both
+			if(!newCustomGroup && (newType == null || (newType = newType.trim()).isEmpty()) ) {
+				throw new IllegalArgumentException("The group type cannot be null or empty if this is not a custom group");
+			} else if(newCustomGroup && (newType != null && !(newType = newType.trim()).isEmpty()) ) {
+				throw new IllegalArgumentException("A custom group cannot have a login type passed in");
 			}
-			description = description.trim();
 			
-			SecurityGroupUtils.getInstance(user).editGroupAndPropagate(groupId, type, newGroupId, newType, description);
-			
+			SecurityGroupUtils.getInstance(user).editGroupAndPropagate(groupId, type, newGroupId, newType, newDescription, newCustomGroup);
 		} catch (IllegalArgumentException e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, e.getMessage());
 			return WebUtility.getResponse(errorRet, 400);
 		} catch (Exception e){
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			errorRet.put(Constants.ERROR_MESSAGE, "An unexpected error happened. Please try again.");
 			return WebUtility.getResponse(errorRet, 500);
 		}
@@ -188,7 +207,7 @@ public class AdminGroupAuthorizationResource extends AbstractAdminResource {
 			user = ResourceUtility.getUser(request);
 			groupUtils = SecurityGroupUtils.getInstance(user);
 		} catch (IllegalAccessException e) {
-			logger.error(Constants.STACKTRACE, e);
+			classLogger.error(Constants.STACKTRACE, e);
 			Map<String, String> errorMap = new HashMap<String, String>();
 			errorMap.put(Constants.ERROR_MESSAGE, e.getMessage());
 			return WebUtility.getResponse(errorMap, 401);
