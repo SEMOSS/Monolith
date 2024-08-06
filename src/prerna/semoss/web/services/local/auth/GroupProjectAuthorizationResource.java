8683865f4b0fafabf6f5b05db03d71446193e70d
kunal0137 <kunal0137@gmail.com>
2024-08-06 16:08:01 -0400

    Update GroupProjectAuthorizationResource.java

--- src/prerna/semoss/web/services/local/auth/GroupProjectAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/GroupProjectAuthorizationResource.java
@@ -48,7 +48,7 @@ public class GroupProjectAuthorizationResource {
 		
 	    projectId=WebUtility.inputSanitizer(projectId);
 	    type=WebUtility.inputSanitizer(type);
-	    groupId=WebUtility.inputSanitizer(groupId);
+	    groupId=WebUtility.inputSQLSanitizer(groupId);
 		
 		Map<String, String> errorMap = new HashMap<String, String>();
 		User user = null;
@@ -110,7 +110,7 @@ public class GroupProjectAuthorizationResource {
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
-		String groupId = WebUtility.inputSanitizer(form.getFirst("groupId"));
+		String groupId = WebUtility.inputSQLSanitizer(form.getFirst("groupId"));
 		String type =  WebUtility.inputSanitizer(form.getFirst("type"));
 		String projectId = WebUtility.inputSanitizer(form.getFirst("projectId"));
 		String permission = WebUtility.inputSanitizer(form.getFirst("permission"));
@@ -173,7 +173,7 @@ public class GroupProjectAuthorizationResource {
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
-		String groupId = WebUtility.inputSanitizer(form.getFirst("groupId"));
+		String groupId = WebUtility.inputSQLSanitizer(form.getFirst("groupId"));
 		String type = WebUtility.inputSanitizer(form.getFirst("type"));
 		String projectId = WebUtility.inputSanitizer(form.getFirst("projectId"));
 		String newPermission = WebUtility.inputSanitizer(form.getFirst("permission"));
@@ -233,7 +233,7 @@ public class GroupProjectAuthorizationResource {
 			return WebUtility.getResponse(errorMap, 401);
 		}
 		
-		String groupId = WebUtility.inputSanitizer(form.getFirst("groupId"));
+		String groupId = WebUtility.inputSQLSanitizer(form.getFirst("groupId"));
 		String type = WebUtility.inputSanitizer(form.getFirst("type"));
 		String projectId = WebUtility.inputSanitizer(form.getFirst("projectId"));
 		try {
