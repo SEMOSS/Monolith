3f35f30bdd89230a6b4ff879ecf13f2e6661b8c0
Maher Khalil <themaherkhalil@gmail.com>
2024-10-21 20:07:47 -0400

    UPDATE ::: initial review fixes

--- src/prerna/semoss/web/services/local/auth/EngineAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/EngineAuthorizationResource.java
@@ -1,6 +1,5 @@
 package prerna.semoss.web.services.local.auth;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +8,6 @@ import java.util.stream.Collectors;
 import javax.annotation.security.PermitAll;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -21,8 +19,6 @@ import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import com.google.gson.Gson;
 
@@ -34,7 +30,6 @@ import prerna.auth.utils.SecurityAdminUtils;
 import prerna.auth.utils.SecurityEngineUtils;
 import prerna.auth.utils.SecurityQueryUtils;
 import prerna.auth.utils.SecurityUpdateUtils;
-import prerna.graph.MSGraphAPICall;
 import prerna.graph.utility.MsGraphUtility;
 import prerna.om.Insight;
 import prerna.reactor.security.MyEnginesReactor;
@@ -926,18 +921,16 @@ public class EngineAuthorizationResource {
 	        }
 	    }
 
-	    if (searchTerm != null) {
-	        try {
-	            List<Map<String, Object>> filteredUsers =MsGraphUtility.getEngineUsers(request, user, engineId, searchTerm, limit, offset);
-	            return WebUtility.getResponse(filteredUsers, 200);
-	        } catch (IllegalStateException e) {
-	            Map<String, String> errorMap = new HashMap<>();
-	            errorMap.put(Constants.ERROR_MESSAGE, e.getMessage());
-	            return WebUtility.getResponse(errorMap, 500); 
-	        }
+	    try {
+	    	List<Map<String, Object>> filteredUsers = MsGraphUtility.getEngineUsers(request, user, engineId, searchTerm, limit, offset);
+	    	return WebUtility.getResponse(filteredUsers, 200);
+	    } catch (Exception e) {
+			classLogger.error(Constants.STACKTRACE, e);
+	    	Map<String, String> errorMap = new HashMap<>();
+	    	errorMap.put(Constants.ERROR_MESSAGE, e.getMessage());
+	    	return WebUtility.getResponse(errorMap, 500); 
 	    }
-	    
-	    return null;}
+	}
  
 	
 	/**
