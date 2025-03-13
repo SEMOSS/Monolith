fbc30840a04ba0b622bf3cb7d3566225d1b28a01
fnachabe <111369875+fnachabe@users.noreply.github.com>
2025-03-13 16:31:33 -0400

    fix(auth): switching to sql sanitizer (#111)

--- src/prerna/semoss/web/services/local/auth/AdminEngineAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/AdminEngineAuthorizationResource.java
@@ -358,7 +358,7 @@ public class AdminEngineAuthorizationResource extends AbstractAdminResource {
 			@QueryParam("searchTerm") String searchTerm, @QueryParam("permission") String permission, 
 			@QueryParam("limit") long limit, @QueryParam("offset") long offset) {
 		engineId = WebUtility.inputSanitizer(engineId);
-	    userId = WebUtility.inputSanitizer(userId);
+	    userId = WebUtility.inputSQLSanitizer(userId);
 	    searchTerm = WebUtility.inputSanitizer(searchTerm);
 	    permission = WebUtility.inputSanitizer(permission);
 	    
@@ -786,7 +786,7 @@ public class AdminEngineAuthorizationResource extends AbstractAdminResource {
 		}
 		Gson gson = new Gson();
 		List<String> ids = gson.fromJson(form.getFirst("ids"), List.class);
-		ids = WebUtility.inputSanitizer(ids);
+		ids = WebUtility.inputSQLSanitizer(ids);
 		try {
 			adminUtils.removeEngineUsers(ids, engineId);
 		} catch (Exception e) {
@@ -1017,7 +1017,7 @@ public class AdminEngineAuthorizationResource extends AbstractAdminResource {
 		
 		// updating user access requests in bulk
 		List<String> requestIds = new Gson().fromJson(form.getFirst("requestIds"), List.class);
-		requestIds = WebUtility.inputSanitizer(requestIds);
+		requestIds = WebUtility.inputSQLSanitizer(requestIds);
 		try {
 			AccessToken token = user.getAccessToken(user.getPrimaryLogin());
 			String userId = token.getId();
