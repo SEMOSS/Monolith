8171cd036d05f6f3176cabdf52a0d0f20feee645
Rithvik Doshi <doshirithvik@gmail.com>
2026-04-06 14:41:18 -0400

    fix: remove 401, clean up

--- src/prerna/web/conf/JsonErrorServlet.java
+++ src/prerna/web/conf/JsonErrorServlet.java
@@ -50,13 +50,10 @@ public class JsonErrorServlet extends HttpServlet {
 	private static String getDefaultMessage(int statusCode) {
 		switch (statusCode) {
 			case 400: return "Bad Request";
-			case 401: return "Unauthorized";
 			case 403: return "Forbidden";
 			case 404: return "Not Found";
-			case 405: return "Method Not Allowed";
+			case 416: return "Requested Range Not Satisfiable";
 			case 500: return "Internal Server Error";
-			case 502: return "Bad Gateway";
-			case 503: return "Service Unavailable";
 			default:  return "Error";
 		}
 	}
