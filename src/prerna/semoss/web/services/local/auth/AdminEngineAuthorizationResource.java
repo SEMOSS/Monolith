ae102dce7b2bb8c624433bfa385ec21cfb3913a8
Maher Khalil <themaherkhalil@gmail.com>
2026-07-12 00:44:30 -0400

    feat: adding limit, offset, project type, changing post to get for endpoints (#378)

--- src/prerna/semoss/web/services/local/auth/AdminEngineAuthorizationResource.java
+++ src/prerna/semoss/web/services/local/auth/AdminEngineAuthorizationResource.java
@@ -263,18 +263,17 @@ public class AdminEngineAuthorizationResource extends AbstractAdminResource {
 		return WebUtility.getResponse(outputNoun.getValue(), 200);
 	}
 
-	@POST
+	@GET
 	@Path("/getAllUserEngines")
 	@Produces("application/json")
-	public Response getAllUserEngines(@Context HttpServletRequest request, MultivaluedMap<String, String> form) {
+	public Response getAllUserEngines(@Context HttpServletRequest request, @QueryParam("userId") String userId,
+			@QueryParam("engineTypes") List<String> engineTypes, @QueryParam("searchTerm") String searchTerm,
+			@QueryParam("limit") long limit, @QueryParam("offset") long offset) {
+		userId = WebUtility.inputSQLSanitizer(userId);
+		searchTerm = WebUtility.inputSQLSanitizer(searchTerm);
+		engineTypes = WebUtility.inputSanitizer(engineTypes);
 		SecurityAdminUtils adminUtils = null;
 		User user = null;
-		String userId = WebUtility.inputSQLSanitizer(form.getFirst("userId"));
-		List<String> engineTypes = null;
-		if (WebUtility.inputSQLSanitizer(form.getFirst("engineTypes")) != null) {
-			engineTypes = new Gson().fromJson(form.getFirst("engineTypes"), List.class);
-			engineTypes = WebUtility.inputSanitizer(engineTypes);
-		}
 		try {
 			user = ResourceUtility.getUser(request);
 			adminUtils = performAdminCheck(request, user);
@@ -286,38 +285,27 @@ public class AdminEngineAuthorizationResource extends AbstractAdminResource {
 			return WebUtility.getResponse(errorMap, 401);
 		}
 
-		return WebUtility.getResponse(adminUtils.getAllUserEngines(userId, engineTypes), 200);
+		return WebUtility.getResponse(adminUtils.getAllUserEngines(userId, engineTypes, searchTerm, limit, offset),
+				200);
 	}
 
-	@POST
+	@GET
 	@Path("/getUserEnginesNoCredentials")
 	@Produces("application/json")
 	public Response getUserEnginesNoCredentials(@Context HttpServletRequest request,
-			MultivaluedMap<String, String> form) {
+			@QueryParam("userId") String userId, @QueryParam("engineTypes") List<String> engineTypes,
+			@QueryParam("searchTerm") String searchTerm, @QueryParam("limit") long limit,
+			@QueryParam("offset") long offset) {
+		userId = WebUtility.inputSQLSanitizer(userId);
+		searchTerm = WebUtility.inputSQLSanitizer(searchTerm);
+		engineTypes = WebUtility.inputSanitizer(engineTypes);
 		SecurityAdminUtils adminUtils = null;
 		User user = null;
-		String userId = WebUtility.inputSQLSanitizer(form.getFirst("userId"));
-		String searchTerm = WebUtility.inputSQLSanitizer(form.getFirst("searchTerm"));
-		List<String> engineTypes = null;
-		if (WebUtility.inputSQLSanitizer(form.getFirst("engineTypes")) != null) {
-			engineTypes = new Gson().fromJson(form.getFirst("engineTypes"), List.class);
-			engineTypes = WebUtility.inputSanitizer(engineTypes);
-		}
-		long limit = 0;
-		long offset = 0;
-		if (form.getFirst("limit") != null) {
-			limit = Long.parseLong(form.getFirst("limit"));
-		}
-		if (form.getFirst("offset") != null) {
-			offset = Long.parseLong(form.getFirst("offset"));
-		}
 		try {
 			user = ResourceUtility.getUser(request);
 			adminUtils = performAdminCheck(request, user);
 		} catch (IllegalAccessException e) {
-			classLogger.warn(
-					"User is trying to pull the engines that user {} does not have access to when not an admin",
-					userId);
+			classLogger.warn("Non-admin user tried to list the engines that user {} does not have access to", userId);
 			classLogger.error("Failed to list the engines that user {} does not have access to", userId, e);
 			Map<String, String> errorMap = new HashMap<String, String>();
 			errorMap.put(Constants.ERROR_MESSAGE, e.getMessage());
